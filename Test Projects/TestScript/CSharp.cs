99e54d96edf0cd6eb5de2bb80e04e3e20e4ab6d8
K.C. Oaks <google@vixenlights.com>
2011-07-16 16:01:37 +0000

    C# and VB script module test projects.

---
+++ Test Projects/TestScript/CSharp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module.Script;
+
+namespace TestScript {
+	public class CSharp : ScriptModuleInstanceBase {
+		// Does a script module do anything?
+	}
+}
