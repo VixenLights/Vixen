b0226049aa2c4baa0142163645bc7d1c376eaa73
Michael Sallaway <michael.sallaway@gmail.com>
2011-07-31 17:06:54 +0000

    - Changed a few more 'Comet' references to VixenApplication, and a few other build settings for release builds.

--- Modules/Application/VixenApplication/Properties/AssemblyInfo.cs
+++ Modules/Application/VixenApplication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Comet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Vixen")]
+[assembly: AssemblyDescription("Vixen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Comet")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Vixen")]
+[assembly: AssemblyProduct("Vixen")]
+[assembly: AssemblyCopyright("Copyright © Vixen 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
