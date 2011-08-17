f1d4ddd5e0c2f15f5323c501b0afe99fe2169890
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2011-08-17 04:24:14 +0000

    - Made the TimelineControl a design-time-added control, and removed old code. - Added a log box to see events

--- GUI_Ideas/TimelineTest/Form1.Designer.cs
+++ GUI_Ideas/TimelineTest/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.buttonElemAt = new System.Windows.Forms.Button();
             this.buttonReset = new System.Windows.Forms.Button();
+            this.tc = new Timeline.TimelineControl();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonElemAt
@@ -52,17 +54,46 @@
             this.buttonReset.UseVisualStyleBackColor = true;
             this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
+            // tc
+            // 
+            this.tc.AutoScroll = true;
+            this.tc.AutoScrollOffset = new System.Drawing.Point(453, 0);
+            this.tc.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tc.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tc.GridlineInterval = System.TimeSpan.Parse("00:00:01");
+            this.tc.Location = new System.Drawing.Point(13, 12);
+            this.tc.MaximumTime = System.TimeSpan.Parse("00:00:00");
+            this.tc.Name = "tc";
+            this.tc.RowSeparatorColor = System.Drawing.Color.Black;
+            this.tc.Size = new System.Drawing.Size(906, 438);
+            this.tc.TabIndex = 2;
+            this.tc.VisibleTimeSpan = System.TimeSpan.Parse("00:00:10");
+            this.tc.VisibleTimeStart = System.TimeSpan.Parse("00:00:04.9999875");
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(395, 533);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(524, 86);
+            this.textBoxLog.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(931, 641);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.tc);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.buttonElemAt);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +101,8 @@
 
         private System.Windows.Forms.Button buttonElemAt;
         private System.Windows.Forms.Button buttonReset;
+        private TimelineControl tc;
+        private System.Windows.Forms.TextBox textBoxLog;
     }
 }
 
--- GUI_Ideas/TimelineTest/Form1.cs
+++ GUI_Ideas/TimelineTest/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Timeline
 {
@@ -28,7 +29,11 @@ namespace Timeline
 
             // Rows
             tc.Rows.Add(new TimelineRow() { Height=50, Tag="Row 0" });
+            tc.Rows.Add(new TimelineRow() { Height=50,  Tag="Row 0" });
             tc.Rows.Add(new TimelineRow() { Height=100, Tag="Row 1" });
+            tc.Rows.Add(new TimelineRow() { Height=100, Tag="Row 2" });
+            tc.Rows.Add(new TimelineRow() { Height=100, Tag="Row 3" });
+            tc.Rows.Add(new TimelineRow() { Height=800, Tag="Row 4" });
 
             // Elements
             tc.Rows[0].Elements.Add(new TimelineElement()
@@ -48,30 +53,115 @@ namespace Timeline
                     Tag = "Green"
                 }
             );
+            
+            tc.Rows[1].Elements.Add(new TimelineElement()
+                {
+                    BackColor = Color.Blue,
+                    Offset = TimeSpan.FromSeconds(4),
+                    Duration = TimeSpan.FromSeconds(1),
+                    Tag = "Blue"
+                }
+            );
+
+            tc.Rows[2].Elements.Add(new TimelineElement()
+                {
+                    BackColor = Color.Orange,
+                    Offset = TimeSpan.FromSeconds(4),
+                    Duration = TimeSpan.FromSeconds(1),
+                    Tag = "Orange"
+                }
+            );
+
+            tc.Rows[3].Elements.Add(new TimelineElement()
+                {
+                    BackColor = Color.Purple,
+                    Offset = TimeSpan.FromSeconds(4),
+                    Duration = TimeSpan.FromSeconds(1),
+                    Tag = "Purple"
+                }
+            );
+
+
+            tc.Rows[4].Elements.Add(new TimelineElement()
+                {
+                    BackColor = Color.Black,
+                    Offset = TimeSpan.FromSeconds(4),
+                    Duration = TimeSpan.FromSeconds(1),
+                    Tag = "Black"
+                }
+            );
+            
 
 			tc.AddSnapTime(TimeSpan.FromSeconds(3.3), 5);
 			tc.AddSnapTime(TimeSpan.FromSeconds(4.6), 10);
 
 
-            tc.ElementsMoved += new EventHandler<ElementMovedEventArgs>(tc_ElementsMoved);
+            tc.ElementsMoved += new EventHandler<MultiElementEventArgs>(tc_ElementsMoved);
+            tc.ElementDoubleClicked += new EventHandler<ElementEventArgs>(tc_ElementDoubleClicked);
+            tc.SelectedElements.CollectionChanged += new EventHandler(SelectedElements_CollectionChanged);
+
+            tc.MouseClick += new MouseEventHandler(tc_MouseClick);
+            tc.MouseDown += new MouseEventHandler(tc_MouseDown);
+            tc.MouseDoubleClick += new MouseEventHandler(tc_MouseDoubleClick);
+            tc.MouseUp += new MouseEventHandler(tc_MouseUp);
         }
 
-        void tc_ElementsMoved(object sender, ElementMovedEventArgs e)
+
+        private int m_logIndex = 0;
+        private void _logMessage(string message)
         {
-            StringBuilder s = new StringBuilder("Elements moved:\n");
-            foreach (TimelineElement elem in e.MovedElements)
-                s.AppendLine(elem.Tag.ToString());
-            MessageBox.Show(s.ToString());
+            textBoxLog.Text = String.Format("{0}) {1}{2}{3}",
+                m_logIndex++, message, Environment.NewLine, textBoxLog.Text);
         }
 
-        private void buttonElemAt_Click(object sender, EventArgs e)
+        private string _elementList(IEnumerable<TimelineElement> elements)
         {
-            StringBuilder s = new StringBuilder("Elements at 2 sec:\n");
-            foreach (TimelineElement elem in tc.ElementsAtTime(TimeSpan.FromSeconds(2.0)))
-                s.AppendLine(elem.Tag.ToString());
-            MessageBox.Show(s.ToString());
+            StringBuilder s = new StringBuilder(); 
+            foreach (TimelineElement elem in elements)
+                s.AppendFormat("{0}, ", elem.Tag.ToString());
+            return s.ToString();
         }
 
+
+        void tc_ElementsMoved(object sender, MultiElementEventArgs e)
+        {
+            _logMessage("Elements moved: " + _elementList(e.Elements));
+        }
+
+        void tc_ElementDoubleClicked(object sender, ElementEventArgs e)
+        {
+            _logMessage(e.Element.Tag.ToString() + " double-clicked.");
+        }
+
+        void buttonElemAt_Click(object sender, EventArgs e)
+        {
+            var elems = tc.ElementsAtTime(TimeSpan.FromSeconds(2.0));
+            _logMessage("Elements at 2 sec: " + _elementList(elems));
+        }
+
+        void SelectedElements_CollectionChanged(object sender, EventArgs e)
+        {
+            _logMessage("Selected elements: " + _elementList((TimelineElementCollection)sender));
+        }
+
+
+        
+        void tc_MouseClick(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("MouseClick");
+        }
+        void tc_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("MouseDoubleClick");
+        }
+        void tc_MouseDown(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("MouseDown");
+        }
+        void tc_MouseUp(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine("MouseUp");
+        }
 
 
         private void buttonReset_Click(object sender, EventArgs e)
