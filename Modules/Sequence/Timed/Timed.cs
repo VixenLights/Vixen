ddf6f151964e7ba469ecdacb31945e42dfbf31a0
Michael Sallaway <michael.sallaway@gmail.com>
2011-09-03 15:26:31 +0000

    - Added the 'Timed' sequence type as a real module. - updated the 'All Modules' solution to store the sub-projects in folders to accurately reflect the structure.

---
+++ Modules/Sequence/Timed/Timed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module;
+using Vixen.Module.Sequence;
+using Vixen.Sys;
+
+namespace TestSequences {
+	public class Timed : SequenceModuleInstanceBase {
+	}
+}
---
+++ Modules/Sequence/Timed/TimedSequenceModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module;
+using Vixen.Module.Sequence;
+
+namespace TestSequences {
+	public class TimedSequenceModule : SequenceModuleDescriptorBase {
+		private Guid _typeId = new Guid("{4C258A3B-E725-4AE7-B50B-103F6AB8121E}");
+
+		override public string FileExtension {
+			get { return ".tim"; }
+		}
+
+		override public Guid TypeId {
+			get { return _typeId; }
+		}
+
+		override public Type ModuleClass {
+			get { return typeof(Timed); }
+		}
+
+		override public Type ModuleDataClass {
+			get { return null; }
+		}
+
+		override public string Author {
+			get { return "Vixen Team"; }
+		}
+
+		override public string TypeName {
+			get { return "Timed Sequence"; }
+		}
+
+		override public string Description {
+			get { return "A basic timed sequence, which is a collection of effects configured to occur at predefined times."; }
+		}
+
+		override public string Version {
+			get { return "0.1"; }
+		}
+	}
+}
