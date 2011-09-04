04e65f0b75fbbe35a10402bf0b8bf1dce32a2ce9
Michael Sallaway <michael.sallaway@gmail.com>
2011-09-04 00:52:47 +0000

    add the start of the timed sequence editor (barebones); doesn't actually do anything yet.

---
+++ Modules/Editor/TimedSequenceEditor/TimedSequenceEditorDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module;
+
+namespace TimedSequenceEditor
+{
+	class TimedSequenceEditorDataModel : IModuleDataModel
+	{
+		// TODO: I have no idea what I should be populating these with. Ideas?
+		public Guid ModuleTypeId { get; set; }
+		public Guid ModuleInstanceId { get; set; }
+		public IModuleDataSet ModuleDataSet { get; set; }
+
+		public IModuleDataModel Clone()
+		{
+			TimedSequenceEditorDataModel newInstance = new TimedSequenceEditorDataModel();
+			// TODO: copy any custom data to the new object here. Like this:
+			//newInstance.LastOpened = LastOpened;
+			return newInstance;
+		}
+
+	}
+}
---
+++ Modules/Editor/TimedSequenceEditor/TimedSequenceEditorDescriptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Sys;
+using Vixen.Module.Editor;
+
+namespace TimedSequenceEditor
+{
+	class TimedSequenceEditorDescriptor : EditorModuleDescriptorBase
+	{
+		private Guid _typeId = new Guid("{d342eedd-ae39-4b30-b557-b9329e6d3a7c}");
+		private string[] _extensions = new string[] { ".tim" };
+		private Guid[] _dependencies = new Guid[] { new Guid("{4C258A3B-E725-4AE7-B50B-103F6AB8121E}") };
+
+		public override string TypeName
+		{
+			get { return "Timed Sequence Editor"; }
+		}
+
+		public override Guid TypeId
+		{
+			get { return _typeId; }
+		}
+
+		public override Type ModuleClass
+		{
+			get { return typeof(TimedSequenceEditor); }
+		}
+
+		public override Type ModuleDataClass
+		{
+			get { return typeof(TimedSequenceEditorDataModel); }
+		}
+
+		public override string Author
+		{
+			get { return "Vixen Team"; }
+		}
+
+		public override string Description
+		{
+			get { return "An editor for a standard timed sequence"; }
+		}
+
+		public override string Version
+		{
+			get { return "0.0.1"; }
+		}
+
+		public override string[] FileExtensions
+		{
+			get { return _extensions; }
+		}
+
+		public override Guid[] Dependencies
+		{
+			get { return _dependencies; }
+		}
+	}
+}
