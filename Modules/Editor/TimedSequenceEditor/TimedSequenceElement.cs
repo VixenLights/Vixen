68b3975af1b15f8fe1e427b8622c9d355122713d
Michael Sallaway <michael.sallaway@gmail.com>
2011-10-09 12:36:14 +0000

    VIX-10
    - nothing fancy yet, but drawing the sequence elements a bit better: random color, and they write on their name and details, so you can see what they're doing a bit better.

--- Modules/Editor/TimedSequenceEditor/TimedSequenceElement.cs
+++ Modules/Editor/TimedSequenceEditor/TimedSequenceElement.cs
@@ -6,18 +6,37 @@ using System.Windows.Forms;
 using CommonElements.Timeline;
 using Vixen.Module.Editor;
 using Vixen.Sys;
+using System.Drawing;
 
 namespace VixenModules.Editor.TimedSequenceEditor
 {
 	class TimedSequenceElement : TimelineElement
 	{
 		public TimedSequenceElement(EffectNode effectNode)
+			: base()
 		{
 			StartTime = effectNode.StartTime;
 			Duration = effectNode.TimeSpan;
 			Effect = effectNode;
+			Random r = new Random();
+			BackColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
 		}
 
 		public EffectNode Effect { get; set; }
+
+		public override System.Drawing.Bitmap Draw(System.Drawing.Size imageSize)
+		{
+			Bitmap result = base.Draw(imageSize);
+			Graphics g = Graphics.FromImage(result);
+
+			// add text describing the effect
+			Font f = new Font("Arial", 7);
+			Brush b = new SolidBrush(Color.Black);
+			g.DrawString(Effect.Effect.EffectName, f, b, new PointF(5, 5));
+			g.DrawString("Start: " + Effect.StartTime.ToString("g"), f, b, new PointF(5, 23));
+			g.DrawString("Length: " + Effect.TimeSpan.ToString("g"), f, b, new PointF(5, 35));
+
+			return result;
+		}
 	}
 }
