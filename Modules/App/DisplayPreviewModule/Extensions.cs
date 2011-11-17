af225bb65a3c10737511e62410abb5d3083b971c
Erik Mathisen <erik@mathisen.us>
2011-11-17 00:52:39 +0000

    VIX-7
    Fixed a few potential null reference issues.

--- Modules/App/DisplayPreviewModule/Extensions.cs
+++ Modules/App/DisplayPreviewModule/Extensions.cs
@@ -20,22 +20,23 @@ namespace VixenModules.App.DisplayPreview
 
         public static Dictionary<ChannelNode, Color> ToMediaColor(this Dictionary<ChannelNode, System.Drawing.Color> stateValues)
         {
+            if (stateValues == null)
+            {
+                return new Dictionary<ChannelNode, Color>();
+            }
+
             var newValues =
                 stateValues.ToList().Select(
-                                            x =>
-                                            new KeyValuePair<ChannelNode, Color>(
-                                                x.Key, Color.FromArgb(x.Value.A, x.Value.R, x.Value.G, x.Value.B))).ToDictionary(
-                                                                                                                                 x =>
-                                                                                                                                 x.Key,
-                                                                                                                                 x =>
-                                                                                                                                 x.
-                                                                                                                                     Value);
+                    x =>
+                    new KeyValuePair<ChannelNode, Color>(
+                        x.Key, Color.FromArgb(x.Value.A, x.Value.R, x.Value.G, x.Value.B))).ToDictionary(x => x.Key, x => x.Value);
             return newValues;
         }
 
         public static bool IsRgbNode(this ChannelNode channelNode)
         {
-            return channelNode != null && channelNode.Properties.Any(x => x is RGBModule);
+            return channelNode != null && channelNode.Properties != null
+                   && channelNode.Properties.Any(x => x is RGBModule);
         }
     }
 }
