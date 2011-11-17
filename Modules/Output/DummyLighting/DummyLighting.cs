b4c74791dbaecf9c545acda5a0de620b7e6e94a0
Michael Sallaway <michael.sallaway@gmail.com>
2011-11-17 13:03:41 +0000

    <no ticket>
    stripped out the 'fake renard' output module into a new one called "Dummy Lighting". Figured it's a good basic one we can keep with it for people that have no controllers and want to play with it.
    
    You'll most likely need to do a clean and rebuild after getting this update, just to make sure.
    
    There's now no more of the Test modules for the real one. Huzzah!

---
+++ Modules/Output/DummyLighting/DummyLighting.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module.Output;
+using Vixen.Module;
+using Vixen.Sys;
+using Vixen.Commands;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace VixenModules.Output.DummyLighting
+{
+	public class DummyLighting : OutputModuleInstanceBase
+	{
+		private List<string> _output = new List<string>();
+		private DummyLightingOutputForm _form;
+		private Stopwatch _sw;
+		private int _updateCount;
+		private DummyLightingData _data;
+
+		public DummyLighting()
+		{
+			_form = new DummyLightingOutputForm();
+			_sw = new Stopwatch();
+		}
+
+		public override IModuleDataModel ModuleData
+		{
+			get { return _data; }
+			set
+			{
+				_data = value as DummyLightingData;
+				_form.renderingStyle = _data.RenderStyle;
+			}
+		}
+
+		override protected void _SetOutputCount(int outputCount)
+		{
+			_form.OutputCount = outputCount;
+		}
+
+		override protected void _UpdateState(Command[] outputStates)
+		{
+			if (_updateCount++ == 0) {
+				_sw.Reset();
+				_sw.Start();
+			}
+
+			_form.UpdateState(1000 * ((double)_updateCount / _sw.ElapsedMilliseconds), outputStates);
+		}
+
+		override public void Start()
+		{
+			_form.Show();
+			_updateCount = 0;
+		}
+
+		override public void Stop()
+		{
+			_form.Hide();
+			_sw.Stop();
+		}
+
+		override public bool HasSetup
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		override public bool Setup()
+		{
+			DummyLightingSetup setup = new DummyLightingSetup();
+			setup.RenderStyle = _form.renderingStyle;
+			DialogResult result = setup.ShowDialog();
+			if (result == DialogResult.OK) {
+				_data.RenderStyle = setup.RenderStyle;
+				_form.renderingStyle = setup.RenderStyle;
+				return true;
+			}
+			return false;
+		}
+
+		override public bool IsRunning
+		{
+			get { return _form != null && _form.Visible; }
+		}
+
+		override public void Dispose()
+		{
+			_form.Dispose();
+			_form = null;
+			GC.SuppressFinalize(this);
+		}
+
+		~DummyLighting()
+		{
+			_form = null;
+		}
+	}
+}
