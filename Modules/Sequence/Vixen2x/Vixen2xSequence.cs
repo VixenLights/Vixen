cda41d5d8c22f15b31b358d0c62f625919845d09
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2011-12-18 22:04:44 +0000

    Added the start of a Vixen2x sequence, to be used for importing old sequences.

---
+++ Modules/Sequence/Vixen2x/Vixen2xSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Module;
+using Vixen.Module.Sequence;
+using Vixen.Sys;
+
+namespace VixenModules.Sequence.Vixen2x
+{
+	public class Vixen2xSequence : SequenceModuleInstanceBase
+	{
+		public Vixen2xSequence()
+		{
+		}
+
+		public Vixen2xSequence(Vixen2xSequence original)
+			: base(original)
+		{
+		}
+
+		public override IModuleInstance Clone()
+		{
+			return new Vixen2xSequence(this);
+		}
+	}
+}
