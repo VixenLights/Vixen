f4a46aaa69dff56f6133f59f3e2c2b8b0c07c125
Tony Eberle <eberletj@gmail.com>
2011-12-20 12:50:14 +0000

    Added an additional check on the PortAddress to ensure that the user could not enter a value of 0

--- CommonElements/ParallelPortConfig.cs
+++ CommonElements/ParallelPortConfig.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace CommonElements
 {
     public partial class ParallelPortConfig : Form
     {
         private int _OtherAddressIndex;
+        private int _DefaultPort;
         public ParallelPortConfig(int portAddress)
         {
             InitializeComponent();
             _OtherAddressIndex = 3;
+            _DefaultPort = portAddress;
             switch (portAddress)
             {
                 case 0x278:
@@ -46,15 +49,24 @@ namespace CommonElements
         {
             if (portComboBox.SelectedIndex == _OtherAddressIndex)
             {
-                try
+                if (PortAddress != 0)
                 {
-                    Convert.ToUInt16(portTextBox.Text, 0x10);
+                    try
+                    {
+                        Convert.ToUInt16(portTextBox.Text, 0x10);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("The port number is not a valid hexadecimal number.", "Parallel Port Setup", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        base.DialogResult = DialogResult.None;
+                    }
                 }
-                catch
+                else
                 {
-                    MessageBox.Show("The port number is not a valid hexadecimal number.", "ParallelPortConfig", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                    base.DialogResult = DialogResult.None;
+                    MessageBox.Show("The port address is 0.", "Parallel Port Setup", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    DialogResult = DialogResult.None;
                 }
+
             }
             
         }
