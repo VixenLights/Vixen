0f69d4becd0b2ceb0ec105bdf9f5a152fae77ef6
Michael Sallaway <michael.sallaway@gmail.com>
2012-01-24 14:22:30 +0000

    VIX-73
    changed the display preview to not steal focus when playing if the window is already open. Also added some debugging info, apparently, but it's all commented out.... hmm...

--- Modules/App/DisplayPreviewModule/ViewModels/VisualizerViewModel.cs
+++ Modules/App/DisplayPreviewModule/ViewModels/VisualizerViewModel.cs
@@ -3,6 +3,8 @@ namespace VixenModules.App.DisplayPreview.ViewModels
     using Vixen.Sys;
     using VixenModules.App.DisplayPreview.Model;
     using VixenModules.Property.RGB;
+	using System.Collections.Generic;
+	using Vixen.Commands;
 
     public class VisualizerViewModel : ViewModelBase
     {
@@ -27,9 +29,33 @@ namespace VixenModules.App.DisplayPreview.ViewModels
             }
         }
 
+		//private static int temp = 0;
+		//private static System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
         public void UpdateExecutionStateValues(ExecutionStateValues stateValues)
         {
-            var colorsByChannel = RGBModule.MapChannelCommandsToColors(stateValues).ToMediaColor();
+			//if (temp % 100 == 0) {
+			//    VixenSystem.Logging.Debug("DP: logged " + temp + " updates in " + watch.ElapsedMilliseconds + "ms, which is " + (temp / (double)(watch.ElapsedMilliseconds / 1000.0)) + " Hz");
+			//    watch.Reset();
+			//    watch.Start();
+			//    temp = 0;
+			//}
+
+			//temp++;
+
+			//bool allNull = true;
+			//foreach (KeyValuePair<Channel, Command> kvp in stateValues) {
+			//    if (kvp.Value != null) {
+			//        allNull = false;
+			//        break;
+			//    }
+			//}
+
+			//if (allNull && stateValues.Count > 0) {
+			//    VixenSystem.Logging.Debug("DP: All null commands. state values has " + stateValues.Count + " items. Count is " + temp);
+			//}
+
+			var colorsByChannel = RGBModule.MapChannelCommandsToColors(stateValues).ToMediaColor();
             foreach (var displayItem in DataModel.DisplayItems)
             {
                 displayItem.UpdateChannelColors(colorsByChannel);
