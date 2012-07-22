96169ab0cc9748ad0d60dbd8d6119ebea6d409e3
K.C. Oaks <kc@vixenlights.com>
2012-07-22 14:08:31 -0400

    Grid property module.  Apply it to a node to describe the node's ultimate channels as being a grid.  This can be used by effects (such as the ImageGrid effect) to know how to format themselves.

---
+++ Modules/Property/Grid/Module.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using Vixen.Module;
+using Vixen.Module.Property;
+
+namespace VixenModules.Property.Grid {
+	public class Module : PropertyModuleInstanceBase {
+		private Data _data;
+
+		public override void SetDefaultValues() {
+			_data.Width = Owner.Count();
+			_data.Height = 1;
+		}
+
+		public override bool HasSetup {
+			get { return true; }
+		}
+
+		public override bool Setup() {
+			using(SetupForm setupForm = new SetupForm(_data.Width, _data.Height, Owner.Count())) {
+				if(setupForm.ShowDialog() == DialogResult.OK) {
+					_data.Width = setupForm.SelectedWidth;
+					_data.Height = setupForm.SelectedHeight;
+					return true;
+				}
+				return false;
+			}
+		}
+
+		public override IModuleDataModel ModuleData {
+			get { return _data; }
+			set { _data = (Data)value; }
+		}
+	}
+}
---
+++ Modules/Property/Grid/SetupForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VixenModules.Property.Grid {
+	partial class SetupForm : Form {
+		private int _productRequired;
+
+		public SetupForm(int width, int height, int productRequired) {
+			InitializeComponent();
+			_Width = width;
+			_Height = height;
+			_ProductRequired = productRequired;
+		}
+
+		public int SelectedWidth { get; private set; }
+
+		public int SelectedHeight { get; private set; }
+
+		private int _Width {
+			get { return (int)nudWidth.Value; }
+			set { nudWidth.Value = value; }
+		}
+
+		private int _Height {
+			get { return (int)nudHeight.Value; }
+			set { nudHeight.Value = value; }
+		}
+
+		private int _ProductRequired {
+			get { return _productRequired; }
+			set {
+				_productRequired = value;
+				labelProduct.Text = "The product of these needs to be " + value;
+			}
+		}
+
+		private void _CheckProduct() {
+			bool productIsCorrect = _Width * _Height == _ProductRequired;
+			labelProduct.Visible = !productIsCorrect;
+			buttonOK.Enabled = productIsCorrect;
+		}
+
+		private void nudWidth_ValueChanged(object sender, EventArgs e) {
+			_CheckProduct();
+		}
+
+		private void nudHeight_ValueChanged(object sender, EventArgs e) {
+			_CheckProduct();
+		}
+
+		private void nudWidth_Leave(object sender, EventArgs e) {
+			_CheckProduct();
+		}
+
+		private void nudHeight_Leave(object sender, EventArgs e) {
+			_CheckProduct();
+		}
+
+		private void buttonOK_Click(object sender, EventArgs e) {
+			SelectedWidth = _Width;
+			SelectedHeight = _Height;
+		}
+	}
+}
