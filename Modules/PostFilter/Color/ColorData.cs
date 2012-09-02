c6915a1231466fcb7bee69423b74c5fd40be7a39
K.C. Oaks <kc@vixenlights.com>
2012-09-01 21:22:11 -0400

    Setting a default (empty) value for ColorFilter in ColorData.cs

--- Modules/PostFilter/Color/ColorData.cs
+++ Modules/PostFilter/Color/ColorData.cs
@@ -4,6 +4,10 @@ using Vixen.Module;
 namespace VixenModules.OutputFilter.Color {
 	[DataContract]
 	public class ColorData : ModuleDataModelBase {
+		public ColorData() {
+			FilterOrder = new ColorFilter[0];
+		}
+
 		public override IModuleDataModel Clone() {
 			ColorData newInstance = (ColorData)MemberwiseClone();
 			return newInstance;
