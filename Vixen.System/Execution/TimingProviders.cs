b534c238ae31960f486156922434ca4fa1d4e742
K.C. Oaks <kc@vixenlights.com>
2012-11-18 15:35:10 -0500

    Removed some old commented code that I came across.

--- Vixen.System/Execution/TimingProviders.cs
+++ Vixen.System/Execution/TimingProviders.cs
@@ -7,57 +7,11 @@ using Vixen.Sys;
 
 namespace Vixen.Execution {
 	public class TimingProviders {
-		//static private Dictionary<string,ITimingProvider> _providers;
-		//private SelectedTimingProvider _selectedTimingProvider;
-
-		//static TimingProviders() {
-		//    _providers = typeof(ITimingProvider)
-		//        .FindImplementationsWithin(Assembly.GetExecutingAssembly())
-		//        .Select(Activator.CreateInstance)
-		//        .Cast<ITimingProvider>()
-		//        .ToDictionary(x => x.TimingProviderTypeName, x => x);
-		//}
-
 		public TimingProviders(ISequence owner) {
 			Owner = owner;
 		}
 
-		//public TimingProviders(ISequence owner, TimingProviders original)
-		//    : this(owner) {
-		//        SelectedTimingProvider = original.SelectedTimingProvider;
-		//}
-
 		public ISequence Owner { get; private set; }
-
-		//public SelectedTimingProvider SelectedTimingProvider {
-		//    get { return _selectedTimingProvider; }
-		//    set {
-		//        if(!SelectedTimingProvider.IsValid(value)) {
-		//            // Reset to having no explicit timing source.
-		//            _selectedTimingProvider = null;
-		//        } else {
-		//            ITimingProvider provider;
-		//            if(_providers.TryGetValue(value.ProviderType, out provider)) {
-		//                if(provider.GetTimingSource(Owner, value.SourceName) != null) {
-		//                    _selectedTimingProvider = value;
-		//                    return;
-		//                }
-		//            }
-		//            throw new InvalidOperationException("Timing source does not exist.");
-		//        }
-		//    }
-		//}
-
-		//public ITiming GetSelectedSource() {
-		//    ITiming selectedSource = null;
-
-		//    ITimingProvider provider;
-		//    if(SelectedTimingProvider.IsValid(SelectedTimingProvider) && _providers.TryGetValue(SelectedTimingProvider.ProviderType, out provider)) {
-		//        selectedSource = provider.GetTimingSource(Owner, SelectedTimingProvider.SourceName);
-		//    }
-
-		//    return selectedSource;
-		//}
 
 		public ITiming GetTimingSource(SelectedTimingProvider selectedProvider) {
 			ITiming selectedSource = null;
@@ -71,24 +25,6 @@ namespace Vixen.Execution {
 
 			return selectedSource;
 		}
-
-		//public void SetSelectedSource(string providerType, string sourceName) {
-		//    if(providerType == null || sourceName == null) {
-		//        // Reset to having no explicit timing source.
-		//        _selectedProviderType = null;
-		//        _selectedSourceName = null;
-		//    } else {
-		//        ITimingProvider provider;
-		//        if(_providers.TryGetValue(providerType, out provider)) {
-		//            if(provider.GetTimingSource(Owner, sourceName) != null) {
-		//                _selectedProviderType = providerType;
-		//                _selectedSourceName = sourceName;
-		//                return;
-		//            }
-		//        }
-		//        throw new InvalidOperationException("Timing source does not exist.");
-		//    }
-		//}
 
 		public string[] GetTimingProviderTypes() {
 			var providers = _GetProviders();
