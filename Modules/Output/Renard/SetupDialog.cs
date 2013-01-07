14198764b9fd7ff9e533bb5bbafccbba307749c2
John McAdams <macebobo@gmail.com>
2013-01-07 08:12:51 -0800

    Replaced tabs with spaces
    I am not sure this is how you folks want to do this, but I did, can back
    out if not appropriate.

--- Modules/Output/Renard/SetupDialog.cs
+++ Modules/Output/Renard/SetupDialog.cs
@@ -10,13 +10,13 @@ using System.IO.Ports;
 
 namespace VixenModules.Output.Renard
 {
-	public partial class SetupDialog : Form {
-		private Data _data;
-		private SerialPort _port;
+    public partial class SetupDialog : Form {
+        private Data _data;
+        private SerialPort _port;
 
-		public SetupDialog(Data data) {
-			InitializeComponent();
-			_data = data;
+        public SetupDialog(Data data) {
+            InitializeComponent();
+            _data = data;
             if (data.ProtocolVersion > 0)
             {
                 comboBoxProtocolVersion.SelectedIndex = _data.ProtocolVersion - 1;
@@ -26,24 +26,24 @@ namespace VixenModules.Output.Renard
                 _port = new SerialPort(_data.PortName, _data.BaudRate, _data.Parity,
                  _data.DataBits, _data.StopBits);
             }
-		}
+        }
 
-		private void buttonPortSetup_Click(object sender, EventArgs e) {
-			using(Common.Controls.SerialPortConfig serialPortConfig = new Common.Controls.SerialPortConfig(_port)) {
-				if(serialPortConfig.ShowDialog() == DialogResult.OK) {
-					_port = serialPortConfig.SelectedPort;
-				}
-			}
-		}
+        private void buttonPortSetup_Click(object sender, EventArgs e) {
+            using(Common.Controls.SerialPortConfig serialPortConfig = new Common.Controls.SerialPortConfig(_port)) {
+                if(serialPortConfig.ShowDialog() == DialogResult.OK) {
+                    _port = serialPortConfig.SelectedPort;
+                }
+            }
+        }
 
-		private void buttonOK_Click(object sender, EventArgs e) {
-			_data.ProtocolVersion = comboBoxProtocolVersion.SelectedIndex + 1;
+        private void buttonOK_Click(object sender, EventArgs e) {
+            _data.ProtocolVersion = comboBoxProtocolVersion.SelectedIndex + 1;
 
-			_data.PortName = _port.PortName;
-			_data.BaudRate = _port.BaudRate;
-			_data.Parity = _port.Parity;
-			_data.DataBits = _port.DataBits;
-			_data.StopBits = _port.StopBits;
-		}
-	}
+            _data.PortName = _port.PortName;
+            _data.BaudRate = _port.BaudRate;
+            _data.Parity = _port.Parity;
+            _data.DataBits = _port.DataBits;
+            _data.StopBits = _port.StopBits;
+        }
+    }
 }
