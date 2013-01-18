08e0f6a10bee8679fad34b463c39cb805e91fceb
John McAdams <macebobo@gmail.com>
2013-01-17 23:24:06 -0800

    Refactor implementation specific methods to GenericSerial.cs

--- Modules/Output/GenericSerial/Module.cs
+++ Modules/Output/GenericSerial/Module.cs
@@ -1,5 +1,5 @@
-﻿using System.IO.Ports;
-using System.Text;
+﻿
+
 using Vixen.Commands;
 using Vixen.Module;
 using Vixen.Module.Controller;
@@ -8,14 +8,9 @@ namespace VixenModules.Output.GenericSerial
 {
     public class Module : ControllerModuleInstanceBase {
 
-        private Data _Data;
-        private SerialPort _SerialPort = null;
+        private GenericSerial gs = new GenericSerial();
+        private Data _ModuleData;
         private CommandHandler _commandHandler;
-        private byte[] _packet;
-        private byte[] _header;
-        private byte[] _footer;
-        private int headerLen = 0;
-        private int footerLen = 0;
 
         public Module()
         {
@@ -25,31 +20,10 @@ namespace VixenModules.Output.GenericSerial
 
         public override void UpdateState(int chainIndex, ICommand[] outputStates)
         {
-            if (serialPortIsValid && _SerialPort.IsOpen)
+            if (IsRunning)
             {
-                _packet = new byte[headerLen + OutputCount + footerLen];
-                var packetLen = _packet.Length; 
-                
-                _header.CopyTo(_packet, 0);
-                _footer.CopyTo(_packet, packetLen - footerLen);
-
-                for (int i = 0; i < outputStates.Length && IsRunning; i++)
-                {
-                    _commandHandler.Reset();
-                    ICommand command = outputStates[i];
-                    if (command != null)
-                    {
-                        command.Dispatch(_commandHandler);
-                    }
-                    _packet[i + headerLen] = _commandHandler.Value;
-                }
-
-                if (outputStates.Length > headerLen + footerLen)
-                {
-                    _SerialPort.Write(_packet, 0, packetLen);
-                }
+                gs.SendUpdate(outputStates, _ModuleData, _commandHandler);
             }
-
         }
 
         public override bool HasSetup
@@ -59,84 +33,44 @@ namespace VixenModules.Output.GenericSerial
 
         public override bool Setup()
         {
-            SetupDialog setup = new SetupDialog(_Data);
+            SetupDialog setup = new SetupDialog(_ModuleData);
             setup.ShowDialog();
             setup.Close();
 
-            return _Data.IsValid;
+            return _ModuleData.IsValid;
         }
 
         public override IModuleDataModel ModuleData
         {
-            get { return _Data; }
+            get { return _ModuleData; }
             set
             {
-                _Data = (Data)value;
-                initModule();
+                _ModuleData = (Data)value;
+
+                gs.initModule(_ModuleData);
+                
+                if (gs.serialPortIsValid && IsRunning)
+                {
+                    gs.openSerialPort();
+                }
             }
         }
 
         public override void Start()
         {
             base.Start();
-            if (serialPortIsValid && !_SerialPort.IsOpen)
-            {
-                _SerialPort.Open();
-            }
+            gs.openSerialPort();
         }
 
         public override void Stop()
         {
-            if (serialPortIsValid && _SerialPort.IsOpen)
-            {
-                _SerialPort.Close();
-            }
+            gs.closeSerialPort();
             base.Stop();
         }
 
-        private void initModule()
-        {
-            dropExistingSerialPort();
-            createSerialPortFromData();
-            _header = Encoding.ASCII.GetBytes(_Data.Header == null ? string.Empty : _Data.Header);
-            headerLen = _header.Length;
-            _footer = Encoding.ASCII.GetBytes(_Data.Footer == null ? string.Empty : _Data.Footer);
-            footerLen = _footer.Length;
 
-            if (serialPortIsValid && IsRunning)
-            {
-                _SerialPort.Open();
-            }
-        }
 
-        private void dropExistingSerialPort()
-        {
-            if (serialPortIsValid)
-            {
-                _SerialPort.Dispose();
-                _SerialPort = null;
-            }
-        }
-    
-        private void createSerialPortFromData()
-        {
-            if (_Data.IsValid)
-            {
-                _SerialPort = new SerialPort(
-                    _Data.PortName,
-                    _Data.BaudRate,
-                    _Data.Parity,
-                    _Data.DataBits,
-                    _Data.StopBits);
 
-                _SerialPort.Handshake = Handshake.None;
-                _SerialPort.Encoding = Encoding.UTF8;
-            }
-        }
 
-        private bool serialPortIsValid
-        {
-            get { return _SerialPort != null; }
-        }
     }
 }
