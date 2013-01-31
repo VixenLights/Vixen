02fab44ea1b91e41dff209d2c4a08f1296d26f59
Tony Eberle <eberletj@cox.net>
2013-01-31 15:32:59 -0600

    Vix-169 FGDimmer Code
    Committing code to github, waiting on testing

---
+++ Modules/Output/FGDimmer/CommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vixen.Sys.Dispatch;
+using Vixen.Commands;
+
+namespace VixenModules.Output.FGDimmer
+{
+    class CommandHandler : CommandDispatch
+    {
+        public byte Value { get; private set; }
+
+        public void Reset()
+        {
+            Value = 0;
+        }
+
+        public override void Handle(_8BitCommand obj)
+        {
+            Value = obj.CommandValue;
+        }
+    }
+}
---
+++ Modules/Output/FGDimmer/Descriptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vixen.Module.Controller;
+
+namespace VixenModules.Output.FGDimmer
+{
+    public class Descriptor : ControllerModuleDescriptorBase
+    {
+        private readonly Guid _typeId = new Guid("{1AD81BB5-CB3E-4479-B758-7BC1EDBECC12}");
+
+        public override string Author
+        {
+            get { return "Tony Eberle"; }
+        }
+
+        public override string Description
+        {
+            get { return "FireGod Dimmer hardware module"; }
+        }
+
+        public override Type ModuleClass
+        {
+            get { return typeof(FGDimmer); }
+        }
+        public override Type ModuleDataClass
+        {
+            get { return typeof(FGDimmerData); }
+        }
+
+        public override Guid TypeId
+        {
+            get { return _typeId; }
+        }
+
+        public override string TypeName
+        {
+            get { return "FGDimmer"; }
+        }
+
+        public override string Version
+        {
+            get { return "1.0"; }
+        }
+    }
+}
