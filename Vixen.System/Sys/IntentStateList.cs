2c26bbbebbe617b69c2c05460616f9df2ba88875
Jeff Uchitjil <jeffu@kb9kld.org>
2013-06-21 11:20:18 -0500

    Add a AddRange method to utilize internal .NET optimizations instead of iterating and adding one at a time.

--- Vixen.System/Sys/IntentStateList.cs
+++ Vixen.System/Sys/IntentStateList.cs
@@ -6,13 +6,19 @@ namespace Vixen.Sys {
 		}
 
 		public IntentStateList(IEnumerable<IIntentState> states) {
-			foreach(IIntentState intentState in states) {
-				AddIntentState(intentState);
-			}
+			//foreach(IIntentState intentState in states) {
+			//    AddIntentState(intentState);
+			//}
+			AddRangeIntentState(states); //Faster than for add
 		}
 
 		virtual public void AddIntentState(IIntentState intentState) {
 			Add(intentState);
+		}
+
+		virtual public void AddRangeIntentState(IEnumerable<IIntentState> intentStates)
+		{
+			AddRange(intentStates);
 		}
 	}
 }
