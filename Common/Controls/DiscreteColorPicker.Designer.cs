0f6424bb2aa8e607634d94296963c2fa961ddb02
Michael Sallaway <michael@sallaway.com>
2013-06-26 00:50:10 +1000

    Add a discrete color picker form/control. Can be used to display a list of colors, and allow the user to tick to select one or more.

---
+++ Common/Controls/DiscreteColorPicker.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Common.Controls
+{
+	partial class DiscreteColorPicker
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tableLayoutPanelColors = new System.Windows.Forms.TableLayoutPanel();
+			this.SuspendLayout();
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(217, 247);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 0;
+			this.buttonCancel.Text = "Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.buttonOK.Location = new System.Drawing.Point(136, 247);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 1;
+			this.buttonOK.Text = "OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(27, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(170, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Select one or more discrete colors:";
+			// 
+			// tableLayoutPanelColors
+			// 
+			this.tableLayoutPanelColors.ColumnCount = 3;
+			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelColors.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+			this.tableLayoutPanelColors.Location = new System.Drawing.Point(28, 52);
+			this.tableLayoutPanelColors.Name = "tableLayoutPanelColors";
+			this.tableLayoutPanelColors.RowCount = 3;
+			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33233F));
+			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33533F));
+			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33233F));
+			this.tableLayoutPanelColors.Size = new System.Drawing.Size(252, 162);
+			this.tableLayoutPanelColors.TabIndex = 3;
+			// 
+			// DiscreteColorPicker
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(304, 282);
+			this.Controls.Add(this.tableLayoutPanelColors);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonOK);
+			this.Controls.Add(this.buttonCancel);
+			this.DoubleBuffered = true;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "DiscreteColorPicker";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Select Color(s)";
+			this.Load += new System.EventHandler(this.DiscreteColorPicker_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.Button buttonOK;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanelColors;
+	}
+}
---
+++ Common/Controls/DiscreteColorPickerItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Common.Controls
+{
+	public partial class DiscreteColorPickerItem : UserControl
+	{
+		public DiscreteColorPickerItem()
+		{
+			InitializeComponent();
+		}
+
+		public bool Selected
+		{
+			get { return checkBoxSelected.Checked; }
+			set { checkBoxSelected.Checked = value; }
+		}
+
+		public Color Color
+		{
+			get { return panelColor.BackColor; }
+			set { panelColor.BackColor = value; }
+		}
+	}
+}
