30c35bdc4390559b254d654ba41e206b3e2ff670
Michael Sallaway <michael@sallaway.com>
2013-06-26 00:58:57 +1000

    Add discrete color support to the SetLevel and Pulse effects. They will render differently based on the effect that is being targeted.
    (Actually, the SetLevel doesn't do anything different I now realize; since there's only a single color, it will just pass it through. It's up to the filters to handle and ditch it if needed.)

--- Modules/Effect/SetLevel/SetLevel.cs
+++ Modules/Effect/SetLevel/SetLevel.cs
@@ -61,66 +61,11 @@ namespace VixenModules.Effect.SetLevel
 		// not a element, will recursively descend until we render its elements.
 		private void RenderNode(ElementNode node)
 		{
-			foreach(Element element in node) {
+			foreach(ElementNode elementNode in node.GetLeafEnumerator()) {
 				LightingValue lightingValue = new LightingValue(Color, (float)IntensityLevel);
 				IIntent intent = new LightingIntent(lightingValue, lightingValue, TimeSpan);
-				_elementData.AddIntentForElement(element.Id, intent, TimeSpan.Zero);
+				_elementData.AddIntentForElement(elementNode.Element.Id, intent, TimeSpan.Zero);
 			}
-			//// if this node is an RGB node, then it will know what to do with it (might render directly,
-			//// might be broken down into sub-elements, etc.) So just pass it off to that instead.
-			//if (node.Properties.Contains(SetLevelDescriptor._RGBPropertyId)) {
-			//    RenderRGB(node);
-			//} else {
-			//    if (node.IsLeaf) {
-			//        RenderMonochrome(node);
-			//    } else {
-			//        foreach (ElementNode child in node.Children)
-			//            RenderNode(child);
-			//    }
-			//}
 		}
-
-		//// renders a single command for each element in the given node with the specified monochrome level.
-		//private void RenderMonochrome(ElementNode node)
-		//{
-		//    // this is probably always going to be a single element for the given node, as
-		//    // we have iterated down to leaf nodes in RenderNode() above. May as well do
-		//    // it this way, though, in case something changes in future.
-		//    foreach (Element element in node.GetElementEnumerator()) {
-		//        //Command setLevelCommand = new Lighting.Monochrome.SetLevel(Level);
-		//        //CommandNode data = new CommandNode(setLevelCommand, TimeSpan.Zero, TimeSpan);
-
-		//        //IIntentModuleInstance intent = ApplicationServices.Get<IIntentModuleInstance>(_levelIntentId);
-		//        //intent.TimeSpan = TimeSpan;
-		//        //IntentNode data = new IntentNode(intent, TimeSpan.Zero);
-		//        if(element != null) {
-		//            //_elementData[element.Id] = new[] {data};
-		//            LightingValue lightingValue = new LightingValue(Color.White, IntensityLevel);
-		//            IIntent intent = new LightingLinearIntent(lightingValue, lightingValue, TimeSpan);
-		//            _elementData.AddIntentForElement(element.Id, intent, TimeSpan.Zero);
-		//        }
-		//    }
-		//}
-
-		//private void RenderRGB(ElementNode node)
-		//{
-		//    //// get the RGB property for the element, and get it to render the data for us
-		//    //RGBModule rgbProperty = node.Properties.Get(SetLevelDescriptor._RGBPropertyId) as RGBModule;
-		//    //ElementCommands rgbData = rgbProperty.RenderColorToCommands(Color, Level);
-
-		//    //// iterate through the rendered commands, adjust them to fit our times, and add them to our rendered data
-		//    //foreach (KeyValuePair<Guid, Command[]> kvp in rgbData) {
-		//    //    foreach (Command c in kvp.Value) {
-		//    //        //TODO
-		//    //        //CommandNode newCommandNode = new CommandNode(c, TimeSpan.Zero, TimeSpan);
-		//    //        //_elementData.AddCommandNodeForElement(kvp.Key, newCommandNode);
-		//    //    }
-		//    //}
-		//    foreach(Element element in node.GetElementEnumerator()) {
-		//        LightingValue lightingValue = new LightingValue(Color, IntensityLevel);
-		//        IIntent intent = new LightingLinearIntent(lightingValue, lightingValue, TimeSpan);
-		//        _elementData.AddIntentForElement(element.Id, intent, TimeSpan.Zero);
-		//    }
-		//}
 	}
 }
