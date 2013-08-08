a09974b73d91ebf7781f684c206b81b4574ac3c2
Derek Backus <derek@kiwimill.com>
2013-08-07 22:55:05 -0400

    Initial commit of new separate preview. Not hooked up yet.

---
+++ Modules/Preview/VixenPreview/GDIPreview/GDIControl.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using Vixen.Data.Value;
+using Vixen.Execution.Context;
+using Vixen.Sys;
+using VixenModules.Preview.VixenPreview.Shapes;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+using VixenModules.Property.Location;
+
+namespace VixenModules.Preview.VixenPreview
+{
+	public partial class GDIControl : UserControl
+	{
+		private Image _background = null;
+		private Bitmap _backgroundAlphaImage = null;
+		private int _backgroundAlpha = 0;
+		private Stopwatch renderTimer = new Stopwatch();
+		private FastPixel.FastPixel fastPixel;
+		BufferedGraphicsContext graphicsContext;
+		BufferedGraphics backBuffer;
+
+		public GDIControl()
+		{
+			InitializeComponent();
+			//SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.DoubleBuffer, true);
+
+			graphicsContext = BufferedGraphicsManager.Current;
+			AllocateGraphicsBuffer();
+		}
+
+		public Image Background
+		{
+			get
+			{
+				return _background;
+			}
+			set
+			{
+				_background = value;
+				CreateAlphaBackground();
+				fastPixel = new FastPixel.FastPixel(_backgroundAlphaImage.Width, _backgroundAlphaImage.Height);
+			}
+		}
+
+		public int BackgroundAlpha 
+		{
+			get
+			{
+				return _backgroundAlpha;
+			}
+			set
+			{
+				_backgroundAlpha = value;
+				CreateAlphaBackground();
+			}
+		}
+
+		public long RenderTime 
+		{ 
+			get
+			{
+				return renderTimer.ElapsedMilliseconds;
+			}
+		}
+
+		public FastPixel.FastPixel FastPixel
+		{
+			get
+			{
+				return fastPixel;
+			}
+			set
+			{
+				fastPixel = value;
+			}
+		}
+
+		public void CreateAlphaBackground()
+		{
+			if (_background != null)
+			{
+				_backgroundAlphaImage = new Bitmap(_background.Width, _background.Height, PixelFormat.Format32bppPArgb);
+				Graphics gfx = Graphics.FromImage(_backgroundAlphaImage);
+				using (SolidBrush brush = new SolidBrush(Color.FromArgb(255 - BackgroundAlpha, 0, 0, 0)))
+				{
+					gfx.DrawImage(_background, 0, 0, _background.Width, _background.Height);
+					gfx.FillRectangle(brush, 0, 0, _backgroundAlphaImage.Width, _backgroundAlphaImage.Height);
+				}
+				gfx.Dispose();
+			}
+			else
+			{
+				_backgroundAlphaImage = new Bitmap(Width, Height, PixelFormat.Format32bppPArgb);
+				Graphics gfx = Graphics.FromImage(_backgroundAlphaImage);
+				gfx.Clear(Color.DeepPink);
+				gfx.Dispose();
+			}
+		}
+
+		private void GDIControl_Resize(object sender, EventArgs e)
+		{
+			AllocateGraphicsBuffer();
+		}
+
+		public void RenderImage()
+		{
+			if (_backgroundAlphaImage != null)
+			{
+				backBuffer.Graphics.DrawImage(fastPixel.Bitmap, 0, 0, fastPixel.Bitmap.Width, fastPixel.Bitmap.Height);
+			}
+			else
+			{
+				backBuffer.Graphics.Clear(Color.Black);
+			}
+
+			if (!this.Disposing && graphicsContext != null)
+				backBuffer.Render(Graphics.FromHwnd(this.Handle));
+
+			renderTimer.Stop();
+		}
+
+		private void AllocateGraphicsBuffer()
+		{
+			if (backBuffer != null)
+				backBuffer.Dispose();
+
+			graphicsContext.MaximumBuffer =
+				  new Size(this.Width + 1, this.Height + 1);
+
+			backBuffer =
+				graphicsContext.Allocate(this.CreateGraphics(),
+												ClientRectangle);
+		}
+
+		public void BeginUpdate()
+		{
+			renderTimer.Restart();
+			fastPixel.CloneBitmap(_backgroundAlphaImage);
+			fastPixel.Lock();
+		}
+
+		public void EndUpdate()
+		{
+			fastPixel.Unlock(true);
+		}
+
+		public void SetPixel(int x, int y, Color color)
+		{
+			fastPixel.SetPixel(new Point(x, y), color);
+		}
+
+	}
+}
---
+++ Modules/Preview/VixenPreview/GDIPreview/GDIPreviewForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using Vixen.Data.Value;
+using Vixen.Execution.Context;
+using Vixen.Sys;
+using VixenModules.Preview.VixenPreview.Shapes;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+using VixenModules.Property.Location;
+
+namespace VixenModules.Preview.VixenPreview
+{
+	public partial class GDIPreviewForm : Form
+	{
+		public GDIPreviewForm(VixenPreviewData data)
+		{
+			InitializeComponent();
+			Data = data;
+		}
+
+		public VixenPreviewData Data { get; set; }
+
+		private Stopwatch lastUpdate = new Stopwatch();
+		public void Update(ElementIntentStates elementStates)
+		{
+			gdiControl.BeginUpdate();
+
+			lastUpdate.Stop();
+			if (lastUpdate.ElapsedMilliseconds > 500)
+			{
+				toolStripStatusLabel2.Text = lastUpdate.ElapsedMilliseconds.ToString();
+			}
+			lastUpdate.Restart();
+
+			CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+			elementStates.AsParallel().WithCancellation(tokenSource.Token).ForAll(channelIntentState =>
+			{
+				var elementId = channelIntentState.Key;
+				Element element = VixenSystem.Elements.GetElement(elementId);
+				if (element != null)
+				{
+					ElementNode node = VixenSystem.Elements.GetElementNodeForElement(element);
+					if (node != null)
+					{
+						//Color pixColor = Vixen.Intent.ColorIntent.GetAlphaColorForIntents(channelIntentState.Value);
+
+						List<PreviewPixel> pixels;
+						if (NodeToPixel.TryGetValue(node, out pixels))
+						{
+							foreach (PreviewPixel pixel in pixels)
+							{
+								pixel.Draw(gdiControl.FastPixel, channelIntentState.Value);
+								//pixel.Draw(gdiControl.FastPixel, pixColor);
+								//gdiControl.SetPixel(pixel.X, pixel.Y, pixColor);
+							}
+						}
+
+						//List<PreviewPixel> pixels;
+						//if (NodeToPixel.TryGetValue(node, out pixels))
+						//{
+						//    foreach (PreviewPixel pixel in pixels)
+						//    {
+						//        pixel.Draw(gdiControl.FastPixel, channelIntentState.Value);
+						//    }
+						//}
+					}
+				}
+			});
+
+			gdiControl.EndUpdate();
+
+			gdiControl.RenderImage();
+			toolStripStatusLabel1.Text = gdiControl.RenderTime.ToString() + "ms";
+		}
+
+		private void GDIPreviewForm_Load(object sender, EventArgs e)
+		{
+			//gdiControl.Background = Bitmap.FromFile("C:\\SkyDrive\\Christmas Lights\\Hillary 2012 1500x489.jpg");
+			Reload();
+		}
+
+
+		public ConcurrentDictionary<ElementNode, List<PreviewPixel>> NodeToPixel = new ConcurrentDictionary<ElementNode, List<PreviewPixel>>();
+		public List<DisplayItem> DisplayItems
+		{
+			get
+			{
+				if (Data != null)
+				{
+					return Data.DisplayItems;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+
+		public void Reload()
+		{
+			if (NodeToPixel == null)
+				throw new System.ArgumentException("PreviewBase.NodeToPixel == null");
+
+			NodeToPixel.Clear();
+
+			if (DisplayItems == null)
+				throw new System.ArgumentException("DisplayItems == null");
+
+			if (DisplayItems != null)
+				foreach (DisplayItem item in DisplayItems)
+				{
+					if (item.Shape.Pixels == null)
+						throw new System.ArgumentException("item.Shape.Pixels == null");
+
+					foreach (PreviewPixel pixel in item.Shape.Pixels)
+					{
+						if (pixel.Node != null)
+						{
+							List<PreviewPixel> pixels;
+							if (NodeToPixel.TryGetValue(pixel.Node, out pixels))
+							{
+								if (!pixels.Contains(pixel))
+								{
+									pixels.Add(pixel);
+								}
+							}
+							else
+							{
+								pixels = new List<PreviewPixel>();
+								pixels.Add(pixel);
+								NodeToPixel.TryAdd(pixel.Node, pixels);
+							}
+						}
+					}
+				}
+
+			if (System.IO.File.Exists(Data.BackgroundFileName))
+				gdiControl.Background = Image.FromFile(Data.BackgroundFileName);
+			else
+				gdiControl.Background = null;
+		}
+
+	}
+}
