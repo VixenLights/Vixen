d0cfc0bcf571a2a0cdb65cc7c89b7b867381cdde
Jeff Uchitjil <jeffu@kb9kld.org>
2013-09-08 17:51:06 -0500

    Reuse the collection to reduce object creation. Method is called for every state update.

--- Vixen.System/Sys/IIntentStates.cs
+++ Vixen.System/Sys/IIntentStates.cs
@@ -5,6 +5,8 @@ namespace Vixen.Sys
 	public interface IIntentStates : IEnumerable<IIntentState>
 	{
 		void AddIntentState(IIntentState intentState);
+		void AddRangeIntentState(IEnumerable<IIntentState> intentStates);
+		void ClearStates();
 		IIntentState this[int index] { get; set; }
 		int Count { get; }
 	}
