a0b5254b9698fc8caf5eba5f035eba13c048492d
Michael Sallaway <michael@sallaway.com>
2013-09-18 03:31:41 +1000

    Fix up a few things with the last merge:  reformat the solution file (remove a bunch of other targets, and revert to VS2010), and reference the common mrdsio.dll in the RDS controller.

--- Modules/Output/RDSController/NativeMethods.cs
+++ Modules/Output/RDSController/NativeMethods.cs
@@ -9,22 +9,18 @@ namespace VixenModules.Output.CommandController
 	internal static class NativeMethods
 	{
 		#region MRDSIO
-		[DllImport("inpout32.dll", EntryPoint = "Inp32")]
-		public static extern void Input(int adress);
-		[DllImport("inpout32.dll", EntryPoint = "Out32")]
-		public static extern void Output(int adress, int Len);
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		public static extern void ConnectionSetup(int ConMode, int PortNum, bool Bidirectional, bool Slow);
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		public static extern string Version();
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		public static extern bool Connect();
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		public static extern void Disconnect();
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		public static extern bool Send(int Len, byte[] Data);
 		[return: MarshalAs(UnmanagedType.Struct)]
-		[DllImport("mrdsio.dll")]
+		[DllImport("Common\\mrdsio.dll")]
 		static extern TData Receive(int Adr, int Len);
 		#endregion
 	
