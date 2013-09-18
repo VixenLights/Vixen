eb11f663e60eccddfa97cf2116d6b36b3b7649ac
Darren McDaniel <gizmohd@gmail.com>
2013-09-18 01:25:50 -0400

    Speed up initial load of system rows in editor by turning off the Layout events that are fired for every element and row that is added,  instead fire the layout routine after all the rows have been added to the form.

--- Common/Controls/TimeLineControl/RowList.cs
+++ Common/Controls/TimeLineControl/RowList.cs
@@ -23,9 +23,7 @@ namespace Common.Controls.Timeline
 			SetStyle(ControlStyles.UserPaint, true);
 			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 			SetStyle(ControlStyles.ResizeRedraw, true);
-			Row.RowToggled += RowLabelChangedHandler;
-			Row.RowHeightChanged += RowLabelChangedHandler;
-			Row.RowHeightResized += RowLabelResizedHandler;
+			EnableDisableHandlers(true);
 		}
 
 		#region Properties
@@ -100,7 +98,21 @@ namespace Common.Controls.Timeline
 		#endregion
 
 		#region Methods
-
+		public void EnableDisableHandlers(bool enabled = true)
+		{
+			if (enabled) {
+				Row.RowToggled -= RowLabelChangedHandler;
+				Row.RowHeightChanged -= RowLabelChangedHandler;
+				Row.RowHeightResized -= RowLabelResizedHandler;
+				Row.RowToggled += RowLabelChangedHandler;
+				Row.RowHeightChanged += RowLabelChangedHandler;
+				Row.RowHeightResized += RowLabelResizedHandler;
+			} else {
+				Row.RowToggled -= RowLabelChangedHandler;
+				Row.RowHeightChanged -= RowLabelChangedHandler;
+				Row.RowHeightResized -= RowLabelResizedHandler;
+			}
+		}
 		private delegate void AddRowLabelDelegate(RowLabel trl);
 
 		public void AddRowLabel(RowLabel trl)
