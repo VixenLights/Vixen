9cebe9c0d15b20d041afd2ecf412db354a53a954
Michael Sallaway <michael@sallaway.com>
2013-09-24 02:49:34 +1000

    3.0.8 release changes.

--- Application/VixenApplication/Properties/AssemblyInfo.cs
+++ Application/VixenApplication/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.8.5")]
-[assembly: AssemblyFileVersion("3.0.8.5")]
+[assembly: AssemblyVersion("3.0.8.50")]
+[assembly: AssemblyFileVersion("3.0.8.50")]
