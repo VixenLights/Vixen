29aac20dd61ca18e26cce67626e6df1247cc3e0f
eberletj <eberletj@gmail.com>
2013-10-15 10:52:56 -0500

    created virtual Helix controller for exporting of Vixen 3 data to Vixen 2 data

---
+++ Modules/Output/HelixController/HelixDataPolicy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Data.Policy;
+using Vixen.Sys;
+using Vixen.Data.Evaluator;
+using Vixen.Data.Combinator._8Bit;
+
+namespace VixenModules.Output.HelixController
+{
+	class HelixDataPolicy : ControllerDataPolicy
+	{
+		protected override IEvaluator GetEvaluator()
+		{
+			return new _8BitEvaluator();
+		}
+
+		protected override ICombinator GetCombinator()
+		{
+			return new _8BitHighestWinsCombinator();
+		}
+	}
+}
---
+++ Modules/Output/HelixController/HelixDataPolicyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vixen.Sys;
+
+namespace VixenModules.Output.HelixController
+{
+	class HelixDataPolicyFactory : IDataPolicyFactory
+	{
+		public IDataPolicy CreateDataPolicy()
+		{
+			return new HelixDataPolicy();
+		}
+	}
+}
