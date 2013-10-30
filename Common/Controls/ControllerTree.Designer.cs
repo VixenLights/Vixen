cf43605286f6ef7234676abb76e79f64cd69873e
Michael Sallaway <michael@sallaway.com>
2013-10-30 21:02:12 +1000

    Added a "controller tree" control, similar to the element tree control, to show controllers and channels in a treeview display.

---
+++ Common/Controls/ControllerTree.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Common.Controls
+{
+	partial class ControllerTree
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControllerTree));
+			this.treeIconsImageList = new System.Windows.Forms.ImageList(this.components);
+			this.contextMenuStripTreeView = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.channelCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.treeview = new Common.Controls.MultiSelectTreeview();
+			this.contextMenuStripTreeView.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// treeIconsImageList
+			// 
+			this.treeIconsImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeIconsImageList.ImageStream")));
+			this.treeIconsImageList.TransparentColor = System.Drawing.Color.Transparent;
+			this.treeIconsImageList.Images.SetKeyName(0, "Group");
+			this.treeIconsImageList.Images.SetKeyName(1, "GreyBall");
+			this.treeIconsImageList.Images.SetKeyName(2, "RedBall");
+			this.treeIconsImageList.Images.SetKeyName(3, "GreenBall");
+			this.treeIconsImageList.Images.SetKeyName(4, "YellowBall");
+			this.treeIconsImageList.Images.SetKeyName(5, "BlueBall");
+			this.treeIconsImageList.Images.SetKeyName(6, "WhiteBall");
+			// 
+			// contextMenuStripTreeView
+			// 
+			this.contextMenuStripTreeView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configureToolStripMenuItem,
+            this.channelCountToolStripMenuItem,
+            this.renameToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+			this.contextMenuStripTreeView.Name = "contextMenuStripTreeView";
+			this.contextMenuStripTreeView.Size = new System.Drawing.Size(155, 92);
+			this.contextMenuStripTreeView.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStripTreeView_Opening);
+			// 
+			// configureToolStripMenuItem
+			// 
+			this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+			this.configureToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+			this.configureToolStripMenuItem.Text = "Configure";
+			this.configureToolStripMenuItem.Click += new System.EventHandler(this.configureToolStripMenuItem_Click);
+			// 
+			// channelCountToolStripMenuItem
+			// 
+			this.channelCountToolStripMenuItem.Name = "channelCountToolStripMenuItem";
+			this.channelCountToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+			this.channelCountToolStripMenuItem.Text = "Channel Count";
+			this.channelCountToolStripMenuItem.Click += new System.EventHandler(this.channelCountToolStripMenuItem_Click);
+			// 
+			// renameToolStripMenuItem
+			// 
+			this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+			this.renameToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+			this.renameToolStripMenuItem.Text = "Rename";
+			this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
+			// 
+			// deleteToolStripMenuItem
+			// 
+			this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+			this.deleteToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+			this.deleteToolStripMenuItem.Text = "Delete";
+			this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+			// 
+			// treeview
+			// 
+			this.treeview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeview.ContextMenuStrip = this.contextMenuStripTreeView;
+			this.treeview.Cursor = System.Windows.Forms.Cursors.Default;
+			this.treeview.CustomDragCursor = null;
+			this.treeview.DragDefaultMode = System.Windows.Forms.DragDropEffects.None;
+			this.treeview.DragDestinationNodeBackColor = System.Drawing.SystemColors.Highlight;
+			this.treeview.DragDestinationNodeForeColor = System.Drawing.SystemColors.HighlightText;
+			this.treeview.DragSourceNodeBackColor = System.Drawing.SystemColors.ControlLight;
+			this.treeview.DragSourceNodeForeColor = System.Drawing.SystemColors.ControlText;
+			this.treeview.HideSelection = false;
+			this.treeview.ImageIndex = 0;
+			this.treeview.ImageList = this.treeIconsImageList;
+			this.treeview.Location = new System.Drawing.Point(0, 0);
+			this.treeview.Name = "treeview";
+			this.treeview.SelectedImageIndex = 0;
+			this.treeview.SelectedNodes = ((System.Collections.Generic.List<System.Windows.Forms.TreeNode>)(resources.GetObject("treeview.SelectedNodes")));
+			this.treeview.Size = new System.Drawing.Size(200, 400);
+			this.treeview.TabIndex = 13;
+			this.treeview.UsingCustomDragCursor = false;
+			this.treeview.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeview_KeyDown);
+			// 
+			// ControllerTree
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.treeview);
+			this.Name = "ControllerTree";
+			this.Size = new System.Drawing.Size(200, 400);
+			this.Load += new System.EventHandler(this.ControllerTree_Load);
+			this.contextMenuStripTreeView.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private MultiSelectTreeview treeview;
+		private System.Windows.Forms.ImageList treeIconsImageList;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStripTreeView;
+		private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem channelCountToolStripMenuItem;
+	}
+}
---
+++ Common/Controls/ControllerTree.cs
@@ -0,0 +1,373 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+using Vixen.Data.Flow;
+using Vixen.Module.Property;
+using Vixen.Services;
+using Vixen.Sys;
+using Vixen.Sys.Output;
+
+namespace Common.Controls
+{
+	public partial class ControllerTree : UserControl
+	{
+		// sets of data to keep track of which items in the treeview are open, selected, visible etc., so that
+		// when we reload the tree, we can keep it looking relatively consistent with what the user had before.
+		private HashSet<string> _expandedNodes; // TreeNode paths that are expanded
+		private HashSet<string> _selectedNodes; // TreeNode paths that are selected
+		private List<string> _topDisplayedNodes; // TreeNode paths that are at the top of the view. Should only
+		// need one, but will have multiple in case the top node is deleted.
+		private static NLog.Logger Logging = NLog.LogManager.GetCurrentClassLogger();
+
+		public ControllerTree()
+		{
+			InitializeComponent();
+		}
+
+		private void ControllerTree_Load(object sender, EventArgs e)
+		{
+			if (!(DesignMode || LicenseManager.UsageMode == LicenseUsageMode.Designtime)) {
+				PopulateControllerTree();
+			}
+		}
+
+
+		#region Tree view population
+
+		public void PopulateControllerTree()
+		{
+			// save metadata that is currently in the treeview
+			_expandedNodes = new HashSet<string>();
+			_selectedNodes = new HashSet<string>();
+			_topDisplayedNodes = new List<string>();
+
+			SaveTreeNodeState(treeview.Nodes);
+			SaveTreeNodeTopVisible();
+
+			// clear the treeview, and repopulate it
+			treeview.BeginUpdate();
+			treeview.Nodes.Clear();
+			treeview.SelectedNodes.Clear();
+
+			foreach (IControllerDevice controller in VixenSystem.OutputControllers) {
+				AddControllerToTree(treeview.Nodes, controller);
+			}
+
+			// go through all the data we saved, and try to update the treeview to look
+			// like it used to (expanded nodes, selected nodes, node at the top)
+
+			foreach (string node in _expandedNodes) {
+				TreeNode resultNode = FindNodeInTreeAtPath(treeview, node);
+
+				if (resultNode != null) {
+					resultNode.Expand();
+				}
+			}
+
+			foreach (string node in _selectedNodes) {
+				TreeNode resultNode = FindNodeInTreeAtPath(treeview, node);
+
+				if (resultNode != null) {
+					treeview.AddSelectedNode(resultNode);
+				}
+			}
+
+			treeview.EndUpdate();
+
+			// see stackoverflow.com/questions/626315/winforms-listview-remembering-scrolled-location-on-reload .
+			// we can only set the topNode after EndUpdate(). Also, it might throw an exception -- weird?
+			foreach (string node in _topDisplayedNodes) {
+				TreeNode resultNode = FindNodeInTreeAtPath(treeview, node);
+
+				if (resultNode != null) {
+					try {
+						treeview.TopNode = resultNode;
+					} catch (Exception) {
+						 Logging.Warn("exception caught trying to set TopNode.");
+					}
+					break;
+				}
+			}
+		}
+
+		private string GenerateTreeNodeFullPath(TreeNode node, string separator)
+		{
+			string result = node.Name;
+			TreeNode parent = node.Parent;
+			while (parent != null) {
+				result = parent.Name + separator + result;
+				parent = parent.Parent;
+			}
+
+			return result;
+		}
+
+		private TreeNode FindNodeInTreeAtPath(TreeView tree, string path)
+		{
+			string[] subnodes = path.Split(new string[] { tree.PathSeparator }, StringSplitOptions.None);
+			TreeNodeCollection searchNodes = tree.Nodes;
+			TreeNode currentNode = null;
+			foreach (string search in subnodes) {
+				bool found = false;
+				foreach (TreeNode tn in searchNodes) {
+					if (tn.Name == search) {
+						found = true;
+						currentNode = tn;
+						searchNodes = tn.Nodes;
+						break;
+					}
+				}
+				if (!found) {
+					currentNode = null;
+					break;
+				}
+			}
+
+			return currentNode;
+		}
+
+		private void SaveTreeNodeState(TreeNodeCollection collection)
+		{
+			foreach (TreeNode tn in collection) {
+				if (tn.IsExpanded) {
+					_expandedNodes.Add(GenerateTreeNodeFullPath(tn, treeview.PathSeparator));
+				}
+
+				if (treeview.SelectedNodes.Contains(tn)) {
+					_selectedNodes.Add(GenerateTreeNodeFullPath(tn, treeview.PathSeparator));
+				}
+
+				SaveTreeNodeState(tn.Nodes);
+			}
+		}
+
+		private void SaveTreeNodeTopVisible()
+		{
+			// this will iterate through all root nodes -- starting with the topmost visible
+			// node -- adding their path to a list in order. Later on, when refreshing the tree,
+			// we can try them in order to place at the top of the display. We should only
+			// need a single node, but in case the top node gets deleted (or the top few),
+			// we keep a list of 'preferred' nodes.
+			if (treeview.Nodes.Count > 0) {
+				TreeNode current = treeview.TopNode;
+				while (current != null) {
+					_topDisplayedNodes.Add(GenerateTreeNodeFullPath(current, treeview.PathSeparator));
+					current = current.NextNode;
+				}
+			}
+		}
+
+		private void AddControllerToTree(TreeNodeCollection collection, IControllerDevice controller)
+		{
+			TreeNode controllerNode = new TreeNode();
+
+			controllerNode.Name = controller.Id.ToString();
+			controllerNode.Text = controller.Name;
+			controllerNode.Tag = controller;
+
+			controllerNode.ImageKey = controllerNode.SelectedImageKey = "Group";
+
+			collection.Add(controllerNode);
+
+			for (int i = 0; i < controller.OutputCount; i++) {
+				TreeNode channelNode = new TreeNode();
+				channelNode.Name = controllerNode.Name + "-#" + (i+1);
+				channelNode.Text = "#" + (i+1);
+				channelNode.Tag = i;
+
+				IDataFlowComponentReference source = controller.Outputs[i].Source;
+
+				if (source == null) {
+					channelNode.ImageKey = channelNode.SelectedImageKey = "WhiteBall";
+				} else if (source.Component == null || source.OutputIndex < 0) {
+					channelNode.ImageKey = channelNode.SelectedImageKey = "GreyBall";
+				} else {
+					channelNode.ImageKey = channelNode.SelectedImageKey = "GreenBall";
+				}
+
+				controllerNode.Nodes.Add(channelNode);
+			}
+		}
+
+		#endregion
+
+
+
+		#region Events
+
+		public List<TreeNode> SelectedTreeNodes
+		{
+			get { return treeview.SelectedNodes; }
+		}
+
+		public IEnumerable<IControllerDevice> SelectedControllers
+		{
+			get
+			{
+				return treeview.SelectedNodes.Select(node => node.Tag).OfType<IControllerDevice>();
+			}
+		}
+
+
+
+		public event EventHandler treeviewDeselected
+		{
+			add { treeview.Deselected += value; }
+			remove { treeview.Deselected -= value; }
+		}
+
+		public event TreeViewEventHandler treeviewAfterSelect
+		{
+			add { treeview.AfterSelect += value; }
+			remove { treeview.AfterSelect -= value; }
+		}
+
+		public event EventHandler ControllersChanged;
+
+		public void OnControllersChanged(EventArgs e = null)
+		{
+			if (e == null)
+				e = EventArgs.Empty;
+			EventHandler handler = ControllersChanged;
+			if (handler != null) handler(this, e);
+		}
+
+		#endregion
+
+
+
+
+		#region Helper functions
+
+		public bool RenameControllerWithPrompt(IControllerDevice outputController)
+		{
+			using (TextDialog textDialog = new TextDialog("Controller Name?", "Controller Name", outputController.Name, true)) {
+				if (textDialog.ShowDialog() == DialogResult.OK) {
+					if (textDialog.Response != string.Empty) {
+						outputController.Name = textDialog.Response;
+						OnControllersChanged();
+						PopulateControllerTree();
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public bool DeleteControllersWithPrompt(IEnumerable<IControllerDevice> controllers)
+		{
+			string message, title;
+			if (controllers.Count() > 1) {
+				message = "Are you sure you want to delete the selected controllers?";
+				title = "Delete Controllers?";
+			} else {
+				message = "Are you sure you want to delete the selected controller?";
+				title = "Delete Controller?";
+			}
+
+			if (controllers.Count() > 0) {
+				if (MessageBox.Show(message, title, MessageBoxButtons.OKCancel) == DialogResult.OK) {
+					foreach (OutputController oc in controllers) {
+						VixenSystem.OutputControllers.Remove(oc);
+					}
+					OnControllersChanged();
+					PopulateControllerTree();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool ConfigureController(IControllerDevice controller)
+		{
+			bool result = false;
+			if (controller.HasSetup) {
+				result = controller.Setup();
+				if (result)
+					OnControllersChanged();
+			}
+			return result;
+		}
+
+		public bool SetControllerOutputCount(IControllerDevice controller)
+		{
+			using (NumberDialog nd = new NumberDialog("Controller Output Count", "Outputs on this controller?", controller.OutputCount)) {
+				if (nd.ShowDialog() == DialogResult.OK) {
+					// TODO: blergh, dodgy hack
+					(controller as OutputController).OutputCount = nd.Value;
+					OnControllersChanged();
+					PopulateControllerTree();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public void ClearSelectedNodes()
+		{
+			treeview.ClearSelectedNodes();
+		}
+
+
+		#endregion
+
+
+
+		#region Context Menus
+
+		private void contextMenuStripTreeView_Opening(object sender, CancelEventArgs e)
+		{
+			e.Cancel = (SelectedControllers.Count() == 0);
+
+			configureToolStripMenuItem.Enabled = (SelectedControllers.Count() == 1);
+			channelCountToolStripMenuItem.Enabled = (SelectedControllers.Count() == 1);
+			renameToolStripMenuItem.Enabled = (SelectedControllers.Count() == 1);
+			deleteToolStripMenuItem.Enabled = (SelectedControllers.Count() > 0);
+		}
+
+
+
+		private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			DeleteControllersWithPrompt(SelectedControllers);
+		}
+
+
+		private void renameToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			RenameControllerWithPrompt(SelectedControllers.First());
+		}
+
+		private void configureToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			ConfigureController(SelectedControllers.First());
+		}
+
+		private void channelCountToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SetControllerOutputCount(SelectedControllers.First());
+		}
+
+
+		#endregion
+
+
+
+		private void treeview_KeyDown(object sender, KeyEventArgs e)
+		{
+			// do our own deleting of items here
+			if (e.KeyCode == Keys.Delete) {
+				if (SelectedControllers.Count() > 0) {
+					DeleteControllersWithPrompt(SelectedControllers);
+				}
+			}
+		}
+
+	}
+}
