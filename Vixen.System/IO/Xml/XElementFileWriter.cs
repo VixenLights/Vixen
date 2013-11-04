62f9439745fa851e0a86e9537a0e5a899f13ad13
Darren McDaniel <gizmohd@gmail.com>
2013-11-04 07:55:04 -0500

    Fix GPF fault when editing using the new configuration window.   Does a little sanity checks on the E131 controller..  And adds a check to make sure the file is not locked by another process (git?) before saving.  If file is locked, then wait 1/4 second to try again until it's not in use any longer.

--- Vixen.System/IO/Xml/XElementFileWriter.cs
+++ Vixen.System/IO/Xml/XElementFileWriter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Xml.Linq;
 
 namespace Vixen.IO.Xml
@@ -7,6 +9,10 @@ namespace Vixen.IO.Xml
 	{
 		public void WriteFile(string filePath, XElement content)
 		{
+		    while (IsFileLocked(filePath))
+		    {
+		        System.Threading.Thread.Sleep(250);
+		    }
 			content.Save(filePath);
 		}
 
@@ -14,6 +20,22 @@ namespace Vixen.IO.Xml
 		{
 			if (!(content is XElement)) throw new InvalidOperationException("Content mst be an XElement.");
 			WriteFile(filePath, (XElement) content);
-		}
-	}
+        }
+        bool IsFileLocked(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite)) { }
+                }
+            }
+            catch (IOException e)
+            {
+                return (Marshal.GetHRForException(e) & 0xFFFF) == 32;
+            }
+
+            return false;
+        }
+    }
 }
