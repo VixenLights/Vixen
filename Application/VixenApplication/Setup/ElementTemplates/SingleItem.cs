d6177fd291886da7726cac83921ed44101bbf1d3
Michael Sallaway <michael@sallaway.com>
2013-11-19 03:16:58 +1000

    Setup: Add new template for a single item.

---
+++ Application/VixenApplication/Setup/ElementTemplates/SingleItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Common.Controls;
+using Vixen.Rule;
+using Vixen.Services;
+using Vixen.Sys;
+
+namespace VixenApplication.Setup.ElementTemplates
+{
+	class SingleItem : IElementTemplate
+	{
+		private string itemName;
+
+		public SingleItem()
+		{
+			itemName = "New Item";
+		}
+
+		public string TemplateName
+		{
+			get { return "Single Item"; }
+		}
+
+		public bool SetupTemplate(IEnumerable<ElementNode> selectedNodes = null)
+		{
+			using (TextDialog td = new TextDialog("New Element Name?", "Element Name", itemName, true)) {
+				DialogResult dr = td.ShowDialog();
+				if (dr == DialogResult.OK) {
+					itemName = td.Response;
+					if (itemName == "") {
+						itemName = "New Item";
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public IEnumerable<ElementNode> GenerateElements(IEnumerable<ElementNode> selectedNodes = null)
+		{
+			ElementNode newNode = ElementNodeService.Instance.CreateSingle(null, itemName);
+			return new[] {newNode};
+		}
+	}
+}
