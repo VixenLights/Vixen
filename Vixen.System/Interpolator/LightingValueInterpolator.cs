7d8bd3db09aa16c4be9e347bd315ef9841f8e36e
Michael Sallaway <michael@sallaway.com>
2013-12-02 01:38:45 +1000

    Interpolate HSV colors properly -- wrap around the 360 degree point on the color wheel, rather than doing all transitions within the 0 -> 360 bound.
    (note: I was pretty damn sleepy when writing this, so I suspect it's inefficient. Feel free to optimize it if there's problems with it.)

--- Vixen.System/Interpolator/LightingValueInterpolator.cs
+++ Vixen.System/Interpolator/LightingValueInterpolator.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using Vixen.Data.Value;
 using Vixen.Sys;
 
@@ -9,10 +10,20 @@ namespace Vixen.Interpolator
 	{
 		protected override LightingValue InterpolateValue(double percent, LightingValue startValue, LightingValue endValue)
 		{
-			double h = (startValue.hsv.H + (endValue.hsv.H - startValue.hsv.H) * percent);
+			// TODO: this could probably be performance optimized a bit; might not matter next year if we rip the guts out of it
+			double closestEndHue;
+			if (endValue.hsv.H - startValue.hsv.H > 0.5)
+				closestEndHue = endValue.hsv.H - 1.0;
+			else if (endValue.hsv.H - startValue.hsv.H < -0.5)
+				closestEndHue = endValue.hsv.H + 1.0;
+			else
+				closestEndHue = endValue.hsv.H;
+
+			double h = (startValue.hsv.H + (closestEndHue - startValue.hsv.H) * percent);
 			double s = (startValue.hsv.S + (endValue.hsv.S - startValue.hsv.S) * percent);
 			double v = (startValue.hsv.V + (endValue.hsv.V - startValue.hsv.V) * percent);
-			return new LightingValue(h, s, v);
+
+			return new LightingValue((h + 1.0) % 1.0, s, v);
 		}
 	}
 }
