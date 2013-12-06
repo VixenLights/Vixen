cbb7b4de777cb87cf4651649b8b1ab2b12274700
Darren McDaniel <gizmohd@gmail.com>
2013-12-05 21:45:50 -0500

    Add global exception handler...

--- Application/VixenApplication/Program.cs
+++ Application/VixenApplication/Program.cs
@@ -7,27 +7,52 @@ namespace VixenApplication
 {
 	internal static class Program
 	{
+		private static NLog.Logger Logging = NLog.LogManager.GetCurrentClassLogger();
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		private static void Main()
 		{
-			bool result;
-			var mutex = new System.Threading.Mutex(true, "Vixen3RunningInstance", out result);
 
-			if (!result) {
-				MessageBox.Show("Another instance is already running; please close that one before trying to start another.",
-				                "Vixen 3 already active", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-				return;
+			try
+			{
+				AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+				Application.ThreadException += Application_ThreadException;
+			 
+				bool result;
+				var mutex = new System.Threading.Mutex(true, "Vixen3RunningInstance", out result);
+
+				if (!result)
+				{
+					MessageBox.Show("Another instance is already running; please close that one before trying to start another.",
+									"Vixen 3 already active", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new VixenApplication());
+
+				// mutex shouldn't be released - important line
+				GC.KeepAlive(mutex);
 			}
+			catch (Exception ex)
+			{
+				Logging.Fatal(ex);
+			}
+		}
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new VixenApplication());
+		static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		{
+			Logging.Error(e.Exception);
+		}
 
-			// mutex shouldn't be released - important line
-			GC.KeepAlive(mutex);
+		static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs args)
+		{ 
+			var e = (Exception)args.ExceptionObject;
+			Logging.Error(e);
 		}
 	}
 }
