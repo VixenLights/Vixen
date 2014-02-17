9a24adab5173c555af0088303e4e2a6636f0fdad
Jeff Uchitjil <jeffu@kb9kld.org>
2014-02-17 12:49:54 -0600

    Put back some global logging messages that where lost in the big merge to mainline.

--- Application/VixenApplication/Program.cs
+++ Application/VixenApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace VixenApplication
@@ -8,6 +9,8 @@ namespace VixenApplication
 	internal static class Program
 	{
 		private static NLog.Logger Logging = NLog.LogManager.GetCurrentClassLogger();
+		private const string ErrorMsg = "An application error occurred. Please contact the Vixen Dev Team " +
+									"with the following information:\n\n";
 
 		/// <summary>
 		/// The main entry point for the application.
@@ -22,7 +25,7 @@ namespace VixenApplication
 				Application.ThreadException += Application_ThreadException;
 			 
 				bool result;
-				var mutex = new System.Threading.Mutex(true, "Vixen3RunningInstance", out result);
+				var mutex = new Mutex(true, "Vixen3RunningInstance", out result);
 
 				if (!result)
 				{
@@ -40,19 +43,29 @@ namespace VixenApplication
 			}
 			catch (Exception ex)
 			{
-				Logging.Fatal(ex);
+				Logging.FatalException(ErrorMsg, ex);
+				Environment.Exit(1);
 			}
 		}
 
-		static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
 		{
-			Logging.Error(e.Exception);
+			LogMessageAndExit(e.Exception);
+			
 		}
 
 		static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs args)
 		{ 
 			var e = (Exception)args.ExceptionObject;
-			Logging.Error(e);
+			LogMessageAndExit(e);
 		}
+
+		private static void LogMessageAndExit(Exception ex)
+		{
+			// Since we can't prevent the app from terminating, log this to the event log. 
+			Logging.FatalException(ErrorMsg, ex);
+			Environment.Exit(1);
+		}
+
 	}
 }
