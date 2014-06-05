VixenLights/Vixen

commit 86c39087e0681e939ae69e74bb35e011a9eb58fb

Partial work on VIX-340 and VIX-455. Consolidate and organize the existing settings into the settings.xml. Clean up and enhance the XMLProfileSettings class to better handle and organize settings into Profile, AppSettings, and Preferences. This will also allow the editor to save it's window positions and some other toggle settings. IT will allow for more settings to be persisted. It will also be the ground work for a new Preferences dialog that any module can contribute a preference pane to and set preferences to be retrieved.

Modules/Editor/TimedSequenceEditor/TimedSequenceEditorForm.cs
@@ -5,12 +5,14 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using System.Xml;
 using System.Drawing;
 using Common.Controls.Timeline;
 using Vixen.Execution;
 using Vixen.Execution.Context;
 using Vixen.Module;
+using Vixen.Module.EffectEditor;
 using VixenModules.App.Curves;
 using VixenModules.Media.Audio;
 using Vixen.Module.Editor;
@@ -164,11 +166,21 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			}
 
 			XMLProfileSettings xml = new XMLProfileSettings();
-			dockPanel.DockLeftPortion = xml.GetSetting(string.Format("{0}/DockLeftPortion", Name), 150);
-			dockPanel.DockRightPortion = xml.GetSetting(string.Format("{0}/DockRightPortion", Name), 150);
-			autoSaveToolStripMenuItem.Checked = xml.GetSetting(string.Format("{0}/AutoSaveEnabled", Name), true);
-			_autoSaveTimer.Interval = xml.GetSetting(string.Format("{0}/AutoSaveInterval", Name), 300000);
-			xml = null;
+
+			//Get preferences
+			_autoSaveTimer.Interval = xml.GetSetting(XMLProfileSettings.SettingType.Preferences, string.Format("{0}/AutoSaveInterval", Name), 300000);
+
+			//Restore App Settings
+			dockPanel.DockLeftPortion = xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/DockLeftPortion", Name), 150);
+			dockPanel.DockRightPortion = xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/DockRightPortion", Name), 150);
+			autoSaveToolStripMenuItem.Checked = xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/AutoSaveEnabled", Name), true);
+			toolStripButton_SnapTo.Checked = xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/SnapToSelected", Name), true);
+			Size = new Size(xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowWidth", Name), Size.Width),
+							xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowHeight", Name), Size.Height));
+			Location = new Point(xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowLocationX", Name), Location.X),
+							xml.GetSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowLocationY", Name), Location.Y));
+
+
 
 			_effectNodeToElement = new Dictionary<EffectNode, Element>();
 			_elementNodeToRows = new Dictionary<ElementNode, List<Row>>();
@@ -200,6 +212,8 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			TimelineControl.ElementsSelected += timelineControl_ElementsSelected;
 			TimelineControl.ContextSelected += timelineControl_ContextSelected;
 			TimelineControl.SequenceLoading = false;
+			TimelineControl.TimePerPixelChanged += TimelineControl_TimePerPixelChanged;
+			
 
 			_virtualEffectLibrary =
 				ApplicationServices.Get<IAppModuleInstance>(VixenModules.App.VirtualEffect.VirtualEffectLibraryDescriptor.Guid) as
@@ -217,6 +231,7 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			toolStripOperations.Items.Add(b);
 #endif
 		}
+
 
 #if DEBUG
 		private void b_Click(object sender, EventArgs e)
@@ -266,6 +281,7 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			TimelineControl.CursorMoved -= CursorMovedHandler;
 			TimelineControl.ElementsSelected -= timelineControl_ElementsSelected;
 			TimelineControl.ContextSelected -= timelineControl_ContextSelected;
+			TimelineControl.TimePerPixelChanged -= TimelineControl_TimePerPixelChanged;
 
 			Execution.ExecutionStateChanged -= OnExecutionStateChanged;
 			_autoSaveTimer.Stop();
@@ -588,9 +604,14 @@ namespace VixenModules.Editor.TimedSequenceEditor
 				}
 				PopulateAudioDropdown();
 
-				MarksForm.Sequence = Sequence as TimedSequence;
+				MarksForm.Sequence = _sequence;
 				MarksForm.PopulateMarkCollectionsList(null);
 
+				if (_sequence.TimePerPixel != TimeSpan.Zero)
+				{
+					TimelineControl.TimePerPixel = _sequence.TimePerPixel;	
+				}
+				
 				Logging.Debug(string.Format("Sequence {0} took {1} to load. ", sequence.Name, loadingWatch.Elapsed));
 			}
 			catch (Exception ee)
@@ -916,6 +937,16 @@ namespace VixenModules.Editor.TimedSequenceEditor
 					sequenceModified();
 				}
 			}
+		}
+
+		private void TimelineControl_TimePerPixelChanged(object sender, EventArgs e)
+		{
+			if (_sequence.TimePerPixel != TimelineControl.TimePerPixel)
+			{
+				_sequence.TimePerPixel = TimelineControl.TimePerPixel;
+				sequenceModified();	
+			}
+			
 		}
 
 		private void timelineControl_ContextSelected(object sender, ContextSelectedEventArgs e)
@@ -2887,14 +2918,29 @@ namespace VixenModules.Editor.TimedSequenceEditor
 
 		void IEditorUserInterface.EditorClosing()
 		{
-
+			
 			dockPanel.SaveAsXml(settingsPath);
-			XMLProfileSettings xml = new XMLProfileSettings();
-			xml.PutSetting(string.Format("{0}/DockLeftPortion", Name), (int)dockPanel.DockLeftPortion);
-			xml.PutSetting(string.Format("{0}/DockRightPortion", Name), (int)dockPanel.DockLeftPortion);
-			xml.PutSetting(string.Format("{0}/AutoSaveEnabled", Name), autoSaveToolStripMenuItem.Checked);
-			xml.PutSetting(string.Format("{0}/AutoSaveInterval", Name), _autoSaveTimer.Interval);
-			xml = null;
+			MarksForm.Close();
+			EffectsForm.Close();
+
+			var xml = new XMLProfileSettings();
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/DockLeftPortion", Name), (int)dockPanel.DockLeftPortion);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/DockRightPortion", Name), (int)dockPanel.DockLeftPortion);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/AutoSaveEnabled", Name), autoSaveToolStripMenuItem.Checked);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/SnapToSelected", Name), toolStripButton_SnapTo.Checked);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowHeight", Name), Size.Height);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowWidth", Name), Size.Width);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowLocationX", Name), Location.X);
+			xml.PutSetting(XMLProfileSettings.SettingType.AppSettings, string.Format("{0}/WindowLocationY", Name), Location.Y);
+
+			//These are only saved in options
+			//xml.PutPreference(string.Format("{0}/AutoSaveInterval", Name), _autoSaveTimer.Interval);
+
+			//Clean up any old locations from before we organized the settings.
+			xml.RemoveNode("StandardNudge");
+			xml.RemoveNode("SuperNudge");
+			xml.RemoveNode(Name);
+
 		}
 
 		#endregion

Application/VixenApplication/DataProfileForm.cs
@@ -36,7 +36,7 @@ namespace VixenApplication
 		{
 			XMLProfileSettings profile = new XMLProfileSettings();
 
-			radioButtonAskMe.Checked = (profile.GetSetting("Profiles/LoadAction", "Ask") == "Ask");
+			radioButtonAskMe.Checked = (profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "LoadAction", "Ask") == "Ask");
 			radioButtonLoadThisProfile.Checked = (!radioButtonAskMe.Checked);
 
 			ProfileItem selectedItem = comboBoxLoadThisProfile.SelectedItem as ProfileItem;
@@ -52,7 +52,7 @@ namespace VixenApplication
 
 			if (firstTime) {
 				if (radioButtonLoadThisProfile.Checked) {
-					int loadItemNum = profile.GetSetting("Profiles/ProfileToLoad", 0);
+					int loadItemNum = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileToLoad", 0);
 					if (loadItemNum < comboBoxLoadThisProfile.Items.Count)
 						comboBoxLoadThisProfile.SelectedIndex = loadItemNum;
 				}
@@ -72,11 +72,11 @@ namespace VixenApplication
             bool duplicateName = false;
             bool duplicateDataFolder = false;
             SaveCurrentItem();
-            profile.PutSetting("Profiles/ProfileCount", comboBoxProfiles.Items.Count);
+			profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "ProfileCount", comboBoxProfiles.Items.Count);
 			for (int i = 0; i < comboBoxProfiles.Items.Count; i++) {
 				ProfileItem item = comboBoxProfiles.Items[i] as ProfileItem;
-				profile.PutSetting("Profiles/" + "Profile" + i.ToString() + "/Name", item.Name);
-				profile.PutSetting("Profiles/" + "Profile" + i.ToString() + "/DataFolder", item.DataFolder);
+				profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i + "/Name", item.Name);
+				profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i + "/DataFolder", item.DataFolder);
                 //We're getting out of here and expect a restart by user, if the specified DataFolder doesn't exist, we should create it.
                 
                 if (item.DataFolder != string.Empty)
@@ -93,12 +93,12 @@ namespace VixenApplication
             }
 
 			if (radioButtonAskMe.Checked)
-				profile.PutSetting("Profiles/LoadAction", "Ask");
+				profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "LoadAction", "Ask");
 			else
-				profile.PutSetting("Profiles/LoadAction", "LoadSelected");
+				profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "LoadAction", "LoadSelected");
 
 			if (comboBoxLoadThisProfile.SelectedIndex >= 0)
-				profile.PutSetting("Profiles/ProfileToLoad", comboBoxLoadThisProfile.SelectedIndex);
+				profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "ProfileToLoad", comboBoxLoadThisProfile.SelectedIndex);
 
             //If a duplicate entry is found, we will prompt the user to contine on, or cancel and edit. This could be done with one bool, but in the event that we want to
             //be more specific about things in the future, Ill leave it the way it is for now.
@@ -136,7 +136,7 @@ namespace VixenApplication
 		{
 			XMLProfileSettings profile = new XMLProfileSettings();
 
-			int profileCount = profile.GetSetting("Profiles/ProfileCount", 0);
+			int profileCount = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileCount", 0);
 			if (profileCount == 0) {
 				ProfileItem item = new ProfileItem();
 				item.Name = "Default";
@@ -146,8 +146,8 @@ namespace VixenApplication
 			else {
 				for (int i = 0; i < profileCount; i++) {
 					ProfileItem item = new ProfileItem();
-					item.Name = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/Name", "New Profile");
-					item.DataFolder = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/DataFolder", defaultFolder);
+					item.Name = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i + "/Name", "New Profile");
+					item.DataFolder = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i + "/DataFolder", defaultFolder);
 					comboBoxProfiles.Items.Add(item);
 				}
 			}

Application/VixenApplication/DataZipForm.cs
@@ -33,7 +33,7 @@ namespace VixenApplication
 		{
 			XMLProfileSettings profile = new XMLProfileSettings();
 
-			int profileCount = profile.GetSetting("Profiles/ProfileCount", 0);
+			int profileCount = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileCount", 0);
 			if (profileCount == 0)
 			{
 				MessageBox.Show("Unable to locate any profiles.");
@@ -44,8 +44,8 @@ namespace VixenApplication
 				for (int i = 0; i < profileCount; i++)
 				{
 					ProfileItem item = new ProfileItem();
-					item.Name = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/Name", "");
-					item.DataFolder = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/DataFolder", "");
+					item.Name = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i.ToString() + "/Name", "");
+					item.DataFolder = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i.ToString() + "/DataFolder", "");
 					comboBoxProfiles.Items.Add(item);
 				}
 			}

Application/VixenApplication/SelectProfile.cs
@@ -38,12 +38,12 @@ namespace VixenApplication
 
             //Make sure we start with an empty listbox since we may repopulate after editing profiles
             listBoxProfiles.Items.Clear();
-            int profileCount = profile.GetSetting("Profiles/ProfileCount", 0);
+			int profileCount = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileCount", 0);
             for (int i = 0; i < profileCount; i++)
             {
                 ProfileItem item = new ProfileItem();
-                item.Name = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/Name", "New Profile");
-                item.DataFolder = profile.GetSetting("Profiles/" + "Profile" + i.ToString() + "/DataFolder", string.Empty);
+				item.Name = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i.ToString() + "/Name", "New Profile");
+				item.DataFolder = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + i.ToString() + "/DataFolder", string.Empty);
                 listBoxProfiles.Items.Add(item);
             }
         }

Application/VixenApplication/VixenApplication.cs
@@ -154,9 +154,9 @@ namespace VixenApplication
 		private void ProcessProfiles()
 		{
 			XMLProfileSettings profile = new XMLProfileSettings();
-			string loadAction = profile.GetSetting("Profiles/LoadAction", "LoadSelected");
-			int profileCount = profile.GetSetting("Profiles/ProfileCount", 0);
-			int profileToLoad = profile.GetSetting("Profiles/ProfileToLoad", -1);
+			string loadAction = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "LoadAction", "LoadSelected");
+			int profileCount = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileCount", 0);
+			int profileToLoad = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ProfileToLoad", -1);
 			// why ask if there is 0 or 1 profile?
 			if (loadAction == "Ask" && profileCount > 1) {
 				SelectProfile f = new SelectProfile();
@@ -173,7 +173,7 @@ namespace VixenApplication
 				}
 				else {
 					if (profileToLoad < profileCount) {
-						_rootDataDirectory = profile.GetSetting("Profiles/Profile" + profileToLoad.ToString() + "/DataFolder", string.Empty);
+						_rootDataDirectory = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Profile" + profileToLoad.ToString() + "/DataFolder", string.Empty);
 						if (_rootDataDirectory != string.Empty) {
 							if (!System.IO.Directory.Exists(_rootDataDirectory))
 								System.IO.Directory.CreateDirectory(_rootDataDirectory);

Common/Controls/TimeLineControl/Ruler.cs
@@ -49,39 +49,9 @@ namespace Common.Controls.Timeline
 			get { return new Size(400, 40); }
 		}
 
-		public int StandardNudgeTime
-		{
-			get
-			{
-				Common.Controls.XMLProfileSettings xml = new Common.Controls.XMLProfileSettings();
-				int nudgeValue = xml.GetSetting("StandardNudge", 10);
-				xml = null;
-				return nudgeValue;
-			}
-			set
-			{
-				Common.Controls.XMLProfileSettings xml = new Common.Controls.XMLProfileSettings();
-				xml.PutSetting("StandardNudge", value);
-				xml = null;
-			}
-		}
+		public int StandardNudgeTime { get; set; }
 
-		public int SuperNudgeTime
-		{
-			get
-			{
-				Common.Controls.XMLProfileSettings xml = new Common.Controls.XMLProfileSettings();
-				int nudgeValue = xml.GetSetting("SuperNudge", 20);
-				xml = null;
-				return nudgeValue;
-			}
-			set 
-			{
-				Common.Controls.XMLProfileSettings xml = new Common.Controls.XMLProfileSettings();
-				xml.PutSetting("SuperNudge", value);
-				xml = null;
-			}
-		}
+		public int SuperNudgeTime { get; set; }
 
 		#region Drawing
 

Common/Controls/XMLProfileSettings.cs
@@ -1,98 +1,122 @@
 ﻿using System;
-using System.Windows.Forms;
 using System.Xml;
 
 namespace Common.Controls
 {
 	public class XMLProfileSettings
 	{
-		private XmlDocument xmlDocument = new XmlDocument();
-		private string documentPath;
+		private readonly XmlDocument _xmlDocument = new XmlDocument();
+		private readonly string _documentPath;
+		private const string Root = "settings";
 
 		public XMLProfileSettings()
 		{
 			try {
 				//documentPath = Application.UserAppDataPath + "\\settings.xml";
-				documentPath =
+				_documentPath =
 					System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), "Vixen",
 					                       "Settings.xml");
-				if( !System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(documentPath)))
-					System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(documentPath));
-				xmlDocument.Load(documentPath);
+				if( !System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(_documentPath)))
+					System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(_documentPath));
+				_xmlDocument.Load(_documentPath);
 			}
 			catch {
-				xmlDocument.LoadXml("<settings></settings>");
+				_xmlDocument.LoadXml("<settings></settings>");
 			}
 		}
 
 		public XMLProfileSettings(string path)
 		{
 			try {
-				documentPath = path;
-				xmlDocument.Load(documentPath);
+				_documentPath = path;
+				_xmlDocument.Load(_documentPath);
 			}
 			catch {
-				xmlDocument.LoadXml("<settings></settings>");
+				_xmlDocument.LoadXml("<settings></settings>");
 			}
 		}
 
-		public int GetSetting(string xPath, int defaultValue)
+		public void RemoveNode(string xPath)
 		{
-			return Convert.ToInt32(GetSetting(xPath, Convert.ToString(defaultValue)));
+			XmlNode xmlNode = _xmlDocument.SelectSingleNode(String.Format("{0}/{1}", Root, xPath));
+			if (xmlNode != null)
+			{
+				xmlNode.RemoveAll();
+				xmlNode.ParentNode.RemoveChild(xmlNode);
+				_xmlDocument.Save(_documentPath);
+			}		
 		}
 
-		public void PutSetting(string xPath, int value)
+		public void PutSetting(SettingType type, string xPath, int value)
 		{
-			PutSetting(xPath, Convert.ToString(value));
+			PutSetting(type, xPath, Convert.ToString(value));
 		}
 
-		public void PutSetting(string xPath, bool value)
+		public void PutSetting(SettingType type, string xPath, bool value)
 		{
-			PutSetting(xPath, Convert.ToString(value));
+			PutSetting(type, xPath, Convert.ToString(value));
 		}
 
-		public bool GetSetting(string xPath, bool defaultValue)
+		public void PutSetting(SettingType type, string xPath, string value)
 		{
-			return Convert.ToBoolean(GetSetting(xPath, Convert.ToString(defaultValue)));
-		}
-
-		public string GetSetting(string xPath, string defaultValue)
-		{
-			XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath);
-			if (xmlNode != null) {
-				return xmlNode.InnerText;
-			}
-			else {
-				return defaultValue;
-			}
-		}
-
-		public void PutSetting(string xPath, string value)
-		{
-			XmlNode xmlNode = xmlDocument.SelectSingleNode("settings/" + xPath);
-			if (xmlNode == null) {
-				xmlNode = createMissingNode("settings/" + xPath);
+			string path = String.Format("{0}/{1}/{2}", Root, type, xPath);
+			XmlNode xmlNode = _xmlDocument.SelectSingleNode(path);
+			if (xmlNode == null)
+			{
+				xmlNode = CreateMissingNode(path);
 			}
 			xmlNode.InnerText = value;
-			xmlDocument.Save(documentPath);
+			_xmlDocument.Save(_documentPath);
 		}
 
-		private XmlNode createMissingNode(string xPath)
+		public int GetSetting(SettingType type, string xPath, int defaultValue)
+		{
+			return Convert.ToInt32(GetSetting(type, xPath, Convert.ToString(defaultValue)));
+		}
+
+		public bool GetSetting(SettingType type, string xPath, bool defaultValue)
+		{
+			return Convert.ToBoolean(GetSetting(type, xPath, Convert.ToString(defaultValue)));
+		}
+
+		public string GetSetting(SettingType type, string xPath, string defaultValue)
+		{
+			XmlNode xmlNode = _xmlDocument.SelectSingleNode(String.Format("{0}/{1}/{2}", Root, type, xPath));
+			if (xmlNode != null)
+			{
+				return xmlNode.InnerText;
+			}
+			return defaultValue;
+		}
+
+		private XmlNode CreateMissingNode(string xPath)
 		{
 			string[] xPathSections = xPath.Split('/');
 			string currentXPath = string.Empty;
-			XmlNode testNode = null;
-			XmlNode currentNode = xmlDocument.SelectSingleNode("settings");
+			XmlNode currentNode = _xmlDocument.SelectSingleNode(Root);
 			foreach (string xPathSection in xPathSections) {
 				currentXPath += xPathSection;
-				testNode = xmlDocument.SelectSingleNode(currentXPath);
+				XmlNode testNode = _xmlDocument.SelectSingleNode(currentXPath);
 				if (testNode == null) {
 					currentNode.InnerXml += string.Format("<{0}></{0}>" , xPathSection);
 				}
-				currentNode = xmlDocument.SelectSingleNode(currentXPath);
+				currentNode = _xmlDocument.SelectSingleNode(currentXPath);
 				currentXPath += "/";
 			}
 			return currentNode;
 		}
+
+		/// <summary>
+		/// Enum values for the types of settings the application can save.
+		/// </summary>
+		public enum SettingType
+		{
+			Profiles,
+			AppSettings,
+			Preferences
+		};
+		
 	}
+
+	
 }

Modules/Editor/TimedSequenceEditor/Form_Marks.Designer.cs
@@ -190,7 +190,8 @@
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form_Marks";
 			this.Text = "Marks";
-			this.Load += new System.EventHandler(this.Form_Effects_Load);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Marks_Closing);
+			this.Load += new System.EventHandler(this.Form_Marks_Load);
 			this.toolStrip1.ResumeLayout(false);
 			this.toolStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownStandardNudge)).EndInit();

commit d2e5a12001a3ea372e82e4d2913f7794bf67d050

Fix a bug introduced in commit 86c39087e0681e939ae69e74bb35e011a9eb58fb with the settings work that would allow Time per pixel to be negative when creating a new sequence.

Modules/Editor/TimedSequenceEditor/TimedSequenceEditorForm.cs
@@ -625,7 +625,7 @@ namespace VixenModules.Editor.TimedSequenceEditor
 				MarksForm.Sequence = _sequence;
 				MarksForm.PopulateMarkCollectionsList(null);
 
-				if (_sequence.TimePerPixel != TimeSpan.Zero)
+				if (_sequence.TimePerPixel > TimeSpan.Zero )
 				{
 					TimelineControl.TimePerPixel = _sequence.TimePerPixel;	
 				}