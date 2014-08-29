e6790602b89a0136cbca49307d9b49227c00677d
Jeff Uchitjil <jeffu@kb9kld.org>
2014-08-29 17:15:24 -0500

    Minor tweak to set the queue size to the size of the effect data so it does not have to resize itself.

--- Vixen.System/Execution/EffectNodeQueue.cs
+++ Vixen.System/Execution/EffectNodeQueue.cs
@@ -15,6 +15,12 @@ namespace Vixen.Execution
 			//_queue = new ConcurrentQueue<IEffectNode>();
 		}
 
+		public EffectNodeQueue(int size)
+		{
+			_queue = new Queue<IEffectNode>(size);
+			//_queue = new ConcurrentQueue<IEffectNode>();
+		}
+
 		public EffectNodeQueue(IEnumerable<IEffectNode> items)
 		{
 			_queue = new Queue<IEffectNode>(items);
