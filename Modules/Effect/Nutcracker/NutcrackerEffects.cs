a4b42aa972d26935c74c3542bc31ef8f53350e95
Jeff Uchitjil <jeffu@kb9kld.org>
2014-09-15 19:19:49 -0500

    Refactor another Pictures type method out.

--- Modules/Effect/Nutcracker/NutcrackerEffects.cs
+++ Modules/Effect/Nutcracker/NutcrackerEffects.cs
@@ -1499,20 +1499,8 @@ namespace VixenModules.Effect.Nutcracker
 		#region Pictures
 
 		
-
-		public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
-		{
-			var ratioX = (double)maxWidth / image.Width;
-			var ratioY = (double)maxHeight / image.Height;
-			var ratio = Math.Min(ratioX, ratioY);
-
-			var newWidth = (int)(image.Width * ratio);
-			var newHeight = (int)(image.Height * ratio);
-
-			var newImage = new Bitmap(newWidth, newHeight);
-			Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
-			return newImage;
-		}
+		//Refactored out to partial class Nutcracker_RenderPictures
+		
 
 		#endregion //Picture
 
--- Modules/Effect/Nutcracker/Nutcracker_RenderPictures.cs
+++ Modules/Effect/Nutcracker/Nutcracker_RenderPictures.cs
@@ -168,6 +168,20 @@ namespace VixenModules.Effect.Nutcracker
 				image.Dispose();
 		}
 
+		public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+		{
+			var ratioX = (double)maxWidth / image.Width;
+			var ratioY = (double)maxHeight / image.Height;
+			var ratio = Math.Min(ratioX, ratioY);
+
+			var newWidth = (int)(image.Width * ratio);
+			var newHeight = (int)(image.Height * ratio);
+
+			var newImage = new Bitmap(newWidth, newHeight);
+			Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+			return newImage;
+		}
+
 		internal enum EffectType
 		{
 			RenderPictureLeft,  
