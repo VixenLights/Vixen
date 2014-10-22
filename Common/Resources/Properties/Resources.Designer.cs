9e02c56f0317b593f17c076a146eebe931647c12
James Bolding <bolwire@cableone.net>
2014-10-22 07:44:52 -0500

    Clean up of Preset Library form code Adds icons for Export/Import of library items

--- Common/Resources/Properties/Resources.Designer.cs
+++ Common/Resources/Properties/Resources.Designer.cs
@@ -923,6 +923,16 @@ namespace Common.Resources.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap disk {
+            get {
+                object obj = ResourceManager.GetObject("disk", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap document_comment_above {
             get {
                 object obj = ResourceManager.GetObject("document_comment_above", resourceCulture);
@@ -1306,6 +1316,16 @@ namespace Common.Resources.Properties {
         public static System.Drawing.Bitmap folder_go {
             get {
                 object obj = ResourceManager.GetObject("folder_go", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap folder_open {
+            get {
+                object obj = ResourceManager.GetObject("folder_open", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
