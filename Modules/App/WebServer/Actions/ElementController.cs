b642c4198065dbd38af8f6fc9efacd3c9d39447b
Jeff Uchitjil <jeffu@kb9kld.org>
2014-12-06 17:28:25 -0600

    Add feature to web api to allow text to be placed on a matrix style element. First cut.

--- Modules/App/WebServer/Actions/ElementController.cs
+++ Modules/App/WebServer/Actions/ElementController.cs
@@ -13,6 +13,7 @@ using Vixen.Sys;
 using VixenModules.App.WebServer.HTTP;
 using VixenModules.App.WebServer.Model;
 using VixenModules.Effect.SetLevel;
+using VixenModules.Effect.Nutcracker;
 using VixenModules.Property.Color;
 using Element = VixenModules.App.WebServer.Model.Element;
 
@@ -34,9 +35,19 @@ namespace VixenModules.App.WebServer.Actions
 					GetElements(request, response);
 					return;
 				}
+				if (request.Uri.StartsWith("/api/element/getElementIdByName"))
+				{
+					GetElementIdByName(request, response);
+					return;
+				}
 				if (request.Uri.StartsWith("/api/element/on"))
 				{
 					TurnOnElement(request, response);
+					return;
+				}
+				if (request.Uri.StartsWith("/api/element/text"))
+				{
+					TextOnElement(request, response);
 					return;
 				}
 			}
@@ -54,6 +65,52 @@ namespace VixenModules.App.WebServer.Actions
 			}
 			
 			SerializeResponse(elements,response);
+		}
+
+		private void GetElementIdByName(HttpRequestHead request, IHttpResponseDelegate response)
+		{
+			NameValueCollection parms = GetParameters(request);
+
+			if (!parms.HasKeys() && parms["name"] != null)
+			{
+				HttpResponseHead headers = GetHeaders(0, HttpStatusCode.BadRequest.ToString());
+				response.OnResponse(headers, new BufferedProducer(""));
+				return;
+			}
+			string name = parms.Get("name");
+			IEnumerable<ElementNode> elementNodes = VixenSystem.Nodes.GetRootNodes();
+			var elements = new List<Element>();
+			foreach (var elementNode in elementNodes)
+			{
+				AddNodesByName(elements, elementNode, name);
+			}
+
+			SerializeResponse(elements, response);
+		}
+
+		private void AddNodesByName(List<Element> elements, ElementNode elementNode, string name)
+		{
+
+			if (elementNode.Name.Equals(name))
+			{
+				var element = new Element
+				{
+					Id = elementNode.Id,
+					Name = elementNode.Name,
+					Colors = ColorModule.getValidColorsForElementNode(elementNode, true).Select(ColorTranslator.ToHtml).ToList()
+				};
+				elements.Add(element);
+				if (!elementNode.IsLeaf)
+				{
+					var children = new List<Element>();
+					element.Children = children;
+					foreach (var childNode in elementNode.Children)
+					{
+						AddNodesByName(children, childNode, name);
+					}
+				}	
+			}
+			
 		}
 
 		private void AddNodes(List<Element> elements, ElementNode elementNode)
@@ -135,6 +192,92 @@ namespace VixenModules.App.WebServer.Actions
 				allElements?"All":VixenSystem.Nodes.GetElementNode(elementId).Name, seconds);
 
 			SerializeResponse(status,response);
+		}
+
+		private static void TextOnElement(HttpRequestHead request, IHttpResponseDelegate response)
+		{
+			var status = new Status();
+			NameValueCollection parms = GetParameters(request);
+
+			if (!parms.HasKeys() && parms["id"] != null && parms["time"] != null 
+				&& parms["text"] != null)
+			{
+				HttpResponseHead headers = GetHeaders(0, HttpStatusCode.BadRequest.ToString());
+				response.OnResponse(headers, new BufferedProducer(""));
+				return;
+			}
+			Color elementColor = Color.White;
+			if (parms["color"] != null)
+			{
+				if (parms["color"].Length != 7 || !parms["color"].StartsWith("#"))
+				{
+					status.Message = "Invalid color. Must be Hex.";
+					SerializeResponse(status, response);
+					return;
+				}
+				elementColor=ColorTranslator.FromHtml(parms["color"]);
+			}
+
+			int position = 5;
+			if (parms["position"] != null && !int.TryParse(parms["position"], out position))
+			{
+				status.Message = "Position must be numeric.";
+				SerializeResponse(status, response);
+				return;
+			}
+			if (position > 100)
+			{
+				status.Message = "Position must be 0 - 100.";
+				SerializeResponse(status, response);
+				return;
+			}
+
+			int direction = 0;
+			if (parms["direction"] != null && !int.TryParse(parms["direction"], out direction))
+			{
+				status.Message = "Direction must be numeric.";
+				SerializeResponse(status, response);
+				return;
+			}
+			if (direction > 4)
+			{
+				status.Message = "Direction must be 0 - 4.";
+				SerializeResponse(status, response);
+				return;	
+			}
+			Guid elementId;
+			Guid.TryParse(parms["id"], out elementId);
+
+			int seconds;
+			if (!int.TryParse(parms["time"], out seconds))
+			{
+				status.Message = "Time must be numeric.";
+				SerializeResponse(status, response);
+				return;
+			}
+
+			string text = parms.Get("text");
+
+			var nutcrackerEffect = new Nutcracker
+			{
+				TimeSpan = TimeSpan.FromSeconds(seconds),
+				NutcrackerData = new NutcrackerData
+				{
+					CurrentEffect = NutcrackerEffects.Effects.Text,
+					Text_Top = position,
+					Text_Direction = direction,
+					Text_Line1 = text
+				},
+				
+				TargetNodes = new[] { VixenSystem.Nodes.GetElementNode(elementId) }
+			};
+			nutcrackerEffect.NutcrackerData.Palette.SetColor(1,elementColor);
+
+			Module.LiveContext.Execute(new EffectNode(nutcrackerEffect, TimeSpan.Zero));
+			status.Message = string.Format("Text \"{0}\" applied to element {1} for {2} seconds.",
+				VixenSystem.Nodes.GetElementNode(elementId).Name, text, seconds);
+
+			SerializeResponse(status, response);
 		}
 
 		
