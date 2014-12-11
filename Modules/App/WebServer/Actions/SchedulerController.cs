18ee3177decf476ac0df8364ce60db1b9c60b6dc
Jeff Uchitjil <jeffu@kb9kld.org>
2014-12-11 11:10:47 -0600

    Add the basics for scheduler API. The scheduler needs hooks to be able to interrogate it.

---
+++ Modules/App/WebServer/Actions/SchedulerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kayak;
+using Kayak.Http;
+using VixenModules.App.WebServer.Model;
+
+namespace VixenModules.App.WebServer.Actions
+{
+	
+	public class SchedulerController:BaseController
+	{
+		private static NLog.Logger Logging = NLog.LogManager.GetCurrentClassLogger();
+
+		public override void ProcessPost(HttpRequestHead request, IDataProducer requestBody, IHttpResponseDelegate response)
+		{
+			if (request.Uri.StartsWith("/api/schedule"))
+			{
+
+				if (request.Uri.StartsWith("/api/schedule/status"))
+				{
+					return;
+				}
+			}
+
+			UnsupportedOperation(request, response);
+		}
+
+		public override void ProcessGet(HttpRequestHead request, IHttpResponseDelegate response)
+		{
+			UnsupportedOperation(request, response);
+		}
+
+		private void Status(HttpRequestHead request, IHttpResponseDelegate response)
+		{
+			var status = new Status {Message = "Not implemented."};
+
+
+			SerializeResponse(status, response);
+		}
+	}
+}
