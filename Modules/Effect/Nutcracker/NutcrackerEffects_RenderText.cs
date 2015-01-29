3b0379e799b38619ddea3b6d66075766049b00ea
gnarmstr <gnarmstr@hotmail.com>
2015-01-29 14:07:06 +1100

    VIX - 728 Added colour selection for each line of text in the Nutcracker Text effect. Also improved the handling of the Text Position.

--- Modules/Effect/Nutcracker/NutcrackerEffects_RenderText.cs
+++ Modules/Effect/Nutcracker/NutcrackerEffects_RenderText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using NLog.Targets;
 
 namespace VixenModules.Effect.Nutcracker
 {
@@ -14,82 +15,99 @@ namespace VixenModules.Effect.Nutcracker
 			{
 				using (Graphics graphics = Graphics.FromImage(bitmap))
 				{
-					Color c = Palette.GetColor(0);
-					using (Brush brush = new SolidBrush(c))
+					var c = new[] { Palette.GetColor(0), Palette.GetColor(1), Palette.GetColor(2), Palette.GetColor(3) };
+					var line = new[] { line1, line2, line3, line4 };
+					int i = 0;
+					string msg = "";
+					var maxlinesize = new[] { Convert.ToInt32(graphics.MeasureString(line1, font).Width), Convert.ToInt32(graphics.MeasureString(line2, font).Width), Convert.ToInt32(graphics.MeasureString(line3, font).Width), Convert.ToInt32(graphics.MeasureString(line4, font).Width) };
+					int maxtextsize = maxlinesize.Max();
+					int maxIndex = maxlinesize.ToList().IndexOf(maxtextsize);
+					SizeF textsize = graphics.MeasureString(line[maxIndex], font);
+					do
 					{
-
-						string msg = line1;
-
-						if (line2.Length > 0)
+						using (Brush brush = new SolidBrush(c[i]))
 						{
-							msg += "\n" + line2;
-						}
-						if (line3.Length > 0)
-						{
-							msg += "\n" + line3;
-						}
-						if (line4.Length > 0)
-						{
-							msg += "\n" + line4;
-						}
-
-						SizeF textsize = graphics.MeasureString(msg, font);
-
-						int maxwidth = Convert.ToInt32(textsize.Width);
-						int maxht = Convert.ToInt32(textsize.Height);
-
-						int extraLeft = IsGoingLeft(dir) ? BufferWi - (int)textsize.Width : 0;
-						int extraRight = IsGoingRight(dir) ? BufferWi - (int)textsize.Width : 0;
-						int extraDown = IsGoingDown(dir) ? BufferHt - (int)textsize.Height : 0;
-						int extraUp = IsGoingUp(dir) ? BufferHt - (int)textsize.Height : 0;
-
-						int xlimit = (BufferWi + maxwidth) * 8 + 1;
-						int ylimit = (BufferHt + maxht) * 8 + 1;
-						int offsetLeft = (position * maxwidth / 100) - maxwidth / 2;
-						int offsetTop = maxht / 2 - (position * maxht / 100);
-
-						graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
-
-						Point point;
-						switch (dir)
-						{
-							case 0:
-								// left
-								point = new Point(Convert.ToInt32(centerStop ? Math.Max(BufferWi - State / 8, extraLeft / 2) : BufferWi - State % xlimit / 8), offsetTop);
-								graphics.DrawString(msg, font, brush, point);
-								break;
-							case 1:
-								// right
-								point = new Point(Convert.ToInt32(centerStop ? Math.Min(State / 8 - BufferWi, extraRight / 2) : State % xlimit / 8 - BufferWi), offsetTop);
-								graphics.DrawString(msg, font, brush, point);
-								break;
-							case 2:
-								// up
-								point = new Point(offsetLeft, Convert.ToInt32(centerStop ? Math.Max((int)(BufferHt - State / 8), extraUp / 2) : BufferHt - State % ylimit / 8));
-								graphics.DrawString(msg, font, brush, point);
-								break;
-							case 3:
-								// down
-								point = new Point(offsetLeft, Convert.ToInt32(centerStop ? Math.Min((int)(State / 8 - BufferHt), extraDown / 2) : State % ylimit / 8 -BufferHt));
-								graphics.DrawString(msg, font, brush, point);
-								break;
-							default:
-								// no movement - centered
-								point = new Point(0, offsetTop);
-								graphics.DrawString(msg, font, brush, point);
-								break;
-						}
-
-						// copy to buffer
-						for (int x = 0; x < BufferWi; x++)
-						{
-							for (int y = 0; y < BufferHt; y++)
+							switch (i)
 							{
-								c = bitmap.GetPixel(x, BufferHt - y - 1);
-								SetPixel(x, y, c);
+								case 0:
+									msg = line[i];
+									break;
+								case 1:
+									msg = "\n" + line[i];
+									break;
+								case 2:
+									msg = "\n\n" + line[i];
+									break;
+								case 3:
+									msg = "\n\n\n" + line[i];
+									break;
+							}
+							int maxwidth = Convert.ToInt32(textsize.Width);
+							int maxht = Convert.ToInt32(textsize.Height);
+							int extraLeft = IsGoingLeft(dir) ? BufferWi - (int) textsize.Width : 0;
+							int extraRight = IsGoingRight(dir) ? BufferWi - (int) textsize.Width : 0;
+							int extraDown = IsGoingDown(dir) ? BufferHt - (int) textsize.Height : 0;
+							int extraUp = IsGoingUp(dir) ? BufferHt - (int) textsize.Height : 0;
+							int xlimit = (BufferWi + maxwidth)*8 + 1;
+							int ylimit = (BufferHt + maxht)*8 + 1;
+							int offsetLeft = (position*maxwidth/100) - maxwidth/2;
+							int offsetTop = (maxht*5) - (position*maxht*8/100);
+								graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+								Point point;
+							switch (dir)
+							{
+								case 0:
+									// left
+									point =
+										new Point(
+											Convert.ToInt32(centerStop
+												? Math.Max(BufferWi - State/8, extraLeft/2)
+												: BufferWi - State%xlimit/8), offsetTop);
+									graphics.DrawString(msg, font, brush, point);
+									break;
+								case 1:
+									// right
+									point =
+										new Point(
+											Convert.ToInt32(centerStop
+												? Math.Min(State/8 - BufferWi, extraRight/2)
+												: State%xlimit/8 - BufferWi), offsetTop);
+									graphics.DrawString(msg, font, brush, point);
+									break;
+								case 2:
+									// up
+									point = new Point(offsetLeft,
+										Convert.ToInt32(centerStop
+											? Math.Max((int) (BufferHt - State/8), extraUp/2)
+											: BufferHt - State%ylimit/8));
+									graphics.DrawString(msg, font, brush, point);
+									break;
+								case 3:
+									// down
+									point = new Point(offsetLeft,
+										Convert.ToInt32(centerStop
+											? Math.Min((int) (State/8 - BufferHt), extraDown/2)
+											: State%ylimit/8 - BufferHt));
+									graphics.DrawString(msg, font, brush, point);
+									break;
+								default:
+									// no movement - centered
+									point = new Point(0, offsetTop);
+									graphics.DrawString(msg, font, brush, point);
+									break;
+							}
+								// copy to buffer
+							for (int x = 0; x < BufferWi; x++)
+							{
+								for (int y = 0; y < BufferHt; y++)
+								{
+									c[i] = bitmap.GetPixel(x, BufferHt - y - 1);
+									SetPixel(x, y, c[i]);
+								}
 							}
 						}
-					}
+						i++;
+					} while (i < 4);
 				}
 			}
 		}
