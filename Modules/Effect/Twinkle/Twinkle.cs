dad59b743ca30817df391969042af12290be3dc6
Jeff Uchitjil <jeffu@kb9kld.org>
2015-04-11 12:49:10 -0500

    VIX-695 Fix a bug in the Twinkle effect that would not allow it to render at selected levels deep. There was a null check on a item that is not used and can be null. Also added an error trap in the prerender wrapper that will prevent the rendering process from breaking when an exception in a effect render occurs.

--- Modules/Effect/Twinkle/Twinkle.cs
+++ Modules/Effect/Twinkle/Twinkle.cs
@@ -231,9 +231,6 @@ namespace VixenModules.Effect.Twinkle
 		private EffectIntents RenderElement(ElementNode node, double positionWithinGroup,
 		                                    List<IndividualTwinkleDetails> twinkles = null)
 		{
-			if (node == null || node.Element == null)
-				return null;
-
 			if (twinkles == null)
 				twinkles = GenerateTwinkleData();
 
