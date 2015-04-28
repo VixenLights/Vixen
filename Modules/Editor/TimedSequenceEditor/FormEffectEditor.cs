201efc947a6188d2cc0d87e13eb19a1d8a92e43c
Jeff Uchitjil <jeffu@kb9kld.org>
2015-04-27 22:24:52 -0500

    Code cleanup.

--- Modules/Editor/TimedSequenceEditor/FormEffectEditor.cs
+++ Modules/Editor/TimedSequenceEditor/FormEffectEditor.cs
@@ -8,8 +8,6 @@ using System.Linq;
 using System.Timers;
 using System.Windows.Forms;
 using Vixen.Execution.Context;
-using Vixen.Module.Effect;
-using Vixen.Services;
 using Vixen.Sys;
 using VixenModules.App.ColorGradients;
 using VixenModules.App.Curves;
@@ -23,39 +21,44 @@ namespace VixenModules.Editor.TimedSequenceEditor
 {
 	public partial class FormEffectEditor : DockContent
 	{
-		//private IEnumerable<Element> _elements = new List<Element>();
-		private Dictionary<Element, EffectModelCandidate> _elements = new Dictionary<Element, EffectModelCandidate>();
+		private readonly Dictionary<Element, EffectModelCandidate> _elements = new Dictionary<Element, EffectModelCandidate>();
 		private readonly TimedSequenceEditorForm _sequenceEditorForm;
-		private ToolStripButton toolStripButtonPreview;
+		private readonly ToolStripButton _toolStripButtonPreview;
 		private LiveContext _previewContext;
-		private bool _isPreviewing;
-		private Timer _previewLoopTimer = new Timer();
+		private readonly Timer _previewLoopTimer = new Timer();
 
 		public FormEffectEditor(TimedSequenceEditorForm sequenceEditorForm)
 		{
 			InitializeComponent();
-			Hashtable table = new Hashtable();
-			table.Add(typeof(ColorGradient), typeof(EffectColorGradientTypeEditor).AssemblyQualifiedName);
-			table.Add(typeof(Curve), typeof(EffectCurveTypeEditor).AssemblyQualifiedName);
-			
+			//This should be refactored out of here and into the actual type classes, but due to some circular dependencies it is not possible until the 
+			//old effect editors are removed.
+			Hashtable table = new Hashtable
+			{
+				{typeof (ColorGradient), typeof (EffectColorGradientTypeEditor).AssemblyQualifiedName},
+				{typeof (Curve), typeof (EffectCurveTypeEditor).AssemblyQualifiedName}
+			};
+
 			TypeDescriptor.AddEditorTable(typeof(UITypeEditor), table);
+			//End refactor block
+
 			_sequenceEditorForm = sequenceEditorForm;
 			foreach (var control in propertyGridEffectProperties.Controls)
 			{
 				if (control is ToolStrip)
 				{
 					ToolStrip ts = control as ToolStrip;
-					//ts.Renderer = new MyRenderer();
 					foreach (ToolStripItem item in ts.Items)
 					{
 						item.Visible = false;
 					}
-					toolStripButtonPreview = new ToolStripButton();
-					toolStripButtonPreview.CheckOnClick = true;
-					toolStripButtonPreview.ForeColor = Color.WhiteSmoke;
-					toolStripButtonPreview.Text = @"Preview";
-					toolStripButtonPreview.Click += toolStripButtonPreview_Click;
-					ts.Items.Add(toolStripButtonPreview);
+					_toolStripButtonPreview = new ToolStripButton
+					{
+						CheckOnClick = true,
+						ForeColor = Color.WhiteSmoke,
+						Text = @"Preview"
+					};
+					_toolStripButtonPreview.Click += toolStripButtonPreview_Click;
+					ts.Items.Add(_toolStripButtonPreview);
 				}
 			}
 
@@ -64,24 +67,7 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			_previewLoopTimer.Elapsed += PreviewLoopTimerOnElapsed;
 		}
 
-		void timelineControl_SelectionChanged(object sender, EventArgs e)
-		{
-			Elements = _sequenceEditorForm.TimelineControl.SelectedElements;
-		}
-
-		void propertyGridEffectProperties_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
-		{
-			_sequenceEditorForm.AddEffectsModifiedToUndo(new Dictionary<Element, EffectModelCandidate>(_elements), e.ChangedItem.Label);
-			var keys = new List<Element>(Elements);
-			foreach (var element in keys)
-			{
-				element.UpdateNotifyContentChanged();
-				_elements[element]=new EffectModelCandidate(element.EffectNode.Effect);
-			}
-			
-		}
-
-		public IEnumerable<Element> Elements
+		internal IEnumerable<Element> Elements
 		{
 			get { return _elements.Keys; }
 			set
@@ -92,15 +78,6 @@ namespace VixenModules.Editor.TimedSequenceEditor
 				SetPreviewState();
 				propertyGridEffectProperties.SelectedObjects = _elements.Keys.Select(x => x.EffectNode.Effect).ToArray();
 			} 
-		}
-
-		private void SetPreviewState()
-		{
-			PreviewStop();
-			if (_elements.Any() && toolStripButtonPreview.Checked)
-			{
-				PreviewPlay();
-			}
 		}
 
 		private void AddElements(IEnumerable<Element> elements)
@@ -127,16 +104,49 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			}
 		}
 
+		#region Events
+		
+		private void timelineControl_SelectionChanged(object sender, EventArgs e)
+		{
+			Elements = _sequenceEditorForm.TimelineControl.SelectedElements;
+		}
+
+		private void propertyGridEffectProperties_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+		{
+			_sequenceEditorForm.AddEffectsModifiedToUndo(new Dictionary<Element, EffectModelCandidate>(_elements), e.ChangedItem.Label);
+			var keys = new List<Element>(Elements);
+			foreach (var element in keys)
+			{
+				element.UpdateNotifyContentChanged();
+				_elements[element] = new EffectModelCandidate(element.EffectNode.Effect);
+			}
+
+		}
+
 		private void element_ContentChanged(object sender, EventArgs e)
 		{
 			propertyGridEffectProperties.Refresh();
 		}
 
+		#endregion
+
+		#region Preview
+
+		private void SetPreviewState()
+		{
+			PreviewStop();
+			if (_elements.Any() && _toolStripButtonPreview.Checked)
+			{
+				PreviewPlay();
+			}
+		}
+
+		
 		private void PreviewLoopTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
 		{
 			if (InvokeRequired)
 			{
-				if (toolStripButtonPreview.Checked && _elements.Any())
+				if (_toolStripButtonPreview.Checked && _elements.Any())
 				{
 					BeginInvoke(new Action(PreviewPlay));	
 				}
@@ -153,8 +163,7 @@ namespace VixenModules.Editor.TimedSequenceEditor
 				_previewContext.Start();
 
 			}
-			IEnumerable<EffectNode> nodes = Elements.Select(x => x.EffectNode);
-			IEnumerable<EffectNode> orderedNodes = nodes.OrderBy(x => x.StartTime);
+			IEnumerable<EffectNode> orderedNodes = Elements.Select(x => x.EffectNode).OrderBy(x => x.StartTime);
 			TimeSpan startOffset = orderedNodes.First().StartTime;
 			TimeSpan duration = orderedNodes.Last().EndTime - startOffset;
 			List<EffectNode> nodesToPlay = orderedNodes.Select(effectNode => new EffectNode(effectNode.Effect, effectNode.StartTime - startOffset)).ToList();
@@ -177,19 +186,9 @@ namespace VixenModules.Editor.TimedSequenceEditor
 			TogglePreviewState();
 		}
 
-		//private void TimedSequenceEditorEffectEditor_KeyDown(object sender, KeyEventArgs e)
-		//{
-		//	if (e.KeyCode == Keys.Space)
-		//	{
-		//		e.SuppressKeyPress = true;
-		//		//buttonPreview.Checked = !buttonPreview.Checked;
-		//		TogglePreviewState();
-		//	}
-		//}
-
 		private void TogglePreviewState()
 		{
-			if (toolStripButtonPreview.Checked)
+			if (_toolStripButtonPreview.Checked)
 			{
 				if (_elements.Any())
 				{
@@ -201,6 +200,8 @@ namespace VixenModules.Editor.TimedSequenceEditor
 				PreviewStop();
 			}
 		}
+
+		#endregion
 	}
 
 }
