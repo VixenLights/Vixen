d052ebabd8a4ffd5f2a74e85cfb013a0830d95f7
Jeff Uchitjil <jeffu@kb9kld.org>
2015-07-15 19:46:05 -0500

    Name the types of butterfly effects.

--- Modules/Effect/Butterfly/ButterflyType.cs
+++ Modules/Effect/Butterfly/ButterflyType.cs
@@ -4,15 +4,22 @@ namespace VixenModules.Effect.Butterfly
 {
 	public enum ButterflyType
 	{
-		[Description("1")]
-		Type1,
-		[Description("2")]
+		[Description("Pulsing Circle")]
 		Type2,
-		[Description("3")]
+		[Description("Pulsing Grid of Circles")]
 		Type3,
-		[Description("4")]
+		[Description("Diagonal Boxes")]
+		Type1,
+		[Description("Alternating Inverted Boxes")]
 		Type4,
-		[Description("5")]
+		[Description("Flapping Wings")]
 		Type5
+
+
+//		1 - Pulsing Circle (formerly 2)
+//2 - #1 within Diagonally Scrolling Boxes (formerly 1)
+//3 - #2 with alternating inverted gradient (formerly 4)
+//4 - Pulsing Grid of Circles (formerly 3)
+//5 - Flapping wings
 	}
 }
