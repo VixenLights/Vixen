f05d60657f58dd2d15e587f3d3fca733a05ced4a
Jeff Uchitjil <jeffu@kb9kld.org>
2015-07-27 18:06:48 -0500

    Remove a unused long type from the LightingIntent class. We use tons of these, so should help save some memory consumption. I have been meaning to do this for a while.

--- Vixen.System/Intent/LightingIntent.cs
+++ Vixen.System/Intent/LightingIntent.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using Vixen.Data.Value;
-using Vixen.Sys;
 
 namespace Vixen.Intent
 {
 	public class LightingIntent : LinearIntent<LightingValue>
 	{
-		private static object lockObject = new object();
-		private static long genericIDValue = 0;
 		public LightingIntent(LightingValue startValue, LightingValue endValue, TimeSpan timeSpan)
 			: base(startValue, endValue, timeSpan)
 		{
-			//Generate a RNG Cryptographically random ID code for reference... 
-			//GenericID = System.IO.Path.GetRandomFileName().Replace(".", "");
-			lock (lockObject) {
-				GenericID = genericIDValue++;
-			}
+			
 		}
-		public long GenericID { get; set; }
+		
 	}
 }
