ebdfbfc76abe95b411b7aefa373879c52b36dd5a
Jared B <jaredb7@internode.on.net>
2015-09-02 21:46:09 +1000

    [VIX-839] Fix a edge case where channels may not have a name set
    Added some exception handling.
    
    A Channel name of "Unnamed Output <output#>" is also substituted in this
    case.

--- Modules/Sequence/Vixen2x/Vixen2SequenceData.cs
+++ Modules/Sequence/Vixen2x/Vixen2SequenceData.cs
@@ -194,12 +194,31 @@ namespace VixenModules.SequenceType.Vixen2x
 			var channelname = string.Empty;
 			if (version == 1)
 			{
-				channelname = element.FirstNode.ToString();
+				try
+				{
+					channelname = element.FirstNode.ToString();
+				}
+				catch (NullReferenceException)
+				{
+					//do nothing
+				}
 			}
 			//must be version 2.5 so get the channel name from attribute 'name'
 			else if (version == 2)
 			{
-				channelname = element.Attribute("name").Value;
+				try
+				{
+					channelname = element.Attribute("name").Value;
+				}
+				catch (NullReferenceException)
+				{
+					//do nothing
+				}
+			}
+			//If for some reason we don't have a channel name, fill one in
+			if (String.IsNullOrEmpty(channelname))
+			{
+				channelname = "Unnamed Output " + element.Attribute("output").Value;
 			}
 
 			mappings.Add(new ChannelMapping(channelname,
