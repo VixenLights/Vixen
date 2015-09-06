e1a1cf8f05b299a5ff83d4ac0e02d8cd05f9a037
Jeff Uchitjil <jeffu@kb9kld.org>
2015-09-05 20:49:19 -0500

    Add Color support to the Candle effect along with group level. This has been a long requested feature.

--- Modules/Effect/Candle/CandleModule.cs
+++ Modules/Effect/Candle/CandleModule.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Design;
 using System.Linq;
 using System.Threading;
 using Common.ValueTypes;
+using NLog;
 using Vixen.Attributes;
 using Vixen.Data.Value;
 using Vixen.Intent;
@@ -14,11 +16,13 @@ using Vixen.Sys;
 using Vixen.Sys.Attribute;
 using Vixen.TypeConverters;
 using VixenModules.EffectEditor.EffectDescriptorAttributes;
+using VixenModules.Property.Color;
 
 namespace VixenModules.Effect.Candle
 {
 	public class CandleModule : EffectModuleInstanceBase
 	{
+		private static readonly Logger Logging = LogManager.GetCurrentClassLogger();
 		private EffectIntents _effectIntents;
 		private Random _r;
 		private CandleData _data;
@@ -35,11 +39,46 @@ namespace VixenModules.Effect.Candle
 
 		protected override void TargetNodesChanged()
 		{
-			//Nothing to do
+			CheckForInvalidColorData();
 		}
 
 		[Value]
-		[ProviderCategory(@"Flicker")]
+		[ProviderCategory(@"Config", 1)]
+		[ProviderDisplayName(@"GroupLevel")]
+		[ProviderDescription(@"GroupLevel")]
+		[NumberRange(1, 5000, 1)]
+		[PropertyOrder(1)]
+		public int GroupLevel
+		{
+			get { return _data.GroupLevel; }
+			set
+			{
+				_data.GroupLevel = value;
+				IsDirty = true;
+				OnPropertyChanged();
+			}
+		}
+
+		[Value]
+		[ProviderCategory(@"Color", 2)]
+		[ProviderDisplayName(@"Color")]
+		[ProviderDescription(@"Color")]
+		public Color Color
+		{
+			get
+			{
+				return _data.Color;
+			}
+			set
+			{
+				_data.Color = value;
+				IsDirty = true;
+				OnPropertyChanged();
+			}
+		}
+
+		[Value]
+		[ProviderCategory(@"Flicker",4)]
 		[ProviderDisplayName(@"Frequency")]
 		[ProviderDescription(@"FlickerFrequency")]
 		public int FlickerFrequency
@@ -48,12 +87,13 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.FlickerFrequency = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
 		}
 
 		[Value]
-		[ProviderCategory(@"Flicker")]
+		[ProviderCategory(@"Flicker", 4)]
 		[ProviderDisplayName(@"ChangePercent")]
 		[ProviderDescription(@"ChangePercent")]
 		public Percentage ChangePercentage
@@ -62,12 +102,13 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.ChangePercentage = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
 		}
 
 		[Value]
-		[ProviderCategory(@"Brightness")]
+		[ProviderCategory(@"Brightness",3)]
 		[ProviderDisplayName(@"MinBrightness")]
 		[ProviderDescription(@"MinBrightness")]
 		public Percentage MinLevel
@@ -76,12 +117,13 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.MinLevel = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
 		}
 
 		[Value]
-		[ProviderCategory(@"Brightness")]
+		[ProviderCategory(@"Brightness",3)]
 		[ProviderDisplayName(@"MaxBrightness")]
 		[ProviderDescription(@"MaxBrightness")]
 		public Percentage MaxLevel
@@ -90,12 +132,13 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.MaxLevel = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
 		}
 
 		[Value]
-		[ProviderCategory(@"Flicker")]
+		[ProviderCategory(@"Flicker", 4)]
 		[ProviderDisplayName(@"FlickerPercent")]
 		[ProviderDescription(@"FlickerPercent")]
 		public Percentage FlickerFrequencyDeviationCap
@@ -104,12 +147,13 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.FlickerFrequencyDeviationCap = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
 		}
 
 		[Value]
-		[ProviderCategory(@"Flicker")]
+		[ProviderCategory(@"Flicker", 4)]
 		[ProviderDisplayName(@"DeviationPercent")]
 		[ProviderDescription(@"DeviationPercent")]
 		public Percentage ChangePercentageDeviationCap
@@ -118,25 +162,42 @@ namespace VixenModules.Effect.Candle
 			set
 			{
 				_data.ChangePercentageDeviationCap = value;
+				IsDirty = true;
 				OnPropertyChanged();
 			}
+		}
+
+
+		//Validate that the we are using valid colors and set appropriate defaults if not.
+		private void CheckForInvalidColorData()
+		{
+			HashSet<Color> validColors = new HashSet<Color>();
+			validColors.AddRange(TargetNodes.SelectMany(x => ColorModule.getValidColorsForElementNode(x, true)));
+			if (validColors.Any() && !validColors.Contains(_data.Color))
+			{
+				//Our color is not valid for any elements we have.
+				//Set a default color 
+				Color = validColors.First();
+			}
+
 		}
 
 		protected override void _PreRender(CancellationTokenSource cancellationToken = null)
 		{
 			_effectIntents = new EffectIntents();
 			_r = new Random();
+		
+			var nodes = TargetNodes.SelectMany(x => x.GetLeafEnumerator());
 
-			foreach (Element element in TargetNodes.SelectMany(x => x))
+			var elementGroup = nodes.Select((x, index) => new { x, index })
+					.GroupBy(x => x.index / GroupLevel, y => y.x);
+
+			foreach (IGrouping<int, ElementNode> block in elementGroup)
 			{
 				
-				if (cancellationToken != null && cancellationToken.IsCancellationRequested)
-					return;
-
-				if (element != null)
-					_RenderCandleOnElement(element);
-
+				_RenderCandleOnElements(block.GetElements().ToList());
 			}
+
 		}
 
 		protected override EffectIntents _Render()
@@ -144,40 +205,56 @@ namespace VixenModules.Effect.Candle
 			return _effectIntents;
 		}
 
-		private void _RenderCandleOnElement(Element element)
+		private void _RenderCandleOnElements(List<Element> elements)
 		{
-			float startTime = 0;
-			float endTime = (float) TimeSpan.TotalMilliseconds;
+			TimeSpan startTime = TimeSpan.Zero;
+			double currentLevel = _GenerateStartingLevel();
 
-			float currentLevel = _GenerateStartingLevel();
-
-			while (startTime < endTime) {
+			while (startTime < TimeSpan)
+			{
 				// What will our new value be?
-				float currentLevelChange = _GenerateLevelChange();
-				float nextLevel = currentLevel + currentLevelChange;
+				double currentLevelChange = _GenerateLevelChange();
+				double nextLevel = currentLevel + currentLevelChange;
 
 				// Make sure we're still within our bounds.
 				nextLevel = Math.Max(nextLevel, _data.MinLevel);
 				nextLevel = Math.Min(nextLevel, _data.MaxLevel);
 
 				// How long will this state last?
-				float stateLength = _GenerateStateLength();
-
+				double stateLength = _GenerateStateLength();
+				
 				// Make sure we don't exceed the end of the effect.
-				stateLength = Math.Min(stateLength, endTime - startTime);
+				stateLength = Math.Min(stateLength, (TimeSpan - startTime).TotalMilliseconds);
+				var length = TimeSpan.FromMilliseconds(stateLength);
+				if (length == TimeSpan.Zero)
+				{
+					length = TimeSpan.FromMilliseconds(1);
+				}
+				else
+				{
+					// Add the intent.
+					LightingValue startValue = new LightingValue(Color, currentLevel);
+					LightingValue endValue = new LightingValue(Color, nextLevel);
 
-				// Add the intent.
-				LightingValue startValue = new LightingValue(Color.White, currentLevel);
-				LightingValue endValue = new LightingValue(Color.White, nextLevel);
-				IIntent intent = new LightingIntent(startValue, endValue, TimeSpan.FromMilliseconds(stateLength));
-				try {
-					_effectIntents.AddIntentForElement(element.Id, intent, TimeSpan.FromMilliseconds(startTime));
+					IIntent intent = new LightingIntent(startValue, endValue, length);
+					try
+					{
+						foreach (var element in elements)
+						{
+							if (element != null)
+							{
+								_effectIntents.AddIntentForElement(element.Id, intent, startTime);
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						Logging.Error("Error generating Candle intents", e);
+						throw;
+					}
 				}
-				catch (Exception e) {
-					Console.WriteLine(e.ToString());
-					throw;
-				}
-				startTime += stateLength;
+				
+				startTime += length;	
 				currentLevel = nextLevel;
 			}
 		}
