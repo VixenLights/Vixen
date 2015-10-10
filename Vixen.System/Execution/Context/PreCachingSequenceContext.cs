00d3edc622aa1f186f980c4da4c5fa811869b835
Jeff Uchitjil <jeffu@kb9kld.org>
2015-10-10 09:46:05 -0500

    Code clean up.

--- Vixen.System/Execution/Context/PreCachingSequenceContext.cs
+++ Vixen.System/Execution/Context/PreCachingSequenceContext.cs
@@ -18,8 +18,7 @@ namespace Vixen.Execution.Context
 		private readonly string _name;
 		private SequenceDataPump _dataSource; //Maybe use  
 		private ISequence _sequence;
-		private ISequenceExecutor _sequenceExecutor;
-
+		
 		public PreCachingSequenceContext(string name)
 		{
 			_name = name;
@@ -31,8 +30,7 @@ namespace Vixen.Execution.Context
 			set
 			{
 				_sequence = value;
-				_dataSource = new SequenceDataPump();
-				_dataSource.Sequence = _sequence;
+				_dataSource = new SequenceDataPump {Sequence = _sequence};
 			}
 		}
 
@@ -63,7 +61,7 @@ namespace Vixen.Execution.Context
 
 		protected override ITiming _SequenceTiming
 		{
-			get { return _sequenceExecutor != null ? _sequenceExecutor.TimingSource : null; }
+			get { return null; }
 		}
 
 		public override IExecutor Executor
