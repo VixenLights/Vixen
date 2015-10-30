a4105309bba1fef5400d5f2562090b91365b2ee1
Jeff Uchitjil <jeffu@kb9kld.org>
2015-10-29 23:33:19 -0500

    Some more work on the group box renderer to allow for the border color to be different than the text.

--- Common/Controls/Theme/ThemeGroupBoxRenderer.cs
+++ Common/Controls/Theme/ThemeGroupBoxRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 
@@ -6,24 +7,46 @@ namespace Common.Controls.Theme
 {
 	public sealed class ThemeGroupBoxRenderer
 	{
+		private const int boxHeaderWidth = 7;
+
 		#region Draw GroupBox borders and Text
 
 		public static void GroupBoxesDrawBorder(object sender, PaintEventArgs e, Font f)
 		{
-			//We have visual styles enabled so we can just use the built in stuff to draw with our own colors.
 			GroupBox groupBox = sender as GroupBox;
 			if (groupBox == null) return;
-
-			GroupBoxState gbState = groupBox.Enabled ? GroupBoxState.Normal : GroupBoxState.Disabled; 
-			TextFormatFlags textFlags = TextFormatFlags.Default | TextFormatFlags.TextBoxControl | TextFormatFlags.WordBreak | TextFormatFlags.PreserveGraphicsTranslateTransform | TextFormatFlags.PreserveGraphicsClipping;
-
-			if (groupBox.RightToLeft == RightToLeft.Yes) {
-				textFlags |= (TextFormatFlags.Right | TextFormatFlags.RightToLeft); 
-			}
-
-			Color textcolor = groupBox.Enabled ? ThemeColorTable.ForeColor : ThemeColorTable.ForeColorDisabled; 
-			GroupBoxRenderer.DrawGroupBox(e.Graphics, new Rectangle(0, 0, groupBox.Width, groupBox.Height), groupBox.Text, f, textcolor, textFlags, gbState);
+			var g = e.Graphics;
 			
+			Color textColor = groupBox.Enabled ? ThemeColorTable.ForeColor : ThemeColorTable.ForeColorDisabled;
+
+
+			Brush textBrush = new SolidBrush(textColor);
+			Brush borderBrush = new SolidBrush(ThemeColorTable.GroupBoxBorderColor);
+			Pen borderPen = new Pen(borderBrush);
+			SizeF strSize = g.MeasureString(groupBox.Text, groupBox.Font);
+			Rectangle rect = new Rectangle(groupBox.ClientRectangle.X,
+										   groupBox.ClientRectangle.Y + (int)(strSize.Height / 2),
+										   groupBox.ClientRectangle.Width - 2,
+										   groupBox.ClientRectangle.Height - (int)(strSize.Height / 2) - 1);
+
+			// Clear text and border
+			g.Clear(groupBox.BackColor);
+
+			// Draw text
+			g.DrawString(groupBox.Text, groupBox.Font, textBrush, groupBox.Padding.Left, 0);
+
+			// Drawing Border
+			//Left
+			g.DrawLine(borderPen, rect.Location, new Point(rect.X, rect.Y + rect.Height));
+			//Right
+			g.DrawLine(borderPen, new Point(rect.X + rect.Width, rect.Y), new Point(rect.X + rect.Width, rect.Y + rect.Height));
+			//Bottom
+			g.DrawLine(borderPen, new Point(rect.X, rect.Y + rect.Height), new Point(rect.X + rect.Width, rect.Y + rect.Height));
+			//Top1
+			g.DrawLine(borderPen, new Point(rect.X, rect.Y), new Point(rect.X + groupBox.Padding.Left, rect.Y));
+			//Top2
+			g.DrawLine(borderPen, new Point(rect.X + groupBox.Padding.Left + (int)(strSize.Width), rect.Y), new Point(rect.X + rect.Width, rect.Y));
+
 		}
 
 		#endregion
