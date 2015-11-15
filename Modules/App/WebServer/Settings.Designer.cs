73578765b763d0bb3f18b724a82271b17f1cf063
Jeff Uchitjil <jeffu@kb9kld.org>
2015-11-14 21:44:50 -0600

    Clean up the theme in the web server setup form. Fix link color to be more readable. Fix logo font size and position.

--- Modules/App/WebServer/Settings.Designer.cs
+++ Modules/App/WebServer/Settings.Designer.cs
@@ -46,7 +46,7 @@
 			// 
 			// numericUpDown1
 			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(152, 139);
+			this.numericUpDown1.Location = new System.Drawing.Point(177, 160);
 			this.numericUpDown1.Maximum = new decimal(new int[] {
             64000,
             0,
@@ -58,7 +58,7 @@
             0,
             0});
 			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+			this.numericUpDown1.Size = new System.Drawing.Size(62, 23);
 			this.numericUpDown1.TabIndex = 0;
 			this.numericUpDown1.Value = new decimal(new int[] {
             8000,
@@ -70,9 +70,9 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(94, 141);
+			this.label1.Location = new System.Drawing.Point(110, 163);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(52, 13);
+			this.label1.Size = new System.Drawing.Size(59, 15);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Http Port:";
 			// 
@@ -80,9 +80,9 @@
 			// 
 			this.checkBox1.AutoSize = true;
 			this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-			this.checkBox1.Location = new System.Drawing.Point(97, 165);
+			this.checkBox1.Location = new System.Drawing.Point(113, 190);
 			this.checkBox1.Name = "checkBox1";
-			this.checkBox1.Size = new System.Drawing.Size(73, 17);
+			this.checkBox1.Size = new System.Drawing.Size(76, 19);
 			this.checkBox1.TabIndex = 3;
 			this.checkBox1.Text = "IsEnabled";
 			this.checkBox1.UseVisualStyleBackColor = true;
@@ -92,7 +92,7 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(168, 89);
+			this.label2.Location = new System.Drawing.Point(196, 103);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(125, 25);
 			this.label2.TabIndex = 5;
@@ -101,9 +101,10 @@
 			// linkLabel1
 			// 
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(12, 210);
+			this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.linkLabel1.Location = new System.Drawing.Point(14, 252);
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+			this.linkLabel1.Size = new System.Drawing.Size(60, 15);
 			this.linkLabel1.TabIndex = 6;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "linkLabel1";
@@ -112,9 +113,9 @@
 			// btnCancel
 			// 
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(227, 281);
+			this.btnCancel.Location = new System.Drawing.Point(265, 324);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.Size = new System.Drawing.Size(87, 27);
 			this.btnCancel.TabIndex = 8;
 			this.btnCancel.Text = "Cancel";
 			this.btnCancel.UseVisualStyleBackColor = true;
@@ -124,9 +125,9 @@
 			// btnOk
 			// 
 			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btnOk.Location = new System.Drawing.Point(146, 281);
+			this.btnOk.Location = new System.Drawing.Point(170, 324);
 			this.btnOk.Name = "btnOk";
-			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.Size = new System.Drawing.Size(87, 27);
 			this.btnOk.TabIndex = 9;
 			this.btnOk.Text = "OK";
 			this.btnOk.UseVisualStyleBackColor = true;
@@ -136,54 +137,54 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 197);
+			this.label3.Location = new System.Drawing.Point(14, 227);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(121, 13);
+			this.label3.Size = new System.Drawing.Size(136, 15);
 			this.label3.TabIndex = 10;
 			this.label3.Text = "Point your browser here:";
 			// 
 			// lblDisclaimer
 			// 
 			this.lblDisclaimer.AutoSize = true;
-			this.lblDisclaimer.Location = new System.Drawing.Point(12, 180);
+			this.lblDisclaimer.Location = new System.Drawing.Point(14, 208);
 			this.lblDisclaimer.Name = "lblDisclaimer";
-			this.lblDisclaimer.Size = new System.Drawing.Size(0, 13);
+			this.lblDisclaimer.Size = new System.Drawing.Size(0, 15);
 			this.lblDisclaimer.TabIndex = 11;
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(4, 2);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.pictureBox1.Location = new System.Drawing.Point(5, 2);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(307, 129);
+			this.pictureBox1.Size = new System.Drawing.Size(358, 149);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 15;
 			this.pictureBox1.TabStop = false;
 			// 
 			// label4
 			// 
-			this.label4.Location = new System.Drawing.Point(12, 243);
+			this.label4.Location = new System.Drawing.Point(14, 280);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(290, 35);
+			this.label4.Size = new System.Drawing.Size(338, 40);
 			this.label4.TabIndex = 16;
 			this.label4.Text = "Turn off the web server when not in use to conserve resources.";
 			// 
 			// labelVixen
 			// 
 			this.labelVixen.AutoSize = true;
-			this.labelVixen.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelVixen.Location = new System.Drawing.Point(116, 2);
+			this.labelVixen.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Bold);
+			this.labelVixen.Location = new System.Drawing.Point(136, 9);
 			this.labelVixen.Name = "labelVixen";
-			this.labelVixen.Size = new System.Drawing.Size(205, 76);
+			this.labelVixen.Size = new System.Drawing.Size(227, 85);
 			this.labelVixen.TabIndex = 18;
 			this.labelVixen.Text = "Vixen";
 			// 
 			// Settings
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(314, 316);
+			this.ClientSize = new System.Drawing.Size(366, 366);
 			this.Controls.Add(this.labelVixen);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.lblDisclaimer);
@@ -196,8 +197,8 @@
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.numericUpDown1);
 			this.Controls.Add(this.pictureBox1);
-			this.MaximumSize = new System.Drawing.Size(330, 355);
-			this.MinimumSize = new System.Drawing.Size(330, 355);
+			this.MaximumSize = new System.Drawing.Size(382, 404);
+			this.MinimumSize = new System.Drawing.Size(382, 404);
 			this.Name = "Settings";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "Configuration";
