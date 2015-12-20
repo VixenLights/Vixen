980044d34f7b8542752babc93af84bb9553c0d49
Jeff Uchitjil <jeffu@kb9kld.org>
2015-12-20 09:23:06 -0600

    Make color name strings static so there is only one instance across all the filters.

--- Modules/Property/Color/ColorSetupHelper.cs
+++ Modules/Property/Color/ColorSetupHelper.cs
@@ -20,9 +20,9 @@ namespace VixenModules.Property.Color
 	public partial class ColorSetupHelper : BaseForm, IElementSetupHelper
 	{
 		private static NLog.Logger Logging = NLog.LogManager.GetCurrentClassLogger();
-		private const string Red = "Red";
-		private const string Green = "Green";
-		private const string Blue = "Blue";
+		private static readonly string Red = "Red";
+		private static readonly string Green = "Green";
+		private static readonly string Blue = "Blue";
 
 		public ColorSetupHelper()
 		{
