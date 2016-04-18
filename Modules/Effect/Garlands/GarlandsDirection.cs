f10b0fe161f0c09b2895f1b909c4b78743fc8ce0
gnarmstr <gnarmstr@hotmail.com>
2016-04-18 23:01:35 +1000

    VIX-1009 Create Native Garlands effect off the Vixen Nutcracker effect. Added Gradient, Brightness, Movement Type (Speed/Iterations) and Direction control.

---
+++ Modules/Effect/Garlands/GarlandsDirection.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace VixenModules.Effect.Garlands
+{
+	public enum GarlandsDirection
+	{
+		[Description("Up")]
+		Up,
+		[Description("Down")]
+		Down,
+		[Description("Left")]
+		Left,
+		[Description("Right")]
+		Right
+	}
+}
---
+++ Modules/Effect/Garlands/MovementType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace VixenModules.Effect.Garlands
+{
+	public enum MovementType
+	{
+		[Description("Iterations")]
+		Iterations,
+		[Description("Speed")]
+		Speed
+	}
+}
