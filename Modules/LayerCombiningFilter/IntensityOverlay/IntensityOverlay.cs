751bacce69b5b9edb9520028f564a2b240e36863
Jeff Uchitjil <jeffu@kb9kld.org>
2016-07-03 11:55:39 -0500

    Add the basics for a intensity factor mixing filter. This probably needs a better name which I can fix later before merging to the mainline.

---
+++ Modules/LayerCombiningFilter/IntensityOverlay/IntensityOverlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using Common.Controls.ColorManagement.ColorModels;
+using Vixen.Data.Value;
+using Vixen.Module.MixingFilter;
+
+namespace VixenModules.LayerMixingFilter.IntensityOverlay
+{
+	public class IntensityOverlay : LayerMixingFilterModuleInstanceBase
+	{
+		
+		public override DiscreteValue CombineDiscreteIntensity(DiscreteValue highLayerValue, DiscreteValue lowLayerValue)
+		{
+			lowLayerValue.Intensity = lowLayerValue.Intensity * highLayerValue.Intensity;
+			return lowLayerValue;
+		}
+
+		public override Color CombineFullColor(Color highLayerColor, Color lowLayerColor)
+		{
+			var newV = HSV.VFromRgb(highLayerColor);
+			HSV color = HSV.FromRGB(lowLayerColor);
+			color.V = color.V * newV;
+			return color.ToRGB();
+		}
+	}
+
+	
+}
