542e24ef674f51f0214bb1635a273fe9eb4235f5
Jeff Uchitjil <jeffu@kb9kld.org>
2016-07-11 18:29:08 -0500

    Fix the super nudge layout in the mark manager. Does anyone use this?

--- Modules/Editor/TimedSequenceEditor/Form_Marks.Designer.cs
+++ Modules/Editor/TimedSequenceEditor/Form_Marks.Designer.cs
@@ -37,7 +37,6 @@
 			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
 			this.numericUpDownStandardNudge = new System.Windows.Forms.NumericUpDown();
 			this.numericUpDownSuperNudge = new System.Windows.Forms.NumericUpDown();
-			this.panel1 = new System.Windows.Forms.Panel();
 			this.label3 = new System.Windows.Forms.Label();
 			this.listViewMarkCollections = new System.Windows.Forms.ListView();
 			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -45,11 +44,12 @@
 			this.boldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.dottedSolidToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.changeColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			this.toolStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownStandardNudge)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownSuperNudge)).BeginInit();
-			this.panel1.SuspendLayout();
 			this.contextMenuStrip1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// toolStrip1
@@ -100,7 +100,7 @@
 			// numericUpDownStandardNudge
 			// 
 			this.numericUpDownStandardNudge.AutoSize = true;
-			this.numericUpDownStandardNudge.Location = new System.Drawing.Point(45, 15);
+			this.numericUpDownStandardNudge.Location = new System.Drawing.Point(45, 2);
 			this.numericUpDownStandardNudge.Margin = new System.Windows.Forms.Padding(2);
 			this.numericUpDownStandardNudge.Maximum = new decimal(new int[] {
             1000,
@@ -116,7 +116,7 @@
 			// numericUpDownSuperNudge
 			// 
 			this.numericUpDownSuperNudge.AutoSize = true;
-			this.numericUpDownSuperNudge.Location = new System.Drawing.Point(99, 15);
+			this.numericUpDownSuperNudge.Location = new System.Drawing.Point(90, 2);
 			this.numericUpDownSuperNudge.Margin = new System.Windows.Forms.Padding(2);
 			this.numericUpDownSuperNudge.Maximum = new decimal(new int[] {
             1000,
@@ -129,18 +129,6 @@
 			this.toolTip1.SetToolTip(this.numericUpDownSuperNudge, "Super Nudge in Miliseconds");
 			this.numericUpDownSuperNudge.ValueChanged += new System.EventHandler(this.numericUpDownSuperNudge_ValueChanged);
 			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.label3);
-			this.panel1.Controls.Add(this.numericUpDownSuperNudge);
-			this.panel1.Controls.Add(this.numericUpDownStandardNudge);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(0, 263);
-			this.panel1.Margin = new System.Windows.Forms.Padding(2);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(192, 44);
-			this.panel1.TabIndex = 6;
-			// 
 			// label3
 			// 
 			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -148,7 +136,7 @@
 			this.label3.AutoSize = true;
 			this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
 			this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(221)))), ((int)(((byte)(221)))));
-			this.label3.Location = new System.Drawing.Point(2, 17);
+			this.label3.Location = new System.Drawing.Point(2, 0);
 			this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(39, 13);
@@ -171,7 +159,7 @@
 			this.listViewMarkCollections.Location = new System.Drawing.Point(0, 25);
 			this.listViewMarkCollections.Margin = new System.Windows.Forms.Padding(2);
 			this.listViewMarkCollections.Name = "listViewMarkCollections";
-			this.listViewMarkCollections.Size = new System.Drawing.Size(192, 238);
+			this.listViewMarkCollections.Size = new System.Drawing.Size(192, 331);
 			this.listViewMarkCollections.TabIndex = 8;
 			this.listViewMarkCollections.UseCompatibleStateImageBehavior = false;
 			this.listViewMarkCollections.View = System.Windows.Forms.View.Details;
@@ -220,6 +208,17 @@
 			this.changeColorToolStripMenuItem.ToolTipText = "Change Mark Collection color";
 			this.changeColorToolStripMenuItem.Click += new System.EventHandler(this.changeColorToolStripMenuItem_Click);
 			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.label3);
+			this.flowLayoutPanel1.Controls.Add(this.numericUpDownStandardNudge);
+			this.flowLayoutPanel1.Controls.Add(this.numericUpDownSuperNudge);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 321);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(192, 35);
+			this.flowLayoutPanel1.TabIndex = 9;
+			// 
 			// Form_Marks
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -227,10 +226,10 @@
 			this.AutoScroll = true;
 			this.AutoSize = true;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
-			this.ClientSize = new System.Drawing.Size(192, 307);
+			this.ClientSize = new System.Drawing.Size(192, 356);
 			this.ControlBox = false;
+			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.listViewMarkCollections);
-			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.toolStrip1);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -243,9 +242,9 @@
 			this.toolStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownStandardNudge)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDownSuperNudge)).EndInit();
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
 			this.contextMenuStrip1.ResumeLayout(false);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -258,7 +257,6 @@
 		private System.Windows.Forms.ToolTip toolTip1;
 		private System.Windows.Forms.ToolStripButton toolStripButtonAddMarkCollection;
 		private System.Windows.Forms.ToolStripButton toolStripButtonDeleteMarkCollection;
-		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.NumericUpDown numericUpDownSuperNudge;
 		private System.Windows.Forms.NumericUpDown numericUpDownStandardNudge;
 		private System.Windows.Forms.ListView listViewMarkCollections;
@@ -268,5 +266,6 @@
 		private System.Windows.Forms.ToolStripMenuItem boldToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem dottedSolidToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem changeColorToolStripMenuItem;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 	}
 }
