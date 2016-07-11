fd2269d4d4fec4b54cfa9bbf5a13dd256d615b94
Jeff Uchitjil <jeffu@kb9kld.org>
2016-07-10 19:43:26 -0500

    Change message box to use a rich text box that behaves better.

--- Common/Controls/MessageBoxForm.Designer.cs
+++ Common/Controls/MessageBoxForm.Designer.cs
@@ -32,9 +32,9 @@ namespace Common.Controls
 			this.buttonCancel = new System.Windows.Forms.Button();
 			this.buttonNo = new System.Windows.Forms.Button();
 			this.messageIcon = new System.Windows.Forms.PictureBox();
-			this.txtMessage = new System.Windows.Forms.TextBox();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.txtMessage = new System.Windows.Forms.RichTextBox();
 			((System.ComponentModel.ISupportInitialize)(this.messageIcon)).BeginInit();
 			this.flowLayoutPanel1.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
@@ -44,14 +44,15 @@ namespace Common.Controls
 			// 
 			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonOk.AutoSize = true;
-			this.buttonOk.BackColor = System.Drawing.SystemColors.Control;
+			this.buttonOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
 			this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.buttonOk.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
 			this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonOk.ForeColor = System.Drawing.SystemColors.ControlText;
-			this.buttonOk.Location = new System.Drawing.Point(294, 3);
+			this.buttonOk.Location = new System.Drawing.Point(544, 6);
+			this.buttonOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.buttonOk.Name = "buttonOk";
-			this.buttonOk.Size = new System.Drawing.Size(93, 29);
+			this.buttonOk.Size = new System.Drawing.Size(173, 79);
 			this.buttonOk.TabIndex = 2;
 			this.buttonOk.Text = "OK";
 			this.buttonOk.UseVisualStyleBackColor = false;
@@ -67,9 +68,10 @@ namespace Common.Controls
 			this.buttonCancel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
 			this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonCancel.ForeColor = System.Drawing.Color.WhiteSmoke;
-			this.buttonCancel.Location = new System.Drawing.Point(96, 3);
+			this.buttonCancel.Location = new System.Drawing.Point(145, 6);
+			this.buttonCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(93, 29);
+			this.buttonCancel.Size = new System.Drawing.Size(202, 79);
 			this.buttonCancel.TabIndex = 3;
 			this.buttonCancel.Text = "CANCEL";
 			this.buttonCancel.UseVisualStyleBackColor = true;
@@ -85,9 +87,10 @@ namespace Common.Controls
 			this.buttonNo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
 			this.buttonNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.buttonNo.ForeColor = System.Drawing.Color.WhiteSmoke;
-			this.buttonNo.Location = new System.Drawing.Point(195, 3);
+			this.buttonNo.Location = new System.Drawing.Point(359, 6);
+			this.buttonNo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.buttonNo.Name = "buttonNo";
-			this.buttonNo.Size = new System.Drawing.Size(93, 29);
+			this.buttonNo.Size = new System.Drawing.Size(173, 79);
 			this.buttonNo.TabIndex = 4;
 			this.buttonNo.Text = "NO";
 			this.buttonNo.UseVisualStyleBackColor = true;
@@ -97,33 +100,14 @@ namespace Common.Controls
 			// 
 			// messageIcon
 			// 
-			this.messageIcon.Location = new System.Drawing.Point(2, 2);
-			this.messageIcon.Margin = new System.Windows.Forms.Padding(2);
+			this.messageIcon.Location = new System.Drawing.Point(4, 4);
+			this.messageIcon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.messageIcon.Name = "messageIcon";
-			this.messageIcon.Size = new System.Drawing.Size(95, 89);
+			this.messageIcon.Size = new System.Drawing.Size(175, 190);
 			this.messageIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.messageIcon.TabIndex = 5;
 			this.messageIcon.TabStop = false;
 			this.messageIcon.Paint += new System.Windows.Forms.PaintEventHandler(this.messageIcon_Paint);
-			// 
-			// txtMessage
-			// 
-			this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.txtMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
-			this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
-			this.txtMessage.Cursor = System.Windows.Forms.Cursors.Default;
-			this.txtMessage.Location = new System.Drawing.Point(102, 10);
-			this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
-			this.txtMessage.MinimumSize = new System.Drawing.Size(290, 60);
-			this.txtMessage.Multiline = true;
-			this.txtMessage.Name = "txtMessage";
-			this.txtMessage.ReadOnly = true;
-			this.txtMessage.Size = new System.Drawing.Size(291, 81);
-			this.txtMessage.TabIndex = 6;
-			this.txtMessage.TabStop = false;
-			this.txtMessage.Text = "{ Message }";
-			this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
 			// 
 			// flowLayoutPanel1
 			// 
@@ -135,9 +119,10 @@ namespace Common.Controls
 			this.flowLayoutPanel1.Controls.Add(this.buttonCancel);
 			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 97);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 204);
+			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(390, 54);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(723, 116);
 			this.flowLayoutPanel1.TabIndex = 7;
 			// 
 			// tableLayoutPanel1
@@ -150,24 +135,40 @@ namespace Common.Controls
 			this.tableLayoutPanel1.Controls.Add(this.txtMessage, 1, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 2;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(396, 139);
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 128F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(735, 297);
 			this.tableLayoutPanel1.TabIndex = 8;
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+			this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtMessage.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtMessage.Location = new System.Drawing.Point(186, 10);
+			this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 10, 10, 3);
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.ReadOnly = true;
+			this.txtMessage.Size = new System.Drawing.Size(539, 185);
+			this.txtMessage.TabIndex = 8;
+			this.txtMessage.Text = "";
 			// 
 			// MessageBoxForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
-			this.ClientSize = new System.Drawing.Size(396, 139);
+			this.ClientSize = new System.Drawing.Size(735, 297);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.KeyPreview = true;
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "MessageBoxForm";
@@ -191,8 +192,8 @@ namespace Common.Controls
 		private Button buttonCancel;
 		private Button buttonNo;
 		private PictureBox messageIcon;
-		private TextBox txtMessage;
 		private FlowLayoutPanel flowLayoutPanel1;
 		private TableLayoutPanel tableLayoutPanel1;
+		private RichTextBox txtMessage;
 	}
 }
