585b7181a6c17bb2ec940785c48ab16a1282946e
Jchuchla <Jon@chuchla.com>
2016-07-12 23:25:11 -0500

    trying to commit initial MultiplyColor mixing filter to my branch.

---
+++ Modules/LayerCombiningFilter/MultiplyColor/MultiplyColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using Common.Controls.ColorManagement.ColorModels;
+using Vixen.Data.Value;
+using Vixen.Module.MixingFilter;
+
+namespace VixenModules.LayerMixingFilter.MultiplyColor
+{
+    public class MultiplyColor : LayerMixingFilterModuleInstanceBase
+	{
+
+        public override DiscreteValue CombineDiscreteIntensity(DiscreteValue highLayerValue, DiscreteValue lowLayerValue)
+        {
+            lowLayerValue.Intensity = lowLayerValue.Intensity * highLayerValue.Intensity;
+            return lowLayerValue;
+        }
+
+        public override Color CombineFullColor(Color highLayerColor, Color lowLayerColor)
+        {
+            int newR , newG, newB;
+            if(highLayerColor.R >0)
+                newR = (highLayerColor.R * lowLayerColor.R)/256;
+            else
+                newR=lowLayerColor.R;
+            if(highLayerColor.G >0)
+                newG = (highLayerColor.G * lowLayerColor.G)/256;
+            else
+                newG=lowLayerColor.G;
+            if(highLayerColor.B>0)
+                newB = (highLayerColor.B * lowLayerColor.B)/256;
+            else
+                newB=lowLayerColor.B;
+            return Color.FromArgb(newR, newG, newB);
+        }
+	}
+
+	
+}
---
+++ Modules/LayerCombiningFilter/MultiplyColor/MultiplyColorModuleDescriptor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Vixen.Module.MixingFilter;
+using VixenModules.LayerMixingFilter.MultiplyColor;
+
+namespace VixenModules.LayerCombiningFilter.Mask
+{
+    public class MultiplyColorModuleDescriptor : LayerMixingFilterModuleDescriptorBase
+	{
+		private static readonly Guid _typeId = new Guid("{0C9DFC0B-8E39-4167-8036-79408C6B0B35}");
+
+		public override string TypeName
+		{
+			get { return "Multiply Color"; }
+		}
+
+		public override Guid TypeId
+		{
+			get { return _typeId; }
+		}
+
+		public override Type ModuleClass
+		{
+            get { return typeof(MultiplyColor); }
+		}
+
+		public override string Author
+		{
+			get { return "Jon Chuchla"; }
+		}
+
+		public override string Description
+		{
+			get
+			{
+				return
+					"Multiplies the colors between layers.  Useful for modifying a color or brightness with another layer as input.";
+			}
+		}
+
+		public override string Version
+		{
+			get { return "1.0"; }
+		}
+	}
+}
