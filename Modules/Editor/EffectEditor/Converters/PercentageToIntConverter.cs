1983d26e608564b535c726fa76a194b4f1883fb9
Jeff Uchitjil <jeffu@kb9kld.org>
2016-11-17 07:17:03 -0600

    VIX-1886 Add the tool tip with the value on thumb hover so you don't have to click to determine the actual value.

---
+++ Modules/Editor/EffectEditor/Converters/PercentageToIntConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Common.ValueTypes;
+
+namespace VixenModules.Editor.EffectEditor.Converters
+{
+	public class PercentageToIntConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is Percentage)
+			{
+				Percentage p = (Percentage)value;
+				return System.Convert.ToInt32(p.Value * 100);
+			}
+			return System.Convert.ToInt32(value) * 100;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			float f = System.Convert.ToSingle(value) / 100;
+			if (f < 0 || f > 1)
+			{
+				f = 1;
+			}
+			return new Percentage(f);
+		}
+	}
+}
