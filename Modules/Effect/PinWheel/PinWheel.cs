29f88cf65fea8552016907dc132f0c4e8935be02
Jeff Uchitjil <jeffu@kb9kld.org>
2017-04-29 15:18:27 -0500

    VIX-2034 Add information links to the website for documentation on the effect. Correct line endings.

--- Modules/Effect/PinWheel/PinWheel.cs
+++ Modules/Effect/PinWheel/PinWheel.cs
@@ -310,6 +310,20 @@ namespace VixenModules.Effect.PinWheel
 		}
 		#endregion
 
+		#region Information
+
+		public override string Information
+		{
+			get { return "Visit the Vixen Lights website for more information on this effect."; }
+		}
+
+		public override string InformationLink
+		{
+			get { return "http://www.vixenlights.com/vixen-3-documentation/sequencer/effects/pinwheel/"; }
+		}
+
+		#endregion
+
 		public override IModuleDataModel ModuleData
 		{
 			get { return _data; }
