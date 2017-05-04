eff0ecb4fc628e5dfc522e3e92032cd252495fc5
gnarmstr <gnarmstr@hotmail.com>
2017-05-04 22:36:15 +1000

    VIX-2046 - Shockwave - Add More info link to Vixen website for this effect.

--- Modules/Effect/Shockwave/Shockwave.cs
+++ Modules/Effect/Shockwave/Shockwave.cs
@@ -203,6 +203,20 @@ namespace VixenModules.Effect.Shockwave
 
 		#endregion
 
+		#region Information
+
+		public override string Information
+		{
+			get { return "Visit the Vixen Lights website for more information on this effect."; }
+		}
+
+		public override string InformationLink
+		{
+			get { return "http://www.vixenlights.com/vixen-3-documentation/sequencer/effects/shockwave/"; }
+		}
+
+		#endregion
+
 		public override IModuleDataModel ModuleData
 		{
 			get { return _data; }
