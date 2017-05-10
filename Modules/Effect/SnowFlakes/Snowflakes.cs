5863e6a5cc3f18e3a6485f8ffe97ca00a96ada64
gnarmstr <gnarmstr@hotmail.com>
2017-05-10 21:29:32 +1000

    VIX-2047 - Snowflakes - Add more info link to Vixen website.

--- Modules/Effect/SnowFlakes/Snowflakes.cs
+++ Modules/Effect/SnowFlakes/Snowflakes.cs
@@ -377,6 +377,20 @@ namespace VixenModules.Effect.Snowflakes
 
 		#endregion
 
+		#region Information
+
+		public override string Information
+		{
+			get { return "Visit the Vixen Lights website for more information on this effect."; }
+		}
+
+		public override string InformationLink
+		{
+			get { return "http://www.vixenlights.com/vixen-3-documentation/sequencer/effects/snowflakes/"; }
+		}
+
+		#endregion
+
 		public override IModuleDataModel ModuleData
 		{
 			get { return _data; }
