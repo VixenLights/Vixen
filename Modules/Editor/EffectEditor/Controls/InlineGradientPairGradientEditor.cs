ba8d6152f0bf2660ec2fbbd36660ceb8425e9fc7
Jeff Uchitjil <jeffu@kb9kld.org>
2017-05-12 20:23:34 -0500

    VIX-2035 Add the editor class for GradientLevelPair editor.

---
+++ Modules/Editor/EffectEditor/Controls/InlineGradientPairGradientEditor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using VixenModules.App.ColorGradients;
+
+namespace VixenModules.Editor.EffectEditor.Controls
+{
+	public class InlineGradientPairGradientEditor:BaseInlineGradientEditor
+	{
+
+		private static readonly Type ThisType = typeof(InlineGradientEditor);
+
+		#region Dependency Property Fields
+
+		/// <summary>
+		/// Identifies the <see cref="Value"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(GradientLevelPair),
+			ThisType, new PropertyMetadata(new GradientLevelPair(), ValueChanged));
+
+		#endregion Dependency Property Fields
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the value. This is a dependency property.
+		/// </summary>
+		/// <value>The value.</value>
+		public GradientLevelPair Value
+		{
+			get { return (GradientLevelPair)GetValue(ValueProperty); }
+			set { SetValue(ValueProperty, value); }
+		}
+
+		#endregion Properties
+
+		#region Property Changed Callbacks
+
+		private static void ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var inlineGradientEditor = (InlineGradientPairGradientEditor)d;
+			if (!inlineGradientEditor.IsInitialized)
+				return;
+			inlineGradientEditor.Value = (GradientLevelPair)e.NewValue;
+			inlineGradientEditor.OnGradientValueChanged();
+		}
+
+		#endregion Property Changed Callbacks
+
+
+		protected override ColorGradient GetColorGradientValue()
+		{
+			return Value.ColorGradient;
+		}
+
+		protected override void SetColorGradientValue(ColorGradient cg)
+		{
+			Value = new GradientLevelPair(cg, Value.Curve);
+		}
+	}
+}
