12224a07abdbbae284f7bcaba66da09cda3fb235
gnarmstr <gnarmstr@hotmail.com>
2017-06-17 17:42:53 +1000

    VIX-2110 - Balls effect - Add web page link to effect.

--- Modules/Effect/Balls/Balls.cs
+++ Modules/Effect/Balls/Balls.cs
@@ -321,7 +321,19 @@ namespace VixenModules.Effect.Balls
 
 		#endregion
 
+		#region Information
 
+		public override string Information
+		{
+			get { return "Visit the Vixen Lights website for more information on this effect."; }
+		}
+
+		public override string InformationLink
+		{
+			get { return "http://www.vixenlights.com/vixen-3-documentation/sequencer/effects/balls/"; }
+		}
+
+		#endregion
 
 		public override IModuleDataModel ModuleData
 		{
