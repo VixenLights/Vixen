5fff3ce77428017fa51c6875664e1adfd44ac1c7
Jeff Uchitjil <jeffu@kb9kld.org>
2017-07-27 19:47:57 -0500

    Start of a Wizard to handle bulk exports of sequences.

---
+++ Application/VixenApplication/BulkExport/BulkExportSources.cs
@@ -0,0 +1,12 @@
+﻿using Common.Controls.Wizard;
+
+namespace VixenApplication.BulkExport
+{
+	public partial class BulkExportSources : WizardStage
+	{
+		public BulkExportSources()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Application/VixenApplication/BulkExport/BulkExportWizard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Common.Controls.Wizard;
+
+namespace VixenApplication.BulkExport
+{
+	internal class BulkExportWizard : Wizard
+	{
+
+		private readonly List<WizardStage> _stages;
+
+		public BulkExportWizard()
+		{
+			_stages = new List<WizardStage>
+			{
+				new BulkExportSources()
+			};
+		}
+
+		protected override List<WizardStage> Stages
+		{
+			get { return _stages; }
+		}
+
+		public override string WizardTitle
+		{
+			get { return "Export Wizard"; }
+		}
+	}
+}
