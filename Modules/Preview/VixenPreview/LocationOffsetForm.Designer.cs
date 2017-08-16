b093aa555588e2a7df83f37d33d633a28bd739e8
jchuchla <jon@chuchla.com>
2017-08-16 13:23:09 -0500

    Adjusted setup form and retested functionality

--- Modules/Preview/VixenPreview/LocationOffsetForm.Designer.cs
+++ Modules/Preview/VixenPreview/LocationOffsetForm.Designer.cs
@@ -28,80 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtY = new Common.Controls.NumericTextBox();
+            this.txtX = new Common.Controls.NumericTextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.txtX = new Common.Controls.NumericTextBox();
-            this.txtY = new Common.Controls.NumericTextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnCancel = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 15);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Horizontal";
-            // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 56);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 15);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Vertical";
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(83, 94);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 27);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtX
-            // 
-            this.txtX.AllowSpace = false;
-            this.tableLayoutPanel1.SetColumnSpan(this.txtX, 2);
-            this.txtX.Location = new System.Drawing.Point(71, 23);
-            this.txtX.Name = "txtX";
-            this.txtX.Size = new System.Drawing.Size(99, 23);
-            this.txtX.TabIndex = 4;
-            this.txtX.TextChanged += new System.EventHandler(this.txtX_TextChanged);
-            // 
-            // txtY
-            // 
-            this.txtY.AllowSpace = false;
-            this.tableLayoutPanel1.SetColumnSpan(this.txtY, 2);
-            this.txtY.Location = new System.Drawing.Point(71, 52);
-            this.txtY.Name = "txtY";
-            this.txtY.Size = new System.Drawing.Size(99, 23);
-            this.txtY.TabIndex = 5;
-            // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.txtY, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btnCancel, 3, 4);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 4);
-            this.tableLayoutPanel1.Controls.Add(this.txtX, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtX, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnCancel, 1, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -111,25 +58,76 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(266, 124);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(236, 124);
             this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // txtY
+            // 
+            this.txtY.AllowSpace = false;
+            this.txtY.Location = new System.Drawing.Point(121, 52);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(63, 23);
+            this.txtY.TabIndex = 5;
+            // 
+            // txtX
+            // 
+            this.txtX.AllowSpace = false;
+            this.txtX.Location = new System.Drawing.Point(121, 23);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(63, 23);
+            this.txtX.TabIndex = 4;
+            this.txtX.TextChanged += new System.EventHandler(this.txtX_TextChanged);
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(176, 94);
+            this.btnCancel.Location = new System.Drawing.Point(121, 94);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(87, 27);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Horizontal";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Vertical";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(28, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // LocationOffsetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 124);
+            this.ClientSize = new System.Drawing.Size(236, 124);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "LocationOffsetForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -142,12 +140,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button1;
-		private Common.Controls.NumericTextBox txtX;
-		private Common.Controls.NumericTextBox txtY;
-		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-		private System.Windows.Forms.Button btnCancel;
+        private Common.Controls.NumericTextBox txtY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private Common.Controls.NumericTextBox txtX;
+        private System.Windows.Forms.Button btnCancel;
 	}
 }
