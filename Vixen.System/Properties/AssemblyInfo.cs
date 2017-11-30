4f1ff0ad6780625f2f4d6cc52460b673efdf68e6
Jeff Uchitjil <jeffu@kb9kld.org>
2017-11-30 06:16:52 -0600

    Prep for 3.4u4 development cycle.

--- Vixen.System/Properties/AssemblyInfo.cs
+++ Vixen.System/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.4.0.1")]
-[assembly: AssemblyFileVersion("3.4.0.1")]
+[assembly: AssemblyVersion("3.4.0.4")]
+[assembly: AssemblyFileVersion("3.4.0.4")]
