f985b6a904471bbed9f0907b0caa5a50efc96e2f
Jeff Uchitjil <jeffu@kb9kld.org>
2018-05-23 21:19:47 -0500

    VIX-2388 Add the logic and the setup screen to be able to drag and drop arrange the preferred patching order for elements.

---
+++ Modules/Property/Order/OrderSetupHelper.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace VixenModules.Property.Order
+{
+	partial class OrderSetupHelper
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnOk = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.elementList = new Common.Controls.DragDropListView.DragDropListView();
+			this.columnOrder = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.btnOk, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.btnCancel, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.elementList, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(7);
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 519);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// btnOk
+			// 
+			this.btnOk.Anchor = System.Windows.Forms.AnchorStyles.Right;
+			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnOk.Location = new System.Drawing.Point(296, 486);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 1;
+			this.btnOk.Text = "Ok";
+			this.btnOk.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnCancel.Location = new System.Drawing.Point(377, 486);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 2;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// elementList
+			// 
+			this.elementList.AllowDrop = true;
+			this.elementList.AllowItemDrag = true;
+			this.elementList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnOrder,
+            this.columnName});
+			this.tableLayoutPanel1.SetColumnSpan(this.elementList, 2);
+			this.elementList.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.elementList.FullRowSelect = true;
+			this.elementList.InsertionLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+			this.elementList.Location = new System.Drawing.Point(10, 10);
+			this.elementList.Name = "elementList";
+			this.elementList.OwnerDraw = true;
+			this.elementList.Size = new System.Drawing.Size(442, 450);
+			this.elementList.TabIndex = 3;
+			this.elementList.UseCompatibleStateImageBehavior = false;
+			this.elementList.View = System.Windows.Forms.View.Details;
+			// 
+			// columnOrder
+			// 
+			this.columnOrder.Text = "Order";
+			// 
+			// columnName
+			// 
+			this.columnName.Text = "Name";
+			// 
+			// OrderSetupHelper
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(462, 519);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "OrderSetupHelper";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Patching Order Preference";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Button btnCancel;
+		private Common.Controls.DragDropListView.DragDropListView elementList;
+		private System.Windows.Forms.ColumnHeader columnOrder;
+		private System.Windows.Forms.ColumnHeader columnName;
+	}
+}
---
+++ Modules/Property/Order/OrderSetupHelper.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using Common.Controls;
+using Common.Controls.Theme;
+using Common.Resources.Properties;
+using Vixen.Rule;
+using Vixen.Sys;
+
+namespace VixenModules.Property.Order
+{
+	public partial class OrderSetupHelper : BaseForm, IElementSetupHelper
+	{
+		private Dictionary<ElementNode, int>  _elementOrderLookup = new Dictionary<ElementNode, int>();
+
+		public OrderSetupHelper()
+		{
+			InitializeComponent();
+			Icon = Resources.Icon_Vixen3;
+			ForeColor = ThemeColorTable.ForeColor;
+			BackColor = ThemeColorTable.BackgroundColor;
+			ThemeUpdateControls.UpdateControls(this);
+
+			elementList.ItemDragDropCompleted += ElementList_ItemDragDropCompleted;
+		}
+
+		#region Implementation of IElementSetupHelper
+
+		/// <inheritdoc />
+		public string HelperName => "Patching Order";
+
+		/// <inheritdoc />
+		public bool Perform(IEnumerable<ElementNode> selectedNodes)
+		{
+			PopulateElementList(selectedNodes);
+
+			DialogResult dr = ShowDialog();
+			if (dr != DialogResult.OK)
+			{
+				return false;
+			}
+
+			foreach (var elementOrder in _elementOrderLookup)
+			{
+				if (elementOrder.Key.Properties.Contains(OrderDescriptor.ModuleId))
+				{
+					var orderProperty = elementOrder.Key.Properties.Get(OrderDescriptor.ModuleId) as OrderModule;
+					if (orderProperty != null)
+					{
+						orderProperty.Order = elementOrder.Value;
+					}
+				}
+				else
+				{
+					var orderProperty = new OrderModule() {Order = elementOrder.Value};
+					elementOrder.Key.Properties.Add(orderProperty);
+				}
+			}
+
+			return true;
+		}
+
+		#endregion
+
+		private void PopulateElementList(IEnumerable<ElementNode> selectedNodes)
+		{
+			IEnumerable<ElementNode> leafElements = selectedNodes.SelectMany(x => x.GetLeafEnumerator()).Distinct();
+
+			_elementOrderLookup.Clear();
+			foreach (var leafElement in leafElements)
+			{
+				int order = Int32.MaxValue;
+				if (leafElement.Properties.Contains(OrderDescriptor.ModuleId))
+				{
+					var orderProperty = leafElement.Properties.Get(OrderDescriptor.ModuleId) as OrderModule;
+					order = orderProperty.Order;
+				}
+
+				_elementOrderLookup.Add(leafElement, order);
+			}
+
+			var orderedElements = _elementOrderLookup.OrderBy(x => x.Value);
+
+			elementList.Items.Clear();
+
+			int count = 1;
+			foreach (var el in orderedElements)
+			{
+				ListViewItem item = new ListViewItem(count.ToString(CultureInfo.InvariantCulture));
+				item.Tag = el.Key;
+				item.SubItems.Add(el.Key.Name);
+				elementList.Items.Add(item);
+				count++;
+			}
+
+			elementList.ColumnAutoSize();
+			elementList.SetLastColumnWidth();
+		}
+
+		private void ReIndexElementNodes()
+		{
+			int index = 1;
+			foreach (ListViewItem item in elementList.Items)
+			{
+				var elementNode = item.Tag as ElementNode;
+				if (elementNode == null)
+				{
+					continue; // This should not happen!
+				}
+
+				item.Text = index.ToString(CultureInfo.InvariantCulture);
+				_elementOrderLookup[elementNode] = index;
+				index++;
+			}
+			elementList.Invalidate();
+		}
+
+		private void ElementList_ItemDragDropCompleted(object sender, Common.Controls.DragDropListView.ListViewItemDragEventArgs e)
+		{
+			ReIndexElementNodes();
+		}
+
+	}
+}
