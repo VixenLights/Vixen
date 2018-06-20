a080888935aa255a356a286f6f7a7166626062f9
gnarmstr <gnarmstr@hotmail.com>
2018-06-20 21:36:27 +1000

    VIX-2367 - Bars Effect - Adjust Iteration range to include 0 so that the bars are stationary. Also added a Movement type to include Iterations and a Speed Curve for adjusting the speed over time. Speed curve default to 50 as this is the stationary position and changed higher and lower will change the direction forward and reverse.

---
+++ Modules/Effect/Bars/MovementType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace VixenModules.Effect.Bars
+{
+	public enum MovementType
+	{
+		[Description("Iterations")]
+		Iterations,
+		[Description("Speed")]
+		Speed
+	}
+}
