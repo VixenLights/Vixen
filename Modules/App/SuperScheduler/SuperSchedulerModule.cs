362bd8cf182ed83ab1f059370a6bcb59e78abe4d
Jeff Uchitjil <jeffu@kb9kld.org>
2018-08-06 17:12:54 -0500

    VIX-2476 Fixed an issue where the scheduler would not detect it is enabled at startup.

--- Modules/App/SuperScheduler/SuperSchedulerModule.cs
+++ Modules/App/SuperScheduler/SuperSchedulerModule.cs
@@ -28,11 +28,13 @@ namespace VixenModules.App.SuperScheduler
 			CreateMenu();
 			//SetSchedulerEnableState(_data.IsEnabled);
 			_executor = new ScheduleExecutor(_data);
+			_executor.CheckSchedule();
 		}
 
 		public override void Unloading()
 		{
 			//SetSchedulerEnableState(false);
+			_executor.Enabled = false;
 			_executor = null;
 		}
 
