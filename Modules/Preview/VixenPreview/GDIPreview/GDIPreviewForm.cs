3c4da0cab77729687c388120f5244935425c2d3d
Jeff Uchitjil <jeffu@kb9kld.org>
2018-08-27 19:48:27 -0500

    VIX-2519 Amend the logic to allow either of the top left or top right to be visible on any screen to restore. This ensures the toolbar is visible and the user can move the form.

--- Modules/Preview/VixenPreview/GDIPreview/GDIPreviewForm.cs
+++ Modules/Preview/VixenPreview/GDIPreview/GDIPreviewForm.cs
@@ -292,8 +292,8 @@ namespace VixenModules.Preview.VixenPreview.GDIPreview
 
 		private bool IsVisibleOnAnyScreen(Rectangle rect)
 		{
-			return Screen.AllScreens.Any(screen => screen.WorkingArea.Contains(rect.Location));
-			//return Screen.AllScreens.Any(screen => screen.WorkingArea.IntersectsWith(rect));
+			return Screen.AllScreens.Any(screen => screen.WorkingArea.Contains(rect.Location)) ||
+				Screen.AllScreens.Any(screen => screen.WorkingArea.Contains(new Point(rect.Top, rect.Right)));
 		}
 
 		public VixenPreviewData Data { get; set; }
