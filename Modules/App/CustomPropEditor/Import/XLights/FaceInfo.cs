e40d67f2dc5b0f69f7c646c6044acb2c3917032f
Jeff Uchitjil <jeffu@kb9kld.org>
2018-09-26 11:37:49 -0500

    VIX-2556 Add logic to import the face info in the xmodel as a group into the prop so the face definition is brought in and can be used.

---
+++ Modules/App/CustomPropEditor/Import/XLights/FaceInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VixenModules.App.CustomPropEditor.Import.XLights
+{
+	public class FaceInfo
+	{
+		public static string[] Attributes = new[]
+		{
+			"Eyes-Closed", "Eyes-Open", "FaceOutline", "Mouth-AI", "Mouth-E", "Mouth-FV", "Mouth-L", "Mouth-MBP",
+			"Mouth-O", "Mouth-U", "Mouth-WQ", "Mouth-etc", "Mouth-rest",
+		};
+
+	}
+}
