694bbebfa73841a51190c85648e70d7e9a774bf5
Jeff Uchitjil <jeffu@kb9kld.org>
2018-10-07 22:09:13 -0500

    VIX-2574 Move the logic that was supposed to be scaling the images to a spot that it will work.

--- Modules/Editor/TimedSequenceEditor/Forms/Form_Effects.cs
+++ Modules/Editor/TimedSequenceEditor/Forms/Form_Effects.cs
@@ -39,13 +39,14 @@ namespace VixenModules.Editor.TimedSequenceEditor
 		private void LoadAvailableEffects()
 		{
 			int imageSize = (int)(18 * ScalingTools.GetScaleFactor());
-			effectTreeImages.ImageSize = new Size(imageSize, imageSize);
+			
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Effects));
 			effectTreeImages.ImageStream = ((ImageListStreamer)(resources.GetObject("effectTreeImages.ImageStream")));
 			effectTreeImages.TransparentColor = treeEffects.BackColor;
 			effectTreeImages.Images.SetKeyName(0, "rightarrow.png");
 			effectTreeImages.Images.SetKeyName(1, "downarrow.png");
 			treeEffects.ItemHeight = (int)ScalingTools.MeasureHeight(treeEffects.Font, "My Text");
+			effectTreeImages.ImageSize = new Size(imageSize, imageSize);
 
 			foreach (
 				IEffectModuleDescriptor effectDesriptor in
