5a165ea059123e37ca517598667e60fa238a7ea1
Jeff Uchitjil <jeffu@kb9kld.org>
2018-10-14 11:00:45 -0500

    VIX-2584 Refactored the logic that removes punctuation to be in a static method that can be used in multiple places. Added calls to it from the word breakdown in the mark bar to cleanse then word to phoneme breakdown.

--- Modules/Editor/TimedSequenceEditor/TimedSequenceEditorForm.cs
+++ Modules/Editor/TimedSequenceEditor/TimedSequenceEditorForm.cs
@@ -2889,13 +2889,14 @@ namespace VixenModules.Editor.TimedSequenceEditor
 					List<IMark> marksAdded = new List<IMark>();
 					foreach (var mark in markGroup)
 					{
-						List<App.LipSyncApp.PhonemeType> phonemeList = LipSyncTextConvert.TryConvert(mark.Text.Trim());
+						var cleanText = LipSyncTextConvert.RemovePunctuation(mark.Text.Trim());
+						List<App.LipSyncApp.PhonemeType> phonemeList = LipSyncTextConvert.TryConvert(cleanText);
 						if (!phonemeList.Any())
 						{
-							var value = GetUserMappingForFailedWord(mark.Text.Trim());
+							var value = GetUserMappingForFailedWord(cleanText);
 							if (value != String.Empty)
 							{
-								phonemeList = LipSyncTextConvert.TryConvert(mark.Text.Trim());
+								phonemeList = LipSyncTextConvert.TryConvert(cleanText);
 							}
 							else
 							{
