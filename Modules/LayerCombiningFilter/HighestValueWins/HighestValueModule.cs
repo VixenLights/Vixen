b862107e33e9a76a44b44d2f7e385761d8493d1f
jon@chuchla.com <jon@chuchla.com>
2018-12-30 22:17:36 -0600

    VIX-2701 Add mixing filter for highest value wins type mixing.  This will better enable users to set up layer groups to build layered effect stacks and combine the stacks.

---
+++ Modules/LayerCombiningFilter/HighestValueWins/HighestValueModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using Vixen.Data.Value;
+using Vixen.Module.MixingFilter;
+
+namespace VixenModules.LayerMixingFilter.HighestValueWins
+{
+	public class HighestValueModule : LayerMixingFilterModuleInstanceBase
+	{
+		public override DiscreteValue CombineDiscreteIntensity(DiscreteValue highLayerValue, DiscreteValue lowLayerValue)
+		{
+            return highLayerValue.Intensity > lowLayerValue.Intensity ? highLayerValue : lowLayerValue;
+        }
+
+		public override Color CombineFullColor(Color highLayerColor, Color lowLayerColor)
+        {
+            var r = highLayerColor.R > lowLayerColor.R ? highLayerColor.R : lowLayerColor.R;
+            var g = highLayerColor.G > lowLayerColor.G ? highLayerColor.G : lowLayerColor.G;
+            var b = highLayerColor.B > lowLayerColor.B ? highLayerColor.B : lowLayerColor.B;
+            return Color.FromArgb(r,g,b);
+		}
+	}	
+}
---
+++ Modules/LayerCombiningFilter/HighestValueWins/HighestValueModuleDescriptor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Vixen.Module.MixingFilter;
+
+namespace VixenModules.LayerMixingFilter.HighestValueWins
+{
+	public class HighestValueModuleDescriptor : LayerMixingFilterModuleDescriptorBase
+	{
+		private static readonly Guid _typeId = new Guid("{01D6D526-9586-44BB-82F4-10ABACF58B61}");
+
+		public override string TypeName
+		{
+			get { return "Highest Value (Default)"; }
+		}
+
+		public override Guid TypeId
+		{
+			get { return _typeId; }
+		}
+
+		public override Type ModuleClass
+		{
+			get { return typeof (HighestValueModule); }
+		}
+
+		public override string Author
+		{
+			get { return "Jon Chuchla"; }
+		}
+
+		public override string Description
+		{
+			get { return "Combines two layers by passing the higher value";}
+		}
+
+		public override string Version
+		{
+			get { return "1.0"; }
+		}
+	}
+}
