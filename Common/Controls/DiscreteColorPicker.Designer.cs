dbde85b26575aa6560558da6f054efc7231b7577
Jeff Uchitjil <jeffu@kb9kld.org>
2019-08-21 19:36:40 -0500

    VIX-2800 Modify the Discrete Color Picker to accommodate more than 9 colors.
    The user had 10 colors in a ALL group and the rigid layout of the color picker only allowed for 9 colors. Changed the layout to grow if more colors are needed.

--- Common/Controls/DiscreteColorPicker.Designer.cs
+++ Common/Controls/DiscreteColorPicker.Designer.cs
@@ -31,13 +31,17 @@
 			this.buttonOK = new System.Windows.Forms.Button();
 			this.labelSelectPrompt = new System.Windows.Forms.Label();
 			this.tableLayoutPanelColors = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// buttonCancel
 			// 
 			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(253, 285);
+			this.buttonCancel.Location = new System.Drawing.Point(239, 3);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(87, 27);
 			this.buttonCancel.TabIndex = 0;
@@ -50,7 +54,7 @@
 			// 
 			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonOK.Location = new System.Drawing.Point(159, 285);
+			this.buttonOK.Location = new System.Drawing.Point(146, 3);
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.Size = new System.Drawing.Size(87, 27);
 			this.buttonOK.TabIndex = 1;
@@ -62,7 +66,7 @@
 			// labelSelectPrompt
 			// 
 			this.labelSelectPrompt.AutoSize = true;
-			this.labelSelectPrompt.Location = new System.Drawing.Point(31, 27);
+			this.labelSelectPrompt.Location = new System.Drawing.Point(13, 10);
 			this.labelSelectPrompt.Name = "labelSelectPrompt";
 			this.labelSelectPrompt.Size = new System.Drawing.Size(188, 15);
 			this.labelSelectPrompt.TabIndex = 2;
@@ -70,29 +74,58 @@
 			// 
 			// tableLayoutPanelColors
 			// 
+			this.tableLayoutPanelColors.AutoSize = true;
 			this.tableLayoutPanelColors.ColumnCount = 3;
 			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
 			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
 			this.tableLayoutPanelColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-			this.tableLayoutPanelColors.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
-			this.tableLayoutPanelColors.Location = new System.Drawing.Point(33, 60);
+			this.tableLayoutPanelColors.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanelColors.Location = new System.Drawing.Point(13, 28);
 			this.tableLayoutPanelColors.Name = "tableLayoutPanelColors";
 			this.tableLayoutPanelColors.RowCount = 3;
 			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33233F));
 			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33533F));
 			this.tableLayoutPanelColors.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33233F));
-			this.tableLayoutPanelColors.Size = new System.Drawing.Size(294, 187);
+			this.tableLayoutPanelColors.Size = new System.Drawing.Size(329, 250);
 			this.tableLayoutPanelColors.TabIndex = 3;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.labelSelectPrompt, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanelColors, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 10, 10, 5);
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(355, 325);
+			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoSize = true;
+			this.flowLayoutPanel1.Controls.Add(this.buttonCancel);
+			this.flowLayoutPanel1.Controls.Add(this.buttonOK);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 284);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(329, 33);
+			this.flowLayoutPanel1.TabIndex = 4;
 			// 
 			// DiscreteColorPicker
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
 			this.ClientSize = new System.Drawing.Size(355, 325);
-			this.Controls.Add(this.tableLayoutPanelColors);
-			this.Controls.Add(this.labelSelectPrompt);
-			this.Controls.Add(this.buttonOK);
-			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.DoubleBuffered = true;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
@@ -100,8 +133,10 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Select Color(s)";
 			this.Load += new System.EventHandler(this.DiscreteColorPicker_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -111,5 +146,7 @@
 		private System.Windows.Forms.Button buttonOK;
 		private System.Windows.Forms.Label labelSelectPrompt;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanelColors;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 	}
 }
