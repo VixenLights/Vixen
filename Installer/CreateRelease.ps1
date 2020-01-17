3ebf2bb72227989a9920b780ff98e399af7b4d1b
Jeff Uchitjil <jeffu@kb9kld.org>
2020-01-16 19:21:56 -0600

    VIX-2890 Create a script to generate a Git label for the build and create offical Github releases.

---
+++ Installer/CreateRelease.ps1
@@ -0,0 +1,47 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $version,
+	[string] $user,
+	[string] $repo,
+	[string] $buildType,
+	[string] $token
+)
+
+$testUrl = "http://www.vixenlights.com/downloads/devbuild/"
+$releaseUrl = "http://www.vixenlights.com/downloads/vixen-3-downloads/"
+
+$status = git tag -l $version
+
+Write-Host $status
+
+if([string]::IsNullOrEmpty($status)){
+	Write-Host "Creating tag $($version)"
+	git tag $version
+	git push -f --tags
+}
+
+$description = "Test builds can be downloaded here: <br/>"
+$command = 'github-release release -s $token -u $user -r $repo -t $version -d $description'
+
+if($buildType -ieq "dev"){
+	$description += $testUrl
+	$command += ' -p'
+}
+else
+{
+	$description += $releaseUrl
+}
+
+Write-Host "Testing to see if release exists."
+
+$status = github-release info -s $token -u $user -r $repo -t $version
+
+if(-Not[string]::IsNullOrEmpty($status) -And ($status.StartsWith("error: could not find the release corresponding to tag"))){
+	Write-Host "Release Exists: Attempting to delete it."
+	github-release delete -s $token -u $user -r $repo -t $version
+}
+
+Write-Host "Creating release with description $($description) for version $($version)"
+Invoke-Expression -Command $command
+
+
