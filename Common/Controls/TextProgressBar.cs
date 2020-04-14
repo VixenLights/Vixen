b2fb5680bafc0c232e026851c43de4b58e5f5d8b
Jeff Uchitjil <jeffu@kb9kld.org>
2020-04-13 19:10:46 -0500

    VIX-2750 Add progress reporting on startup.
    Add a text enabled progress bar to the main admin screen to conserve space.
    Refactor the profile loading logic to async and also to report progress. This has a bit more knowledge built into it about the total progress than I would like, but it serves the purpose for now.
    Added a new progress bar to the common controls that allows text to be overlaid in the bar itself to save space.

---
+++ Common/Controls/TextProgressBar.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Media;
+using Common.Controls.Theme;
+using Brush = System.Drawing.Brush;
+using Brushes = System.Drawing.Brushes;
+using Color = System.Drawing.Color;
+
+namespace Common.Controls
+{
+    public enum ProgressBarDisplayMode
+    {
+        NoText,
+        Percentage,
+        CurrProgress,
+        CustomText,
+        TextAndPercentage,
+        TextAndCurrProgress
+    }
+
+    public class TextProgressBar : ProgressBar
+    {
+        [Description("Font of the text on ProgressBar"), Category("Additional Options")]
+        public Font TextFont { get; set; } = ThemeUpdateControls.StandardFont;
+        
+        private SolidBrush _textColourBrush = new SolidBrush(ThemeColorTable.ForeColor);
+        [Category("Additional Options")]
+        public Color TextColor {
+            get {
+                return _textColourBrush.Color;
+            }
+            set
+            {
+                _textColourBrush.Dispose();
+                _textColourBrush = new SolidBrush(value);
+            }
+        }
+
+        private SolidBrush _progressColourBrush = (SolidBrush) Brushes.LightGreen;
+        [Category("Additional Options"), Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
+        public Color ProgressColor
+        {
+            get
+            {
+                return _progressColourBrush.Color;
+            }
+            set
+            {
+                _progressColourBrush.Dispose();
+                _progressColourBrush = new SolidBrush(value);
+            }
+        }
+
+        private ProgressBarDisplayMode _visualMode = ProgressBarDisplayMode.CurrProgress;
+        [Category("Additional Options"), Browsable(true)]
+        public ProgressBarDisplayMode VisualMode {
+            get {
+                return _visualMode;
+            }
+            set
+            {
+                _visualMode = value;
+                Invalidate();//redraw component after change value from VS Properties section
+            }
+        }
+
+        private string _text = string.Empty;
+
+        [Description("If it's empty, % will be shown"), Category("Additional Options"), Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
+        public string CustomText
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                Invalidate();//redraw component after change value from VS Properties section
+            }
+        }
+
+        private string TextToDraw
+        {
+            get
+            {
+                string text = CustomText;
+
+                switch (VisualMode)
+                {
+                    case (ProgressBarDisplayMode.Percentage):
+                        text = PercentageStr;
+                        break;
+                    case (ProgressBarDisplayMode.CurrProgress):
+                        text = CurrProgressStr;
+                        break;
+                    case (ProgressBarDisplayMode.TextAndCurrProgress):
+                        text = $"{CustomText}: {CurrProgressStr}";
+                        break;
+                    case (ProgressBarDisplayMode.TextAndPercentage):
+                        text = $"{CustomText}: {PercentageStr}";
+                        break;
+                }
+
+                return text;
+            }
+            set { }
+        }
+        
+        private string PercentageStr => $"{(int)((float)Value - Minimum) / ((float)Maximum - Minimum) * 100 } %";
+
+        private string CurrProgressStr => $"{Value}/{Maximum}";
+
+        public TextProgressBar()
+        {
+            Value = Minimum;
+            FixComponentBlinking();
+        }
+
+        private void FixComponentBlinking()
+        {
+            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            DrawProgressBar(g);
+
+            DrawStringIfNeeded(g);
+        }
+
+        private void DrawProgressBar(Graphics g)
+        {
+            Rectangle rect = ClientRectangle;
+            
+
+            ProgressBarRenderer.DrawHorizontalBar(g, rect);
+
+            rect.Inflate(-1, -1);
+            g.FillRectangle(new SolidBrush(ThemeColorTable.BackgroundColor), ClientRectangle);
+
+            rect.Inflate(-2, -2);
+
+            if (Value > 0)
+            {
+                Rectangle clip = new Rectangle(rect.X, rect.Y, (int)Math.Round(((float)Value / Maximum) * rect.Width), rect.Height);
+
+                g.FillRectangle(_progressColourBrush, clip);
+            }
+        }
+
+        private void DrawStringIfNeeded(Graphics g)
+        {
+            if (VisualMode != ProgressBarDisplayMode.NoText)
+            {
+                
+                string text = TextToDraw;
+
+                SizeF len = g.MeasureString(text, TextFont);
+
+                Point location = new Point(((Width / 2) - (int)len.Width / 2), ((Height / 2) - (int)len.Height / 2));
+                
+                g.DrawString(text, TextFont, (Brush)_textColourBrush, location);
+            }
+        }
+        
+        public new void Dispose()
+        {
+            _textColourBrush.Dispose();
+            _progressColourBrush.Dispose();
+            base.Dispose();
+        }
+    }
+}
