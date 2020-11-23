9c9ae1ce1ca62bc22b9ce8f8767b7bd97c0c6b87
Jeff Uchitjil <jeffu@kb9kld.org>
2020-11-23 14:40:29 -0600

    Try adding BR tags

--- Build/CreateReleaseNotes.ps1
+++ Build/CreateReleaseNotes.ps1
@@ -109,8 +109,8 @@ $actionsOutput = $actionsOutput -replace '\A', '## '
 $actionsOutput = $actionsOutput -replace '(?m)^\*\* ', '### '
 $actionsOutput = $actionsOutput -replace '(?m)^    \* ', '* '
 # set-env doesn't like multiline strings - escape CR/LF
-$actionsOutput = $actionsOutput -replace "`r", 'EOF'
-$actionsOutput = $actionsOutput -replace "`n", 'EOF'
+$actionsOutput = $actionsOutput -replace "`r", '<br />'
+$actionsOutput = $actionsOutput -replace "`n", '<br />'
 echo "VIX_RELEASE_NOTES_MARKDOWN=$actionsOutput" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 
