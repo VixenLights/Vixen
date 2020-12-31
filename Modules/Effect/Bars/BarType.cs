20536ab6a84d63b1b93e1ce72602aeaa60f631ef
johncbaur <46469358+johncbaur@users.noreply.github.com>
2020-12-31 16:19:33 -0600

    VIX-2963 Adding BarType.cs

---
+++ Modules/Effect/Bars/BarType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VixenModules.Effect.Bars
+{
+    public enum BarType
+    {
+        [Description("Flat")]
+        Flat,
+
+        [Description("Zig Zag")]
+        ZigZag,
+    }
+}
