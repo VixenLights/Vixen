58b68190175677bf2c99a0aac1d6fbb848618b5f
johncbaur <46469358+johncbaur@users.noreply.github.com>
2021-07-10 11:39:50 -0500

    VIX-3063
    Initial implementation of a DMX moving head fixture.

---
+++ Modules/Effect/DMXFixture/GoboType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VixenModules.Effect.DMXFixture
+{
+	public enum Gobos
+	{
+		[Description("Open")]
+		Open,
+
+		[Description("Gobo 1")]
+		Gobo1,
+
+		[Description("Gobo 2")]
+		Gobo2,
+
+		[Description("Gobo 3")]
+		Gobo3,
+
+		[Description("Gobo 4")]
+		Gobo4,
+
+		[Description("Gobo 5")]
+		Gobo5,
+
+		[Description("Gobo 6")]
+		Gobo6,
+
+		[Description("Gobo 7")]
+		Gobo7,
+
+		[Description("Gobo 8")]
+		Gobo8,
+
+		[Description("Gobo 9")]
+		Gobo9,
+
+		[Description("Gobo 10")]
+		Gobo10,
+
+		[Description("Gobo 11")]
+		Gobo11,
+
+		[Description("Gobo 12")]
+		Gobo12,
+
+		[Description("Gobo 13")]
+		Gobo13,
+
+		[Description("Gobo 14")]
+		Gobo14,
+	}
+}
