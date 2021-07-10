3be40edba083195805999c2031bc0941934c6046
johncbaur <46469358+johncbaur@users.noreply.github.com>
2021-07-10 11:39:50 -0500

    VIX-3063
    Initial implementation of a DMX moving head fixture.

---
+++ Modules/Preview/VixenPreview/Fixtures/OpenGL/Volumes/IUpdateCylinder.cs
@@ -0,0 +1,21 @@
+﻿namespace VixenModules.Preview.VixenPreview.Fixtures.OpenGL.Volumes
+{
+	/// <summary>
+	/// Updates the geometry of a cylinder.
+	/// </summary>
+	public interface IUpdateCylinder
+	{
+		/// <summary>
+		/// Updates the geometry of a cylinder.
+		/// </summary>		
+		/// <param name="yOffset">Y offset of the cylinder</param>
+		/// <param name="length">Length of the cylinder</param>
+		/// <param name="bottomRadius">Bottom radius of the cylinder</param>
+		/// <param name="topRadius">Top radius of the cylinder</param>
+		void Update(
+			float yOffset,
+			float length,
+			float bottomRadius,
+			float topRadius);			
+	}
+}
---
+++ Modules/Preview/VixenPreview/Fixtures/OpenGL/Volumes/RotatingCylinderWithEndCaps.cs
@@ -0,0 +1,66 @@
+﻿using OpenTK;
+
+namespace VixenModules.Preview.VixenPreview.Fixtures.OpenGL.Volumes
+{
+	/// <summary>
+	/// Defines a rotating cylinder with end caps.
+	/// </summary>
+	public class RotatingCylinderWithEndCaps : CylinderWithEndCaps, IRotatableCylinder
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="length">Length of the cylinder</param>
+		/// <param name="bottomRadius">Radius of the bottom of the cylinder</param>
+		/// <param name="topRadius">Radius at the top of the cylinder</param>
+		/// <param name="isDynamic">Whether the cylinder changes shape during execution</param>
+		public RotatingCylinderWithEndCaps(float yOffset, float length, float bottomRadius, float topRadius, bool isDynamic) :
+			base(yOffset, length, bottomRadius, topRadius, isDynamic)
+		{
+		}
+
+		#endregion
+
+		#region IRotatableCylinder
+		
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public Vector3 TiltRotation { get; set; }
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public Vector3 TiltTranslation { get; set; }
+
+		#endregion
+
+		#region IVolume
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public override void UpdateModelMatrix()
+		{
+			ModelMatrix =
+				Matrix4.CreateRotationX(Rotation.X) *
+				Matrix4.CreateRotationY(Rotation.Y) *
+				Matrix4.CreateRotationZ(Rotation.Z) *
+				Matrix4.CreateTranslation(Position) *
+
+				Matrix4.CreateRotationX(TiltRotation.X) *
+				Matrix4.CreateRotationY(TiltRotation.Y) *
+				Matrix4.CreateRotationZ(TiltRotation.Z) *
+				Matrix4.CreateTranslation(TiltTranslation) *
+
+				Matrix4.CreateRotationX(GroupRotation.X) *
+				Matrix4.CreateRotationY(GroupRotation.Y) *
+				Matrix4.CreateRotationZ(GroupRotation.Z) *
+				Matrix4.CreateTranslation(GroupTranslation);
+		}
+
+		#endregion
+	}
+}
