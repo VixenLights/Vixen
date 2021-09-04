0eb6b5c06302eae93a553acef39a0f240315f19e
johncbaur <46469358+johncbaur@users.noreply.github.com>
2021-09-04 12:36:51 -0500

    VIX-3063 Incorporating feedback and merging in VIX-3076
    Addressed feedback from VixenCore.
    Merged in changes from VIX-3076.

---
+++ Modules/Preview/VixenPreview/Fixtures/OpenGL/Shaders/ISpecifyVolumeIntensity.cs
@@ -0,0 +1,13 @@
+﻿namespace VixenModules.Preview.VixenPreview.Fixtures.OpenGL.Shaders
+{
+	/// <summary>
+	/// Maintains the intensity of a volume.
+	/// </summary>
+	public interface ISpecifyVolumeIntensity
+	{
+		/// <summary>
+		/// Intensity of the volume.
+		/// </summary>
+		int Intensity { get; set; }
+	}
+}
