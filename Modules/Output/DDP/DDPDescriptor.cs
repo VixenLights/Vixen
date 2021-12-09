10f40656a1194fec0aab7a023af22b9e9ef82e87
jon@chuchla.com <jon@chuchla.com>
2021-12-09 00:11:46 -0600

    Initial Commit.  Began work to implement DDP ModuleC. Started by using BlinkyLinky as a reference.  Changed filenames and all code references from BlinkyLinky to DDP Removed extraneous data members, and setup form controls. Brought in reference code from FPP implementation.  Started to port to vixen implementation

---
+++ Modules/Output/DDP/DDPDescriptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Vixen.Module.Controller;
+
+namespace VixenModules.Output.DDP
+{
+	internal class DDPDescriptor : ControllerModuleDescriptorBase
+	{
+		private Guid _typeId = new Guid("{b9d5d3a1-c746-4395-8e19-a0c975ced438}");
+
+		public override string Author
+		{
+			get { return "Jon Chuchla"; }
+		}
+
+		public override string Description
+		{
+			get { return "DDP Output Controller Module"; }
+		}
+
+		public override Type ModuleClass
+		{
+			get { return typeof (DDP); }
+		}
+
+		public override Type ModuleDataClass
+		{
+			get { return typeof (DDPData); }
+		}
+
+		public override Guid TypeId
+		{
+			get { return _typeId; }
+		}
+
+		public override string TypeName
+		{
+			get { return "DDP"; }
+		}
+
+		public override string Version
+		{
+			get { return "1.0"; }
+		}
+
+	}
+}
---
+++ Modules/Output/DDP/DataPolicyFactory.cs
@@ -0,0 +1,12 @@
+﻿using Vixen.Sys;
+
+namespace VixenModules.Output.DDP
+{
+	internal class DataPolicyFactory : IDataPolicyFactory
+	{
+		public IDataPolicy CreateDataPolicy()
+		{
+			return new DataPolicy();
+		}
+	}
+}
