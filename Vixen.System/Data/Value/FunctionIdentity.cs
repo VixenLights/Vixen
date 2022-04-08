21c000dfbfea9aea3d626c2299f5ba5d2d627238
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-08 17:37:07 -0500

    Update / sync with master.
    Update / sync with master.

---
+++ Vixen.System/Data/Value/FunctionIdentity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Vixen.TypeConverters;
+
+namespace Vixen.Data.Value
+{
+	/// <summary>
+	/// Defines specific functions.  This enum allows the Vixen Preview and filtering to support
+	/// specific functions.
+	/// </summary>
+	[TypeConverter(typeof(EnumDescriptionTypeConverter))]
+	public enum FunctionIdentity
+	{
+		[Description("Pan")]
+		Pan,
+
+		[Description("Tilt")]
+		Tilt,
+
+		[Description("Zoom")]
+		Zoom,
+
+		[Description("Dim")]
+		Dim,
+
+		[Description("Shutter")]
+		Shutter,
+
+		[Description(" ")]
+		Custom,
+	}
+}
