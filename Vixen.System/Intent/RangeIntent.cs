129944539b2401bcc61a0af8e6aa5ed753d934aa
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-08 17:31:41 -0500

    Update / synch with master.
    Update / synch with master.

---
+++ Vixen.System/Intent/RangeIntent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vixen.Data.Value;
+
+namespace Vixen.Intent
+{
+	public class RangeIntent : NonSegmentedLinearIntent<RangeValue<FunctionIdentity>>
+	{
+		public RangeIntent(RangeValue<FunctionIdentity> startValue, RangeValue<FunctionIdentity> endValue, TimeSpan timeSpan)
+			: base(startValue, endValue, timeSpan)
+		{
+		}
+	}
+}
