b7f4a2a590031bb94daf6dfa7c1a1c2beb491266
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-09 13:20:19 -0500

    VIX-3158 Adding Setup Dialog
    VIX-3158 Adding Setup Dialog

--- Modules/PostFilter/TaggedFilter/TaggedFilterModule.cs
+++ Modules/PostFilter/TaggedFilter/TaggedFilterModule.cs
@@ -1,4 +1,5 @@
-﻿using VixenModules.OutputFilter.TaggedFilter.Outputs;
+﻿using System.Windows.Forms;
+using VixenModules.OutputFilter.TaggedFilter.Outputs;
 
 namespace VixenModules.OutputFilter.TaggedFilter
 {
@@ -12,15 +13,32 @@ namespace VixenModules.OutputFilter.TaggedFilter
 		/// <summary>
 		/// Refer interface documentation.
 		/// </summary>
-		public override bool HasSetup => false;
+		public override bool HasSetup => true;
 
 		/// <summary>
 		/// Refer interface documentation.
 		/// </summary>
 		public override bool Setup()
-		{			
-			return true;
-		}
+		{
+			// Default to the OK button not being selected
+			bool okSelected = false;
+
+			// Display the Tagger filter setup dialog
+			using (TaggedFilterSetup setup = new TaggedFilterSetup(Data))
+			{
+				// If the user selected OK then...
+				if (setup.ShowDialog() == DialogResult.OK)
+				{					
+					// Re-create the filter's output
+					CreateOutput();
+
+					// Indicate that setup completed successfully
+					okSelected = true;
+				}
+			}
+
+			return okSelected;
+		}	
 
 		#endregion
 
