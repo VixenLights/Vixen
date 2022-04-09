e8a2e3cbc0ec490085dfa55c7d67b168760fc0d2
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-09 14:22:22 -0500

    VIX-3160 Adding Setup Dialog
    VIX-3160 Adding Setup Dialog

--- Modules/PostFilter/ShutterFilter/ShutterFilterModule.cs
+++ Modules/PostFilter/ShutterFilter/ShutterFilterModule.cs
@@ -1,4 +1,5 @@
-﻿using VixenModules.OutputFilter.ShutterFilter.Output;
+﻿using System.Windows.Forms;
+using VixenModules.OutputFilter.ShutterFilter.Output;
 using VixenModules.OutputFilter.TaggedFilter;
 
 namespace VixenModules.OutputFilter.ShutterFilter
@@ -8,7 +9,9 @@ namespace VixenModules.OutputFilter.ShutterFilter
 	/// </summary>
 	public class ShutterFilterModule : TaggedFilterModuleBase<ShutterFilterData, ShutterFilterOutput, ShutterFilterDescriptor>
 	{
-		#region Public Methods
+		#region IHasSetup
+
+		public override bool HasSetup => true;
 
 		/// <summary>
 		/// Configures the filter.
@@ -16,18 +19,22 @@ namespace VixenModules.OutputFilter.ShutterFilter
 		/// <returns>True if the filter was configured</returns>
 		public override bool Setup()
 		{
+			// Default to the OK button not being selected
 			bool okSelected = false;
 
-			/*
-			using (DimmingFilterSetup setup = new DimmingFilterSetup(Data))
+			// Display the Tagger filter setup dialog
+			using (ShutterFilterSetup setup = new ShutterFilterSetup(Data))
 			{
+				// If the user selected OK then...
 				if (setup.ShowDialog() == DialogResult.OK)
 				{
+					// Re-create the filter's output
 					CreateOutput();
+
+					// Indicate that setup completed successfully
 					okSelected = true;
 				}
 			}
-			*/
 
 			return okSelected;
 		}
@@ -35,9 +42,16 @@ namespace VixenModules.OutputFilter.ShutterFilter
 		#endregion
 
 		#region Public Properties
-
-		public override bool HasSetup => false;
 		
+		/// <summary>
+		/// Flag which determines if RGB colors are converted into shutter intents.
+		/// </summary>
+		public bool ConvertRGBIntoShutterIntents
+		{
+			get { return Data.ConvertRGBIntoShutterIntents; }
+			set { Data.ConvertRGBIntoShutterIntents = value; }
+		}
+
 		/// <summary>
 		/// Open Shutter index command value.
 		/// </summary>
@@ -57,7 +71,7 @@ namespace VixenModules.OutputFilter.ShutterFilter
 		protected override ShutterFilterOutput CreateOutputInternal()
 		{
 			// Create the shutter filter output
-			return new ShutterFilterOutput(Data.Tag, Data.OpenShutterIndexValue);
+			return new ShutterFilterOutput(Data.Tag, Data.ConvertRGBIntoShutterIntents, Data.OpenShutterIndexValue);
 		}
 
 		#endregion
