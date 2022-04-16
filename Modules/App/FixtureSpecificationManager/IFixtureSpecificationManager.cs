86b3095addbfd687952d038e58b6492d3cafbd0c
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-15 20:20:51 -0500

    VIX-3147 Create Fixture Specification Manager
    VIX-3147 Create Fixture Specification Manager

---
+++ Modules/App/FixtureSpecificationManager/IFixtureSpecificationManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using VixenModules.App.Fixture;
+
+namespace VixenModules.App.FixtureSpecificationManager
+{
+    /// <summary>
+    /// Manages a repository of intelligent fixtures.
+    /// </summary>
+    public interface IFixtureSpecificationManager
+    {
+        /// <summary>
+        /// Initializes the manager with the active profile path.
+        /// </summary>
+        /// <param name="profilePath">Active profile path</param>
+        void InitializeProfilePath(string profilePath);
+
+        /// <summary>
+        /// Saves the specified fixture to the fixture respository.
+        /// </summary>
+        /// <param name="fixture">Fixture specification to save</param>
+        void Save(FixtureSpecification fixture);
+
+        /// <summary>
+        /// Gets the fixture specifications in the respository.
+        /// </summary>
+        IList<FixtureSpecification> FixtureSpecifications { get; }
+    }
+}
