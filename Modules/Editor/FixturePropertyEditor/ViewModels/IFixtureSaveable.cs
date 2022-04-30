3d53a450ba172a2eca4f0dccf2b46d599c18098c
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-30 10:16:38 -0500

    VIX-3179 Create Fixture Property Editor
    VIX-3179 Create Fixture Property Editor

---
+++ Modules/Editor/FixturePropertyEditor/ViewModels/IFixtureSaveable.cs
@@ -0,0 +1,20 @@
+﻿namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
+{
+    /// <summary>
+    /// Validates fixture view model to determine if it safe to save.
+    /// </summary>
+    public interface IFixtureSaveable
+    {
+        /// <summary>
+		/// Returns true if all required function data has been populated.
+		/// </summary>
+		/// <returns>True if all required function data has been populated</returns>
+        bool CanSave();
+        
+        /// <summary>
+        /// Returns the results of the data validation.
+        /// </summary>
+        /// <returns>Results of the data validation.</returns>
+        string GetValidationResults();
+    }
+}
