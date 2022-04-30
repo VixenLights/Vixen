87d6c7d12da0b18182bd41ed52d144f43dd38228
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-30 10:16:38 -0500

    VIX-3179 Create Fixture Property Editor
    VIX-3179 Create Fixture Property Editor

---
+++ Modules/Editor/FixturePropertyEditor/Views/PanTiltView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using VixenModules.App.Fixture;
+using VixenModules.Editor.FixturePropertyEditor.ViewModels;
+
+namespace VixenModules.Editor.FixturePropertyEditor.Views
+{
+    /// <summary>
+    /// Maintains fixture index data view.
+    /// </summary>
+    public partial class PanTiltView
+	{
+		#region Constructor
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>		
+		public PanTiltView()
+		{
+			// Initialize the user control
+			InitializeComponent();
+		}
+		
+		#endregion				    		
+	}
+}
