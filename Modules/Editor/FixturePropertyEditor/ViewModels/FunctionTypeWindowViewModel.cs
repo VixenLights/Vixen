95add6e77e3d34bba94b31eaf417b85fc12a6958
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-04-30 10:16:38 -0500

    VIX-3179 Create Fixture Property Editor
    VIX-3179 Create Fixture Property Editor

---
+++ Modules/Editor/FixturePropertyEditor/ViewModels/FunctionTypeWindowViewModel.cs
@@ -0,0 +1,169 @@
+﻿using Catel.Data;
+using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using VixenModules.App.Fixture;
+
+namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
+{
+    /// <summary>
+    /// Maintains a window used to edit the fixture function types.
+    /// </summary>
+    public class FunctionTypeWindowViewModel : FixturePropertyWindowViewModelBase
+    {
+        #region Constructor 
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FunctionTypeWindowViewModel()
+        {            
+        }
+
+        #endregion
+
+        #region Catel Public Properties
+
+        /// <summary>
+        /// Tuple that contains the functions associated with the fixture and a specific function to select.
+        /// </summary>
+        public Tuple<List<FixtureFunction>, string, Action> Functions
+        {
+            get
+            {
+                return GetValue<Tuple<List<FixtureFunction>, string, Action>>(FunctionsProperty);
+            }
+            set
+            {
+                SetValue(FunctionsProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Functions property data.
+        /// </summary>
+        public static readonly PropertyData FunctionsProperty = RegisterProperty(nameof(Functions), typeof(Tuple<List<FixtureFunction>, string, Action>), null);
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Functions that were updated FunctionTypeViewModel (User Control).
+        /// </summary>
+        public List<FixtureFunction> UpdatedFunctions { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes the view model with the functions associated with the fixture and which function to select initially.
+        /// </summary>
+        /// <param name="functions">Functions associated with the fixture</param>
+        /// <param name="functionToSelect">Function to select</param>
+        public void InitializeChildViewModels(List<FixtureFunction> functions, string functionToSelect)
+        {
+            // Store off the fixture functions and which function to select as a Tuple
+            Functions = new Tuple<List<FixtureFunction>, string, Action>(functions, functionToSelect, ((Command)OkCommand).RaiseCanExecuteChanged);
+        }
+
+        #endregion
+
+        #region Protected Methods
+        
+        /// <summary>
+        /// Returns true if the OK command can be executed.
+        /// </summary>
+        /// <returns>True if the OK command can be executed</returns>
+        protected override bool CanExecuteOK()
+        {
+            // Default to disabling the OK command
+            bool canOK = false;
+
+            // Attempt to get the child view model
+            FunctionTypeViewModel functionTypeViewModel = GetChildViewModel();
+
+            // If the child view model has been created then...
+            if (functionTypeViewModel != null)
+            { 
+                // Delegate to the child view model
+                canOK = functionTypeViewModel.CanSave();
+               
+                // If NOT already updating the OK button tooltip then...
+                if (!UpdatingOKTooltip)
+                {
+                    // Set a flag to avoid recursion
+                    UpdatingOKTooltip = true;
+
+                    // If the OK button is disabled then...
+                    if (!canOK)
+                    {                    
+                        // Update the OK button tooltip with the validation results
+                        OKTooltip = functionTypeViewModel.GetValidationResults();
+
+                        // Display the error triangle
+                        ShowError = Visibility.Visible;   
+                    }
+                    // Otherwise the function data is valid
+                    else
+                    {
+                        // Clear the OK button tooltip
+                        OKTooltip = String.Empty;
+
+                        // Hide the error triangle
+                        ShowError = Visibility.Hidden;
+                    }
+
+                    // Clear the flag indicating that we are done updating the OK button tooltip
+                    UpdatingOKTooltip = false;
+                }
+            }
+
+            // Return whether the OK command can be executed
+            return canOK;
+        }
+              
+        /// <summary>
+        /// OK command handler.
+        /// </summary>
+        protected override void OK()
+        {
+            // Update the functions retrieving them from the child view model
+            UpdatedFunctions = GetChildViewModel().GetFunctionData();
+                                                        
+            // Call base class Catel processing
+            base.OK();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Gets the child view model.  This is the view model associated with the user control.
+        /// </summary>
+        /// <returns>Child view model</returns>
+        private FunctionTypeViewModel GetChildViewModel()
+        {
+            // Default the child view model to null
+            FunctionTypeViewModel childViewModel = null;
+
+            // Get the collection of child view models from Catel base classes
+            IList<IViewModel> childViewModels = GetChildViewModels().ToList();
+
+            // If the child view models have been created then...
+            if (childViewModels.Count > 0)
+            {
+                // Cast the view model to the specific type
+                childViewModel = (FunctionTypeViewModel)childViewModels[0];
+            }
+
+            return childViewModel;
+        }
+
+        #endregion
+    }
+}
---
+++ Modules/Editor/FixturePropertyEditor/ViewModels/IndexedViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using VixenModules.App.Fixture;
+
+namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
+{
+    /// <summary>
+    /// Maintains the indexes (enumerations) for a fixture function.
+    /// </summary>
+    public class IndexedViewModel : ItemsViewModel<IndexedItemViewModel>
+	{
+        #region Constructor 
+		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+        public IndexedViewModel()
+		{					
+		}
+
+		#endregion
+
+		#region Constants
+
+		/// <summary>
+		/// Numerical value to represent an unpopulated index end value.
+		/// </summary>
+		private const int UnusedIndexValue = -1;
+
+        #endregion 
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initialize the index view model items from the model data.
+        /// </summary>
+        /// <param name="indexData">Fixture index model data</param>
+        public void InitializeChildViewModels(List<FixtureIndex> indexData)
+		{
+			// Clear any existing items
+			Items.Clear();
+
+			// Loop over the model index data
+			foreach (FixtureIndex fixtureIndex in indexData)
+			{
+				// Create the index name value pair
+				IndexedItemViewModel indexNameValuePair = CreateNewItem();
+
+				// Assign the name to the index item
+				indexNameValuePair.Name = fixtureIndex.Name;
+
+				// Assign the start value to the index item
+				indexNameValuePair.StartValue = fixtureIndex.StartValue.ToString();
+
+				// If the index has an end value then...
+				if (fixtureIndex.EndValue != UnusedIndexValue)
+				{
+					// Assign the end value to the item
+					indexNameValuePair.EndValue = fixtureIndex.EndValue.ToString();
+				}
+
+				// Assign the use curve to the item
+				indexNameValuePair.UseCurve = fixtureIndex.UseCurve;
+
+				// Assign the index type to the item
+				indexNameValuePair.IndexType = fixtureIndex.IndexType;
+								
+				// Add the item to the item collection
+				Items.Add(indexNameValuePair);
+			}
+		}
+
+		/// <summary>
+		/// Converts the index view model data back to model data.
+		/// </summary>
+		/// <returns>Index data associated with the view model</returns>
+		public List<FixtureIndex> GetIndexData()
+		{
+			// Create a collection of model index data
+			List<FixtureIndex> returnCollection = new List<FixtureIndex>();
+
+			// Loop over the view model index items
+			foreach (IndexedItemViewModel item in Items)
+			{
+				// Create a new model fixture index 
+				FixtureIndex indexValuePair = new FixtureIndex();
+
+				// Assign the index name
+				indexValuePair.Name = item.Name;
+
+				// Assign whether the index should be edited by a curve
+				indexValuePair.UseCurve = item.UseCurve;
+
+				// Assign the start value of the index
+				indexValuePair.StartValue = int.Parse(item.StartValue);
+
+				// If the end value has been defined then...
+				if (!string.IsNullOrEmpty(item.EndValue))
+				{
+					// Assign the index end value
+					indexValuePair.EndValue = int.Parse(item.EndValue);
+				}
+				else
+                {
+					// Otherwise set the end value to -1 to represent unused
+					indexValuePair.EndValue = UnusedIndexValue;
+				}
+				
+				// Assign the index type
+				indexValuePair.IndexType = item.IndexType;
+										
+				// Add the index object to the model collection
+				returnCollection.Add(indexValuePair);
+			}
+
+			// Return the model index collection
+			return returnCollection;
+		}
+				
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// For reasons unknown Catel seems to continue to validate items that have been removed from the collection.
+		/// This method ensures they are valid so they don't produce errors.
+		/// </summary>
+		/// <param name="item">Item to make valid</param>
+		protected override void MakeObjectValidBeforeDeleting(IndexedItemViewModel item)
+		{
+			item.Name = "Zombie";
+			item.StartValue = "0";
+			item.EndValue = "0";
+		}
+
+		#endregion
+	}
+}
