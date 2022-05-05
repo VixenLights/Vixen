f646a83853f3c683314b684074967cf0e732029d
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-05-05 12:41:17 -0500

    VIX-31386 Add flags to the Wizard Item Templates interface to control parent behavior also add Cancel support
    VIX-31386 Add flags to the Wizard Item Templates interface to control parent behavior also add Cancel support

---
+++ Application/VixenApplication/Setup/ElementTemplates/ElementTemplateBase.cs
@@ -0,0 +1,40 @@
+﻿using Common.Controls;
+using System.Collections.Generic;
+using Vixen.Sys;
+
+namespace VixenApplication.Setup.ElementTemplates
+{
+	/// <summary>
+	/// Base class for an element template.
+	/// </summary>
+	public abstract class ElementTemplateBase : BaseForm
+	{
+		#region IElementTamplate
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public virtual bool ConfigureColor => true;
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public virtual bool ConfigureDimming => true;
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public virtual bool Cancelled => false;
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>
+		public virtual IEnumerable<ElementNode> GetElementsToDelete()
+		{
+			// By default there are no element nodes to delete
+			return new List<ElementNode>();
+		}
+
+		#endregion
+	}
+}
