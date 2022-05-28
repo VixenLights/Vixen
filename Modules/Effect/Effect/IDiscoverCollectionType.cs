b70a717205ca4ce3018b5528696d9164a4b697ca
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-05-27 19:44:04 -0500

    VIX-3190 Effect Editor does not support empty collections
    VIX-3190 Effect Editor does not support empty collections

---
+++ Modules/Effect/Effect/IDiscoverCollectionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VixenModules.Effect.Effect
+{
+	/// <summary>
+	/// Provides type information about the items contained in a collection.
+	/// </summary>
+	public interface IDiscoverCollectionItemType
+	{
+		/// <summary>
+		/// Returns the concrete type of the items in the collection.
+		/// </summary>
+		/// <returns>Type of the items contained in a collection</returns>
+		Type GetItemType();
+	}
+}
