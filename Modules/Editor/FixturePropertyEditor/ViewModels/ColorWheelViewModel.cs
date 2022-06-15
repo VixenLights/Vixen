e0bdf12b9c78854636a6d13ea7f4da06a06001f9
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-14 20:15:23 -0500

    VIX-3179 More Fixture Property Editor Updates
    The addition of fixture effects necessitated further changes to the property editor.
    
    - Added the ability to control whether the fixture zooms wide to narrow or vice versa.
    - Fixed threading issue with validation.
    - Added support for Gobo images.
    - Adding spinning support to the color wheel

--- Modules/Editor/FixturePropertyEditor/ViewModels/ColorWheelViewModel.cs
+++ Modules/Editor/FixturePropertyEditor/ViewModels/ColorWheelViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using VixenModules.App.Fixture;
 
 namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
@@ -17,16 +18,29 @@ namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
 		{
 		}
 
-		#endregion		
+		#endregion
+
+		#region Private Constants
+
+		/// <summary>
+		/// Numerical value to represent an unpopulated color wheel end value.
+		/// </summary>
+		private const int UnusedEndValue = -1;
+
+		#endregion
 
 		#region Public Methods
-				
+
 		/// <summary>
 		/// Creates the color wheel item view model items from the model data.
 		/// </summary>
 		/// <param name="colorWheelData">Color wheel item data</param>
-		public void InitializeChildViewModels(List<FixtureColorWheel> colorWheelData)
+		/// <param name="raiseCanExecuteChanged">Delegate to refresh the command status</param>
+		public void InitializeChildViewModels(List<FixtureColorWheel> colorWheelData, Action raiseCanExecuteChanged)
 		{
+			// Store of the delegate to refresh the command status
+			RaiseCanExecuteChanged = raiseCanExecuteChanged;
+
 			// Clear any existing color wheel items
 			Items.Clear();
 
@@ -41,6 +55,12 @@ namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
 
 				// Assign the start value
 				colorSlot.StartValue = colorWheel.StartValue.ToString();
+
+				// Assign the end value
+				colorSlot.EndValue = colorWheel.EndValue.ToString();
+
+				// Assign whether a curve is applicable
+				colorSlot.UseCurve = colorWheel.UseCurve;
 								
 				// Assign whether the entry is a half step
 				colorSlot.HalfStep = colorWheel.HalfStep;
@@ -74,7 +94,22 @@ namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
 
 				// Assign the start value 
 				colorSlot.StartValue = int.Parse(item.StartValue);
-				
+
+				// If the end value has been defined then...
+				if (!string.IsNullOrEmpty(item.EndValue))
+				{
+					// Assign the end value
+					colorSlot.EndValue = int.Parse(item.EndValue);
+				}
+				else
+				{
+					// Otherwise mark the end value as unused
+					colorSlot.EndValue = UnusedEndValue;
+				}
+
+				// Assign whether the curve is applicable
+				colorSlot.UseCurve = item.UseCurve;
+
 				// Assign whether this entry is a half step
 				colorSlot.HalfStep = item.HalfStep;
 				
@@ -121,7 +156,8 @@ namespace VixenModules.Editor.FixturePropertyEditor.ViewModels
 		{
 			item.Name = "Delete Me";
 			item.StartValue = "0";
-	  	}
+			item.CloseViewModelAsync(null);
+		}
 
 		#endregion
 
