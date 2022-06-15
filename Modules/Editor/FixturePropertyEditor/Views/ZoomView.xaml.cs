b18adfc68a3b4c82752ab8791ecb66f6857a6d6a
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-14 20:15:23 -0500

    VIX-3179 More Fixture Property Editor Updates
    The addition of fixture effects necessitated further changes to the property editor.
    
    - Added the ability to control whether the fixture zooms wide to narrow or vice versa.
    - Fixed threading issue with validation.
    - Added support for Gobo images.
    - Adding spinning support to the color wheel

--- Modules/Editor/FixturePropertyEditor/Views/ZoomView.xaml.cs
+++ Modules/Editor/FixturePropertyEditor/Views/ZoomView.xaml.cs
@@ -1,16 +1,16 @@
 ﻿namespace VixenModules.Editor.FixturePropertyEditor.Views
 {
 	/// <summary>
-	/// Maintains fixture pan / tilt data view.
+	/// Maintains fixture zoom data view.
 	/// </summary>
-	public partial class PanTiltView
+	public partial class ZoomView
 	{
 		#region Constructor
 		
 		/// <summary>
 		/// Constructor
 		/// </summary>		
-		public PanTiltView()
+		public ZoomView()
 		{
 			// Initialize the user control
 			InitializeComponent();
