203dae08db4217020de75e52a747da82b5398af9
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-14 20:15:23 -0500

    VIX-3179 More Fixture Property Editor Updates
    The addition of fixture effects necessitated further changes to the property editor.
    
    - Added the ability to control whether the fixture zooms wide to narrow or vice versa.
    - Fixed threading issue with validation.
    - Added support for Gobo images.
    - Adding spinning support to the color wheel

---
+++ Modules/Editor/FixturePropertyEditor/Views/BindingProxy.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace VixenModules.Editor.FixturePropertyEditor.Views
+{
+	/// <summary>
+	/// https://thomaslevesque.com/2011/03/21/wpf-how-to-bind-to-data-when-the-datacontext-is-not-inherited/
+	/// </summary>
+	/// <remarks>This class helps with controlling the visibility of WPF data grid columns</remarks>
+	public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+		#endregion
+
+		#region Public Properties
+
+		public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        #endregion
+    }
+}
