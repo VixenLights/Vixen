c0e4e9d3021804dc0009e990b43f0e5a722a192c
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-15 19:31:10 -0500

    VIX-3200 Update Fixture Specification to support spinning color wheels and gobo images
    Also adding more built in functions to help users get started filling out their profile.  Built in functions include: Gobo, Prism, Open Close Prism, Frost.  These functions also help support fixture effects with similar names.

--- Modules/App/Fixture/FixtureFunction.cs
+++ Modules/App/Fixture/FixtureFunction.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.Serialization;
 using Vixen.Data.Value;
 using Vixen.TypeConverters;
@@ -32,17 +33,29 @@ namespace VixenModules.App.Fixture
 	}
 
 	/// <summary>
+	/// Defines the direction of the zoom.
+	/// </summary>
+	public enum FixtureZoomType
+	{
+		[Description("Narrow To Wide")]
+		NarrowToWide,
+
+		[Description("Wide To Narrow")]
+		WideToNarrow
+	}
+
+	/// <summary>
 	/// Maintains a fixture function.
 	/// </summary>
 	[DataContract]
 	public class FixtureFunction : FixtureItem
 	{
-        #region Constructor
+		#region Constructor
 
 		/// <summary>
 		/// Constructor
 		/// </summary>
-        public FixtureFunction()
+		public FixtureFunction()
 		{
 			// Initialize the collection of color wheel data
 			ColorWheelData = new List<FixtureColorWheel>();
@@ -50,13 +63,16 @@ namespace VixenModules.App.Fixture
 			// Initialize the collection of index (enumeration) data
 			IndexData = new List<FixtureIndex>();
 
+			// Initialize the rotation limits of the function
+			RotationLimits = new FixtureRotationLimits();
+
 			// Default the function identity to custom
 			FunctionIdentity = FunctionIdentity.Custom;
 		}
 
-        #endregion
+		#endregion
 
-        #region Public Properties
+		#region Public Properties
 		
 		/// <summary>
 		/// Type of the function.
@@ -69,7 +85,7 @@ namespace VixenModules.App.Fixture
 		/// </summary>
 		[DataMember]
 		public FunctionIdentity FunctionIdentity { get; set; }
-
+		
 		/// <summary>
 		/// Index (enumeration) data associated with the function.
 		/// </summary>
@@ -81,7 +97,7 @@ namespace VixenModules.App.Fixture
 		/// </summary>
 		[DataMember]
 		public List<FixtureColorWheel> ColorWheelData { get; set; }
-		
+
 		/// <summary>
 		/// Label associated with the function.
 		/// </summary>
@@ -94,15 +110,21 @@ namespace VixenModules.App.Fixture
 		[DataMember]
 		public FixtureRotationLimits RotationLimits { get; set; }
 
-        #endregion
+		/// <summary>
+		/// Indicates how the fixture zooms.
+		/// </summary>
+		[DataMember]
+		public FixtureZoomType ZoomType { get; set; }
 
-        #region Public Properties
+		#endregion
+
+		#region Public Properties
 
 		/// <summary>
 		/// Creates a clone of the fixture function.
 		/// </summary>
 		/// <returns>Clone of the fixture function</returns>
-        public FixtureFunction CreateInstanceForClone()
+		public FixtureFunction CreateInstanceForClone()
 		{
 			// Create a clone of the fixture function
 			FixtureFunction result = new FixtureFunction
@@ -111,6 +133,7 @@ namespace VixenModules.App.Fixture
 				FunctionType = FunctionType,
 				Label = Label,
 				FunctionIdentity = FunctionIdentity,				
+				ZoomType = ZoomType,	
 			};
 
 			// If rotation limits are defined then...
@@ -135,6 +158,36 @@ namespace VixenModules.App.Fixture
 			}
 
 			return result;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Retrieves normalized index data for the function.
+		/// This method is supported by both index functions and color wheel functions.
+		/// </summary>
+		/// <returns>Collection of index data</returns>
+		public List<FixtureIndexBase> GetIndexDataBase()
+		{
+			// Initialize the return collection to empty
+			List<FixtureIndexBase> indexData = new List<FixtureIndexBase>();
+
+			// If the function is an index function then...
+			if (FunctionType == FixtureFunctionType.Indexed)
+			{
+				// Retrieve the fixture index data
+				indexData = IndexData.Cast<FixtureIndexBase>().ToList();
+			}
+			// Otherwise if the function is a color wheel function then...
+			else if (FunctionType == FixtureFunctionType.ColorWheel)
+			{
+				// Retrieve the function color wheel index data
+				indexData = ColorWheelData.Cast<FixtureIndexBase>().ToList();
+			}
+
+			return indexData;
 		}
 
 		#endregion
