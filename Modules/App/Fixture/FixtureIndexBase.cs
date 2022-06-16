eee2868bbdbff06f3fd6d364eeb5dfd3f609f4c6
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-15 19:31:10 -0500

    VIX-3200 Update Fixture Specification to support spinning color wheels and gobo images
    Also adding more built in functions to help users get started filling out their profile.  Built in functions include: Gobo, Prism, Open Close Prism, Frost.  These functions also help support fixture effects with similar names.

---
+++ Modules/App/Fixture/FixtureIndexBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Serialization;
+using Vixen.Commands;
+
+namespace VixenModules.App.Fixture
+{
+	/// <summary>
+	/// Maintains a fixture index item.
+	/// </summary>
+	[DataContract]
+	public abstract class FixtureIndexBase : FixtureItem
+	{
+		#region Constructor 
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FixtureIndexBase()
+		{
+			// Default to custom index type
+			IndexType = FixtureIndexType.Custom;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// DMX start value of the entry.
+		/// </summary>
+		[DataMember]
+		public int StartValue { get; set; }
+
+		/// <summary>
+		/// DMX end value of the entry.
+		/// </summary>
+		[DataMember]
+		public int EndValue { get; set; }
+
+		/// <summary>
+		/// Indicates that this index entry selection needs to be represented by a curve.
+		/// </summary>
+		[DataMember]
+		public virtual bool UseCurve { get; set; }
+
+		/// <summary>
+		/// Type of index.  This property supports the preview.
+		/// </summary>
+		[DataMember]
+		public FixtureIndexType IndexType { get; set; }
+
+		#endregion
+	}
+}
