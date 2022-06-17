63dbd328ee9157dec957aadd5506ca3662db8c80
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-17 16:06:45 -0500

    VIX-3074 Set Position Effect
    VIX-3074 Set Position Effect

---
+++ Modules/Effect/SetPosition/SetPositionData.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.Serialization;
+using VixenModules.App.Curves;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.SetPosition
+{
+	/// <summary>	
+	/// Maintains the Set Position effect data.
+	/// </summary>	
+	[DataContract]
+	public class SetPositionData : EffectTypeModuleData
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public SetPositionData()
+		{
+			Pan = new Curve(CurveType.RampUp);
+			Tilt = new Curve(CurveType.RampDown);
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets or sets pan curve data.
+		/// </summary>
+		[DataMember]
+		public Curve Pan { get; set; }
+
+		/// <summary>
+		/// Gets or sets tilt curve data.
+		/// </summary>
+		[DataMember]
+		public Curve Tilt { get; set; }
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Creates a clone of the Set Position data.
+		/// </summary>
+		/// <returns>Clone of the Set Position data</returns>
+		protected override EffectTypeModuleData CreateInstanceForClone()
+		{
+			// Create a new Set Position data instance
+			SetPositionData result = new SetPositionData
+			{
+				// Copy the curve data
+				Pan = new Curve(Pan),
+				Tilt = new Curve(Tilt)
+			};
+			return result;
+		}
+
+		#endregion
+	}
+}
