1d9133bccfdbfcc9de13580d0e7111befbfeaaac
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-17 20:58:24 -0500

    VIX-3205 Create Intelligent Fixture Prism Effect
    VIX-3205 Create Intelligent Fixture Prism Effect

---
+++ Modules/Effect/Prism/Converters/PrismFixtureIndexConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Prism
+{
+	/// <summary>
+	/// Provides the prism value names associated with a target node.
+	/// </summary>
+	public class PrismFixtureIndexCollectionNameConverter : EffectListTypeConverterBase
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Retrieves the collection of available prism index value names.
+		/// </summary>
+		/// <param name="context">Prism effect instance</param>
+		/// <returns>Collection of available prism index values</returns>
+		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			// Convert the context to the prism effect module
+			PrismModule prismEffect = (PrismModule)context.Instance;
+			
+			// Return the collection of prism index names
+			return new TypeConverter.StandardValuesCollection(prismEffect.IndexValues.ToArray());
+		}
+
+		#endregion
+	}
+}
---
+++ Modules/Effect/Prism/PrismDescriptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Vixen.Module.Effect;
+using Vixen.Sys;
+
+namespace VixenModules.Effect.Prism
+{
+	/// <summary>
+	/// Descriptor for the prism effect.
+	/// </summary>
+	public class PrismDescriptor : EffectModuleDescriptorBase
+	{
+		#region Fields
+
+		/// <summary>
+		/// Type ID for the effect.
+		/// </summary>
+		private Guid _typeId = new Guid("{A9F4EABD-143B-4477-9CF5-01E7E8A5C633}");
+
+		#endregion
+
+		#region IModuleDescriptor
+
+		/// <inheritdoc />
+		public override string TypeName => "Prism";
+
+		/// <inheritdoc />
+		public override Guid TypeId => _typeId;
+
+		/// <inheritdoc />
+		public override Type ModuleClass => typeof(PrismModule);
+
+		/// <inheritdoc />
+		public override Type ModuleDataClass => typeof(PrismData);
+
+		/// <inheritdoc />
+		public override string Author => "Vixen Team";
+
+		/// <inheritdoc />
+		public override string Description => "Configures the prism of an intelligent fixture";
+
+		/// <inheritdoc />
+		public override string Version => "1.0";
+
+		#endregion
+
+		#region Public Properites
+
+		/// <summary>
+		/// Gets the effect name.
+		/// </summary>
+		public override string EffectName => "Prism";
+
+		/// <inheritdoc />
+		public override ParameterSignature Parameters { get; }
+
+		/// <inheritdoc />
+		public override EffectGroups EffectGroup => EffectGroups.IntelligentFixture;
+
+		#endregion
+	}
+}
