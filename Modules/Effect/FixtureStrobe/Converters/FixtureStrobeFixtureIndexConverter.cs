35bb7ef80b5935063c65961d2c49f3d26b8f56e8
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-18 10:04:37 -0500

    VIX-3199 Create Intelligent Fixture Strobe Effect
    VIX-3194 Create Intelligent Fixture Strobe Effect

---
+++ Modules/Effect/FixtureStrobe/Converters/FixtureStrobeFixtureIndexConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.FixtureStrobe
+{
+	/// <summary>
+	/// Provides the fixture strobe index names associated with a target node.
+	/// </summary>
+	public class FixtureStrobeFixtureIndexCollectionNameConverter : EffectListTypeConverterBase
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Retrieves the collection of available fixture strobe index value names.
+		/// </summary>
+		/// <param name="context">Fixture strobe effect instance</param>
+		/// <returns>Collection of available fixture strobe index values</returns>
+		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			// Convert the context to the fixture strobe effect module
+			FixtureStrobeModule fixtureStrobeEffect = (FixtureStrobeModule)context.Instance;
+			
+			// Return the collection of fixture strobe index names
+			return new TypeConverter.StandardValuesCollection(fixtureStrobeEffect.IndexValues.ToArray());
+		}
+
+		#endregion
+	}
+}
---
+++ Modules/Effect/FixtureStrobe/FixtureStrobeData.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.Serialization;
+using VixenModules.App.Curves;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.FixtureStrobe
+{
+	/// <summary>	
+	/// Maintains the fixture strobe effect data.
+	/// </summary>	
+	[DataContract]
+	public class FixtureStrobeData : EffectTypeModuleData
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FixtureStrobeData()
+		{
+			// Default the strobe rotation curve to ramp up
+			FixtureStrobeCurve = new Curve(CurveType.RampUp);
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Fixture strobe function name.
+		/// </summary>
+		[DataMember]
+		public string FixtureStrobeFunctionName { get; set; }
+
+		/// <summary>
+		/// Fixture strobe index name.
+		/// </summary>
+		[DataMember]
+		public string FixtureStrobeIndexValue { get; set; }
+
+		/// <summary>
+		/// Curve for the fixture strobe.
+		/// </summary>
+		[DataMember]
+		public Curve FixtureStrobeCurve { get; set; }
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Creates a clone of the fixture strobe data.
+		/// </summary>
+		/// <returns>Clone of the fixture strobe data</returns>
+		protected override EffectTypeModuleData CreateInstanceForClone()
+		{
+			// Create a new fixture strobe data instance
+			FixtureStrobeData result = new FixtureStrobeData
+			{
+				// Copy the fixture strobe function name
+				FixtureStrobeFunctionName = FixtureStrobeFunctionName,
+				
+				// Copy the fixture strobe index value
+				FixtureStrobeIndexValue = FixtureStrobeIndexValue,
+
+				// Copy the curve
+				FixtureStrobeCurve = new Curve(FixtureStrobeCurve),				
+			};
+
+			return result;
+		}
+
+		#endregion
+	}
+}
