53f53e3be8f03dd1c32b8cc843f2071f00624725
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-17 20:58:24 -0500

    VIX-3205 Create Intelligent Fixture Prism Effect
    VIX-3205 Create Intelligent Fixture Prism Effect

---
+++ Modules/Effect/Prism/Converters/PrismFunctionNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Vixen.Data.Value;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Prism
+{
+	/// <summary>
+	/// Provides the prism function names associated with a target node.
+	/// </summary>
+	public class PrismFunctionNameConverter : EffectListTypeConverterBase
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Retrieves the collection of available prism function names.
+		/// </summary>
+		/// <param name="context">Prism effect instance</param>
+		/// <returns>Collection of available prism function names</returns>		
+		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			// Convert the context to a Prism effect module
+			PrismModule prismEffect = (PrismModule)context.Instance;
+
+			// Return the prism function names associated with effect 
+			return new TypeConverter.StandardValuesCollection(prismEffect.GetMatchingFunctionNames(FunctionIdentity.Prism).ToArray());
+		}
+
+		#endregion
+	}
+}
