2c41cb116a9db81efcb4ea88d00a5db0c22a5915
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-06-18 08:55:18 -0500

    VIX-3194 Create Intelligent Fixture Spin Color Wheel Effect
    VIX-3194 Create Intelligent Fixture Spin Color Wheel Effect

---
+++ Modules/Effect/SpinColorWheel/SpinColorWheelData.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.Serialization;
+using VixenModules.App.Curves;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.SpinColorWheel
+{
+	/// <summary>	
+	/// Maintains the spin color wheel effect data.
+	/// </summary>	
+	[DataContract]
+	public class SpinColorWheelData : EffectTypeModuleData
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public SpinColorWheelData()
+		{
+			// Default the color wheel rotation curve to ramp up
+			SpinColorWheelCurve = new Curve(CurveType.RampUp);
+
+			// Default the intensity to full
+			Intensity = new Curve(CurveType.Flat100);
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Selected spin color wheel function name.
+		/// </summary>
+		[DataMember]
+		public string SpinColorWheelFunctionName { get; set; }
+
+		/// <summary>
+		/// Selected Spin Color Wheel index value.
+		/// </summary>
+		[DataMember]
+		public string SpinColorWheelIndexValue { get; set; }
+
+		/// <summary>
+		/// Curve for the position or rotation of the color wheel.
+		/// </summary>
+		[DataMember]
+		public Curve SpinColorWheelCurve { get; set; }
+
+		/// <summary>
+		/// Curve for the intensity of the light.
+		/// </summary>
+		[DataMember]
+		public Curve Intensity { get; set; }
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Creates a clone of the spin color wheel data.
+		/// </summary>
+		/// <returns>Clone of the spin color wheel data</returns>
+		protected override EffectTypeModuleData CreateInstanceForClone()
+		{
+			// Create a new spin color wheel data instance
+			SpinColorWheelData result = new SpinColorWheelData
+			{
+				// Copy the spin color wheel function name
+				SpinColorWheelFunctionName = SpinColorWheelFunctionName,
+				
+				// Copy the spin color wheel index value
+				SpinColorWheelIndexValue = SpinColorWheelIndexValue,
+
+				// Copy the spin color wheel curve
+				SpinColorWheelCurve = new Curve(SpinColorWheelCurve),				
+
+				// Copy the light intensity
+				Intensity = new Curve(Intensity),
+			};
+
+			return result;
+		}
+
+		#endregion
+	}
+}
---
+++ Modules/Effect/SpinColorWheel/SpinColorWheelDescriptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Vixen.Module.Effect;
+using Vixen.Sys;
+
+namespace VixenModules.Effect.SpinColorWheel
+{
+	/// <summary>
+	/// Descriptor for the Spin Color Wheel effect.
+	/// </summary>
+	public class SpinColorWheelDescriptor : EffectModuleDescriptorBase
+	{
+		#region Fields
+
+		/// <summary>
+		/// Type ID for the effect.
+		/// </summary>
+		private Guid _typeId = new Guid("{6E3623B6-258A-4CD7-8D0F-A9BAA575A5C4}");
+
+		#endregion
+
+		#region IModuleDescriptor
+
+		/// <inheritdoc />
+		public override string TypeName => "SpinColorWheel";
+
+		/// <inheritdoc />
+		public override Guid TypeId => _typeId;
+
+		/// <inheritdoc />
+		public override Type ModuleClass => typeof(SpinColorWheelModule);
+
+		/// <inheritdoc />
+		public override Type ModuleDataClass => typeof(SpinColorWheelData);
+
+		/// <inheritdoc />
+		public override string Author => "Vixen Team";
+
+		/// <inheritdoc />
+		public override string Description => "Spins the color wheel of an intelligent fixture";
+
+		/// <inheritdoc />
+		public override string Version => "1.0";
+
+		#endregion
+
+		#region Public Properites
+
+		/// <summary>
+		/// Gets the effect name.
+		/// </summary>
+		public override string EffectName => "Spin Color Wheel";
+
+		/// <inheritdoc />
+		public override ParameterSignature Parameters { get; }
+
+		/// <inheritdoc />
+		public override EffectGroups EffectGroup => EffectGroups.IntelligentFixture;
+
+		#endregion
+	}
+}
