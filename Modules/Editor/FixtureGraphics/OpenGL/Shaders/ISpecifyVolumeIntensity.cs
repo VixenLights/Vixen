bee15ce0c40f3148dc05215e5d2e950c3da071bb
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-01 09:26:15 -0500

    VIX-3212 Add WPF and OpenGL Fixture Graphics
    VIX-3212 Add WPF and OpenGL Fixture Graphics

---
+++ Modules/Editor/FixtureGraphics/OpenGL/Shaders/ISpecifyVolumeIntensity.cs
@@ -0,0 +1,13 @@
+﻿namespace VixenModules.Editor.FixtureGraphics.OpenGL.Shaders
+{
+	/// <summary>
+	/// Maintains the intensity of a volume.
+	/// </summary>
+	public interface ISpecifyVolumeIntensity
+	{
+		/// <summary>
+		/// Intensity of the volume.
+		/// </summary>
+		int Intensity { get; set; }
+	}
+}
