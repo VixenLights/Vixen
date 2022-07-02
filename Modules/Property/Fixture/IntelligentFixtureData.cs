8539b8311289f57a57d704793e29cb4e4be2c2f5
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-02 09:05:35 -0500

    VIX-3181 Create Intelligent Fixture Property
    VIX-3181 Create Intelligent Fixture Property

---
+++ Modules/Property/Fixture/IntelligentFixtureData.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.Serialization;
+using Vixen.Module;
+using VixenModules.App.Fixture;
+
+namespace VixenModules.Property.IntelligentFixture
+{
+    /// <summary>
+    /// Intelligent fixture property data module.
+    /// </summary>
+    [DataContract]
+	public class IntelligentFixtureData : ModuleDataModelBase
+	{
+        #region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+        public IntelligentFixtureData()
+		{
+			// Create a new empty fixture profile
+			FixtureSpecification = new FixtureSpecification();
+			FixtureSpecification.InitializeBuiltInFunctions();			
+		}
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Clones the intelligent fixture property.
+        /// </summary>
+        /// <returns>Clone of the intelligent fixture property</returns>
+        public override IModuleDataModel Clone()
+		{
+			// Create a new intelligent fixture data module
+			IntelligentFixtureData clone = new IntelligentFixtureData();
+
+			// Clone the fixture profile
+			clone.FixtureSpecification = FixtureSpecification.CreateInstanceForClone();
+
+			// Return the clone fixture property data module
+			return clone;
+		}
+
+        #endregion
+
+        #region Public Properties
+
+		/// <summary>
+		/// Fixture specification or profile associated with the property.
+		/// </summary>
+        [DataMember]
+		public FixtureSpecification FixtureSpecification
+		{
+			get;
+			set;
+		}
+
+		#endregion			
+	}
+}
