0a758ce75d7a958aca151681e6431c8f79e2287e
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-09 20:28:32 -0500

    Add XML schema version tag or property to fixture specification
    Add XML schema version tag or property to fixture specification

---
+++ Vixen.System/Sys/IDeepCopy.cs
@@ -0,0 +1,14 @@
+﻿namespace Vixen.Sys
+{
+	/// <summary>
+	/// Makes a deep copy of the specified source object.
+	/// </summary>
+	public interface IDeepCopy
+	{
+		/// <summary>
+		/// Copies the contents of the source object into this object.
+		/// </summary>
+		/// <param name="source">Source to copy from</param>
+		void Copy(object source);
+	}
+}
