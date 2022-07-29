aedda535d085fa863c1fc301ca9322ac48c940f7
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-28 19:26:47 -0500

    VIX-3176 Original Orc Wizard 4.8.2
    VIX-3176 Original Orc Wizard 4.8.2

---
+++ Common/Orc.Wizard/Models/Extensions/IWizardPageExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Orc.Wizard
+{
+    using System.Threading.Tasks;
+    using Catel;
+
+    public static class IWizardPageExtensions
+    {
+        public static Task MoveForwardOrResumeAsync(this IWizardPage wizardPage)
+        {
+            Argument.IsNotNull(() => wizardPage);
+
+            return wizardPage.Wizard.MoveForwardOrResumeAsync();
+        }
+    }
+}
---
+++ Common/Orc.Wizard/Services/Interfaces/IWizardService.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWizardService.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Wizard
+{
+    using System.Threading.Tasks;
+
+    public interface IWizardService
+    {
+        Task<bool?> ShowWizardAsync(IWizard wizard);
+    }
+}
