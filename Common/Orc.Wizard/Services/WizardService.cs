f39729e0563a6cfb8c0b9acc48881abed7b78f56
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-28 19:26:47 -0500

    VIX-3176 Original Orc Wizard 4.8.2
    VIX-3176 Original Orc Wizard 4.8.2

---
+++ Common/Orc.Wizard/Services/WizardService.cs
@@ -0,0 +1,42 @@
+﻿namespace Orc.Wizard
+{
+    using System.Threading.Tasks;
+    using Catel;
+    using Catel.Logging;
+    using Catel.Reflection;
+    using Catel.Services;
+    using ViewModels;
+
+    public class WizardService : IWizardService
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private readonly IUIVisualizerService _uiVisualizerService;
+
+        public WizardService(IUIVisualizerService uiVisualizerService)
+        {
+            Argument.IsNotNull(() => uiVisualizerService);
+
+            _uiVisualizerService = uiVisualizerService;
+        }
+
+        public Task<bool?> ShowWizardAsync(IWizard wizard)
+        {
+            Argument.IsNotNull(() => wizard);
+
+            Log.Debug("Showing wizard '{0}'", wizard.GetType().GetSafeFullName(false));
+
+            if (wizard is SideNavigationWizardBase)
+            {
+                return _uiVisualizerService.ShowDialogAsync<SideNavigationWizardViewModel>(wizard);
+            }
+
+            if (wizard is FullScreenWizardBase)
+            {
+                return _uiVisualizerService.ShowDialogAsync<FullScreenWizardViewModel>(wizard);
+            }
+
+            return _uiVisualizerService.ShowDialogAsync<WizardViewModel>(wizard);
+        }
+    }
+}
---
+++ Common/Orc.Wizard/Views/WizardWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿namespace Orc.Wizard.Views
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Automation.Peers;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+    using Automation;
+    using Catel.Threading;
+    using Catel.Windows;
+    using Catel.Windows.Threading;
+    using ViewModels;
+
+    /// <summary>
+    /// Interaction logic for WizardWindow.xaml
+    /// </summary>
+    public partial class WizardWindow
+    {
+        public WizardWindow()
+            : this(null)
+        {
+        }
+
+        public WizardWindow(WizardViewModel viewModel)
+            : base(viewModel, DataWindowMode.Custom, infoBarMessageControlGenerationMode: InfoBarMessageControlGenerationMode.Overlay)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoaded(EventArgs e)
+        {
+            base.OnLoaded(e);
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                UpdateOpacityMask();
+            });
+        }
+
+        protected override void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnViewModelPropertyChanged(e);
+
+#pragma warning disable WPF1014
+            if (e.HasPropertyChanged(nameof(IWizard.CurrentPage)))
+#pragma warning restore WPF1014
+            {
+#pragma warning disable AvoidAsyncVoid
+                Dispatcher.BeginInvoke(async () =>
+                {
+#pragma warning restore AvoidAsyncVoid
+                    var vm = (WizardViewModel) ViewModel;
+
+                    breadcrumb.CenterSelectedItem();
+                    breadcrumbProgress.UpdateProgress(vm.Wizard.CurrentPage.Number, vm.Wizard.Pages.Count());
+
+                    // We need to await the animation
+                    await TaskShim.Delay(WizardConfiguration.AnimationDuration);
+
+                    UpdateOpacityMask();
+                });
+            }
+        }
+
+        private void UpdateOpacityMask()
+        {
+            var scrollViewer = breadcrumb.FindVisualDescendantByType<ScrollViewer>();
+            if (scrollViewer is null)
+            {
+                return;
+            }
+
+            var opacityMask = new LinearGradientBrush();
+            if (scrollViewer.HorizontalOffset > 0d)
+            {
+                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 0d));
+                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.05d));
+            }
+
+            var scrollableWidth = scrollViewer.ScrollableWidth;
+            if (scrollableWidth > scrollViewer.HorizontalOffset)
+            {
+                opacityMask.GradientStops.Add(new GradientStop(Colors.Black, 0.95d));
+                opacityMask.GradientStops.Add(new GradientStop(Colors.Transparent, 1d));
+            }
+
+            breadcrumb.SetCurrentValue(OpacityMaskProperty, opacityMask.GradientStops.Count > 0 ? opacityMask : null);
+        }
+
+        //protected override AutomationPeer OnCreateAutomationPeer()
+        //{
+        //    return new WizardWindowPeer(this);
+        //}
+    }
+}
