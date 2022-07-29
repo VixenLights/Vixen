faa0a48278f07dba8746b3d6672fd2c12db81394
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-28 19:26:47 -0500

    VIX-3176 Original Orc Wizard 4.8.2
    VIX-3176 Original Orc Wizard 4.8.2

---
+++ Common/Orc.Wizard/Locators/Interfaces/IWizardPageViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWizardPageViewModelLocator.cs" company="WildGums">
+//   Copyright (c) 2013 - 2015 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Wizard
+{
+    using System;
+    using Catel.MVVM;
+
+    public interface IWizardPageViewModelLocator : IViewModelLocator
+    {
+    }
+}
---
+++ Common/Orc.Wizard/Models/EventArgs/NavigatingEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace Orc.Wizard
+{
+    using System;
+
+    public class NavigatingEventArgs : EventArgs
+    {
+        public NavigatingEventArgs(IWizardPage from, IWizardPage to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public IWizardPage From { get; }
+
+        public IWizardPage To { get; }
+
+        public bool Cancel { get; set; }
+    }
+}
