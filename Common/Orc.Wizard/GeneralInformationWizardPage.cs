e4868af90b75600b58fc78a175797fc660b6135c
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-28 19:26:47 -0500

    VIX-3176 Original Orc Wizard 4.8.2
    VIX-3176 Original Orc Wizard 4.8.2

---
+++ Common/Orc.Wizard/GeneralInformationWizardPage.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GeneralInformationWizardPage.cs" company="WildGums">
+//   Copyright (c) 2013 - 2015 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Wizard
+{
+    using System;
+    using System.Globalization;
+
+    public class GeneralInformationWizardPage : WizardPageBase
+    {
+        public GeneralInformationWizardPage()
+        {
+            
+        }
+
+        public string Name { get; set; }
+        public CultureInfo CultureInfo { get; set; }
+        public DayOfWeek FirstDayOfWeek { get; set; }
+        public string ShortTimeFormat { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Common/Orc.Wizard/Models/SideNavigationWizardBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Orc.Wizard
+{
+    using Catel.IoC;
+
+    public abstract class SideNavigationWizardBase : WizardBase
+    {
+        protected SideNavigationWizardBase(ITypeFactory typeFactory) 
+            : base(typeFactory)
+        {
+        }
+
+        public bool ShowFullScreen { get; set; }
+    }
+}
