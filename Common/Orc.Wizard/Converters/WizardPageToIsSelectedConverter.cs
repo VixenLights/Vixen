ed560f2adeaee14846b7ff9b9794a0baf8c2e154
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-07-28 19:26:47 -0500

    VIX-3176 Original Orc Wizard 4.8.2
    VIX-3176 Original Orc Wizard 4.8.2

---
+++ Common/Orc.Wizard/Converters/WizardPageToIsSelectedConverter.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WizardPageToIsSelectedConverter.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Wizard.Converters
+{
+    using System;
+    using Catel.MVVM.Converters;
+
+    public class WizardPageToIsSelectedConverter : ValueConverterBase<IWizardPage>
+    {
+        protected override object Convert(IWizardPage value, Type targetType, object parameter)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+
+            var wizard = value.Wizard;
+
+            if (ReferenceEquals(wizard.CurrentPage, value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
