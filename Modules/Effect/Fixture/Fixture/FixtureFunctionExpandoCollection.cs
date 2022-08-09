58094d91f8c592f8a69a0e5f8dea36cb2bad73ba
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-08 21:18:16 -0500

    VIX-3189 Fixture Catch All Effect
    VIX-3189 Fixture Catch All Effect

---
+++ Modules/Effect/Fixture/Fixture/FixtureFunctionExpandoCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Fixture
+{
+	/// <summary>
+	/// Maintains a collection of fixture function expando objects.
+	/// </summary>
+	public class FixtureFunctionExpandoCollection : ExpandoObjectObservableCollection<IFixtureFunctionExpando, FixtureFunctionExpando>		
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FixtureFunctionExpandoCollection() :
+			base("FixtureFunctions")
+		{
+		}
+
+		#endregion
+
+		#region IExpandoObjectCollection
+
+		/// <summary>
+		/// Refer to interface documentation.
+		/// </summary>		
+		public override int GetMinimumItemCount()
+		{
+			// The fixture function collection starts out empty
+			return 0;
+		}
+
+		#endregion
+	}
+}
