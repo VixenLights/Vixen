858b40bf7d589331aea0ad3fd618613ebd996984
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-08 21:18:16 -0500

    VIX-3189 Fixture Catch All Effect
    VIX-3189 Fixture Catch All Effect

---
+++ Modules/Effect/Fixture/Fixture/Data/FixtureData.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Fixture
+{
+	/// <summary>
+	/// Maintains the fixture effect data.
+	/// </summary>
+	[DataContract]
+	public class FixtureData : EffectTypeModuleData
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FixtureData()
+		{			
+			// Create the collection of fixture function data
+			FunctionData = new List<FixtureFunctionData>();								
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Maintains a collection fixture function data associated with the effect.
+		/// </summary>
+		[DataMember]
+		public List<FixtureFunctionData> FunctionData { get; set; }
+
+		/// <summary>
+		/// Collection of fixture functions from the fixture specification.
+		/// </summary>
+		[DataMember]
+		public List<App.Fixture.FixtureFunction> FixtureFunctions { get; set; }
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// Makes a clone of the fixture data.
+		/// </summary>
+		/// <returns>Clone of the fixture data</returns>
+		protected override EffectTypeModuleData CreateInstanceForClone()
+		{
+			// Create a fixture data instance
+			FixtureData clone = new FixtureData();
+						
+			// Loop over the fixture function data items
+			for (int index = 0; index < FunctionData.Count; index++)
+			{
+				// Make a clone of the specified fixture function
+				clone.FunctionData[index] = FunctionData[index].CreateInstanceForClone();
+			}
+
+			return clone;
+		}
+
+		#endregion
+	}
+}
