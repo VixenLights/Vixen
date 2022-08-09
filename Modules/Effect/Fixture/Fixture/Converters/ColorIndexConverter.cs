a720c131b4278412a9f52a129061a3b7f1c248e8
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-08 21:18:16 -0500

    VIX-3189 Fixture Catch All Effect
    VIX-3189 Fixture Catch All Effect

---
+++ Modules/Effect/Fixture/Fixture/Converters/ColorIndexConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using VixenModules.Effect.Effect;
+
+namespace VixenModules.Effect.Fixture
+{
+	/// <summary>
+	/// Provides the color wheel names associated with a target node.
+	/// </summary>
+	public class ColorIndexConverter : EffectListTypeConverterBase
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Gets a collection of color wheel names associated with the node(s).
+		/// </summary>
+		/// <param name="context">Effects associated with the request</param>
+		/// <returns>Collection of color wheel names</returns>
+		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			// Create the return collection
+			List<string> values = new List<string>();
+
+			// Cast the context to a fixture function expando object
+			FixtureFunctionExpando fixtureFunction = (FixtureFunctionExpando)context.Instance;
+
+			// Add the color wheel names to the return collection
+			values.AddRange(fixtureFunction.ColorWheelIndexData.Select(colorWheelItem => colorWheelItem.Name));
+									
+			return new TypeConverter.StandardValuesCollection(values.ToArray());
+		}
+
+		#endregion
+	}
+}
