8db07401f3b4a4a251201620cdd637129544e9f4
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-16 20:58:00 -0500

    VIX-3224 Fixture Profile Editor - Need to prevent adding new items when the current item is invalid
    VIX-3224 Fixture Profile Editor - Need to prevent adding new items when the current item is invalid

---
+++ Modules/Editor/FixturePropertyEditor/Views/IRefreshGrid.cs
@@ -0,0 +1,18 @@
+﻿namespace VixenModules.Editor.FixturePropertyEditor.Views
+{
+	/// <summary>
+	/// Interface to allow view model to refresh grid(s) contained in the view.
+	/// </summary>
+	/// <remarks>
+	/// This interface is necessary because the WPF Datagrid seems to hold
+	/// onto invalid rows after they have been deleted from the view model.
+	/// Without refreshing the grid it is left in a read-only state.
+	/// </remarks>
+	internal interface IRefreshGrid
+	{
+		/// <summary>
+		/// Refreshes the grid(s) in the view.
+		/// </summary>
+		void Refresh();
+	}
+}
