ec750bad9e89c20d1efeb54c2358089ec3f9cc71
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-24 20:39:29 -0500

    VIX-3180 Create Intelligent Fixture Wizard
    VIX-3180 Create Intelligent Fixture Wizard

---
+++ Modules/Editor/FixtureWizard/Wizard/Models/SummaryWizardPageBase.cs
@@ -0,0 +1,50 @@
+﻿using Orc.Wizard;
+
+namespace VixenModules.Editor.FixtureWizard.Wizard.Models
+{
+    /// <summary>
+    /// Base Wizard page that provides a summary string for the last page of the wizard.
+    /// </summary>
+    public abstract class SummaryWizardPageBase : FixtureWizardPageBase
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="helpURL">Help URL</param>
+        public SummaryWizardPageBase(string helpURL) : base(helpURL)
+        {            
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the summary info for the page.
+        /// This information is displayed on the last page of the wizard.
+        /// </summary>
+        /// <returns></returns>
+        public override ISummaryItem GetSummary()
+        {
+            return new SummaryItem
+            {
+                Title = Title,
+                Summary = GetSummaryString(),
+            };
+        }
+
+        #endregion
+      
+        #region Protected Methods
+
+        /// <summary>
+        /// Allows the derived page to construct the summary string.
+        /// </summary>
+        /// <returns>The string to display on the summary page</returns>
+        protected abstract string GetSummaryString();
+
+        #endregion
+    }
+}
---
+++ Modules/Editor/FixtureWizard/Wizard/Views/EditProfileWizardPageView.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace VixenModules.Editor.FixtureWizard.Wizard.Views
+{
+	/// <summary>
+	/// Maintains the edit profile automation wizard page view.
+	/// </summary>
+	public partial class EditProfileWizardPageView
+    {
+		#region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+		public EditProfileWizardPageView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        //TODO
+        //static EditProfileWizardPageView()
+        //{
+        //    typeof(EditProfileWizardPageView).AutoDetectViewPropertiesToSubscribe();
+        //}
+    }
+}
