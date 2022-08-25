797c8619b5cbad56e43702b89aad05202418d934
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-24 20:39:29 -0500

    VIX-3180 Create Intelligent Fixture Wizard
    VIX-3180 Create Intelligent Fixture Wizard

---
+++ Modules/Editor/FixtureWizard/Wizard/Models/EditProfileWizardPage.cs
@@ -0,0 +1,24 @@
+﻿using Orc.Wizard;
+
+namespace VixenModules.Editor.FixtureWizard.Wizard.Models
+{
+    /// <summary>
+	/// Wizard page edits the fixture's associated channels.
+	/// </summary>
+    public class EditProfileWizardPage : FixtureWizardPageBase
+    {
+		#region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+		public EditProfileWizardPage() :
+            base("http://www.vixenlights.com/vixen-3-documentation/setup-configuration/setup-display-elements/intelligent-fixture-wizard/edit-channels/")
+        {
+            Title = "Edit Channels";
+            Description = "Define the fixture's channels.";            
+        }
+
+        #endregion
+    }
+}
