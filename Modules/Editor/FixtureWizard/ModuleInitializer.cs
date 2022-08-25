7fcb1c36c9adea90dba6f310c6ef97ef9b10efbe
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-24 20:39:29 -0500

    VIX-3180 Create Intelligent Fixture Wizard
    VIX-3180 Create Intelligent Fixture Wizard

---
+++ Modules/Editor/FixtureWizard/ModuleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Catel.IoC;
+using Catel.IO;
+using Catel.MVVM;
+
+
+/// <summary>
+/// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
+/// </summary>
+public static class ModuleInitializer
+{
+    /// <summary>
+    /// Initializes the module.
+    /// </summary>
+    public static void Initialize()
+    {        
+    }
+}
---
+++ Modules/Editor/FixtureWizard/Wizard/Models/DimmingCurveSelection.cs
@@ -0,0 +1,23 @@
+﻿namespace VixenModules.Editor.FixtureWizard.Wizard.Models
+{
+	/// <summary>
+	/// Identifies if and how dimming curves should be applied to the fixture.
+	/// </summary>
+	public enum DimmingCurveSelection
+    {
+        /// <summary>
+        /// Don't add any dimming curves.
+        /// </summary>
+        NoDimmingCurve,
+
+        /// <summary>
+        /// Add one dimming curve to the fixture's color flow.
+        /// </summary>
+        OneDimmingCurvePerFixture,
+
+        /// <summary>
+        /// Add one dimming curve for each color channel.
+        /// </summary>
+        OneDimmingCurvePerColor,
+    };
+}
