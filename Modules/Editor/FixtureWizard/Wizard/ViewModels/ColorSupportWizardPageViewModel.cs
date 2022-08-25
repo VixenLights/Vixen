d3b7f016d307187c7f53d934ec38917089621a7c
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-08-24 20:39:29 -0500

    VIX-3180 Create Intelligent Fixture Wizard
    VIX-3180 Create Intelligent Fixture Wizard

---
+++ Modules/Editor/FixtureWizard/Wizard/ViewModels/ColorSupportWizardPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿namespace VixenModules.Editor.FixtureWizard.Wizard.ViewModels
+{
+	using Catel.MVVM;
+	using Orc.Wizard;
+	using System.Linq;
+	using VixenModules.Editor.FixtureWizard.Wizard.Models;
+
+	/// <summary>
+	/// Wizard view model page that configures color support for the fixture.
+	/// </summary>
+	public class ColorSupportWizardPageViewModel : WizardPageViewModelBase<ColorSupportWizardPage>
+    {
+		#region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="wizardPage">Color support wizard page model</param>
+		public ColorSupportWizardPageViewModel(ColorSupportWizardPage wizardPage) :
+            base(wizardPage)
+        {
+            // Retrieve the Select Fixture wizard page
+            SelectProfileWizardPage selectProfilePage = (SelectProfileWizardPage)Wizard.Pages.Single(page => page is SelectProfileWizardPage);
+
+            // If the fixture is color mixing then...
+            if (selectProfilePage.Fixture.IsColorMixing())
+            {
+                // Default the fixture to color mixing
+                ColorMixing = true;                
+            }
+            else if (selectProfilePage.Fixture.ContainsColorWheel())
+            {
+                // Default the fixture to using a color wheel
+                ColorWheel = true;
+            }  
+            else
+			{
+                // Default to color being handled by other means
+                NoColorSupport = true;
+			}
+        }
+
+		#endregion
+
+		#region Public Catel Properties
+
+        /// <summary>
+        /// Indicates the fixture supports color mixing.
+        /// </summary>
+		[ViewModelToModel]
+        public bool ColorMixing { get; set; }
+
+        /// <summary>
+        /// Indicates the fixture uses a color wheel to achieve colors.
+        /// </summary>
+        [ViewModelToModel]
+        public bool ColorWheel { get; set; }
+
+        /// <summary>
+        /// Indicates color is going to be handled by other means (individual pixel nodes).
+        /// </summary>
+        [ViewModelToModel]
+        public bool NoColorSupport { get; set; }
+
+        #endregion
+    }
+}
---
+++ Modules/Editor/FixtureWizard/Wizard/Views/ColorSupportWizardPageView.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace VixenModules.Editor.FixtureWizard.Wizard.Views
+{
+    /// <summary>
+    /// Maintains the color support wizard page view.
+    /// </summary>
+    public partial class ColorSupportWizardPageView
+    {
+		#region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+		public ColorSupportWizardPageView()
+        {
+            InitializeComponent();
+        }
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// User control loaded event, used to draw background graphics.
+		/// </summary>
+		/// <param name="sender">Event sender</param>
+		/// <param name="e"><Event arguments/param>
+		private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Draw two moving heads providing bookends to the widgets in the middle of the page
+            DrawMovingHeads(400, System.Drawing.Color.Blue, LeftMainViewport, RightMainViewport);
+        }
+
+        #endregion
+    }
+}
