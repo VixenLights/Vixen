fa27e1eb47ba958d1689dd405324311e9e0d6047
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-05 09:43:29 -0500

    VIX-3242 Add Intelligent Fixture support for 16 bit color channels
    VIX-3242 Add Intelligent Fixture support for 16 bit color channels

---
+++ Vixen.System/Data/Evaluator/CommandLookup16BitEvaluator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Vixen.Commands;
+
+namespace Vixen.Data.Evaluator
+{
+	/// <summary>
+	/// This class improves performance by creating re-useable 16 bit commands.
+	/// </summary>
+	public class CommandLookup16BitEvaluator
+	{
+		#region Public Properties
+
+		/// <summary>
+		/// Dictionary of 16-bit commands indexed by the command ushort value.
+		/// </summary>
+		public static Dictionary<ushort, _16BitCommand> CommandLookup { get; private set; }
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		static CommandLookup16BitEvaluator()
+		{
+			// Create the dictionary
+			CommandLookup = new Dictionary<ushort, _16BitCommand>();
+
+			// Loop over all possible ushort values
+			for (int i = 0; i <= ushort.MaxValue; i++)
+			{
+				// Add the specified value to the dictionary
+				CommandLookup.Add((ushort)i, new _16BitCommand((ushort)i));
+			}
+		}
+
+		#endregion
+	}
+}
