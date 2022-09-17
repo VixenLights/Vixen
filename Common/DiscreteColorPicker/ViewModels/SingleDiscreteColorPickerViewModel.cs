6526c489225aaa89fac0226fba25a6e8fd3ebfb0
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-17 09:14:24 -0500

    VIX-3197 Select Color(s) dialog only displays 9 discrete colors
    VIX-3197 Select Color(s) dialog only displays 9 discrete colors

---
+++ Common/DiscreteColorPicker/ViewModels/SingleDiscreteColorPickerViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Common.DiscreteColorPicker.ViewModels
+{
+	/// <summary>
+	/// Maintains a view model for selecting a discrete color.
+	/// </summary>
+	public class SingleDiscreteColorPickerViewModel : DiscreteColorPickerViewModelBase<ColorItem>
+	{
+		#region Protected Methods
+
+		/// <summary>
+		/// Enable or disables the OK button.
+		/// </summary>
+		/// <returns>True when the OK button is enabled.</returns>
+		protected override bool CanExecuteOK()
+		{
+			// Only enable the OK button when a color is selected
+			return SelectedItem != null;	
+		}
+
+		#endregion
+	}
+}
