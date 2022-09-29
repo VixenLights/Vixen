221c1c16f42640f1db6a9b8c3f641af1dd822c81
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-28 21:31:27 -0500

    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected
    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected

--- Modules/Effect/Gobo/Converters/GoboFunctionNameConverter.cs
+++ Modules/Effect/Gobo/Converters/GoboFunctionNameConverter.cs
@@ -1,28 +1,25 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
 using Vixen.Data.Value;
 using VixenModules.Effect.Effect;
 
 namespace VixenModules.Effect.Gobo
 {
 	/// <summary>
-	/// Provides the gobo function names associated with a target node.
+	/// Provides the gobo function names associated with the target node(s).
 	/// </summary>
-	public class GoboFunctionNameConverter : EffectListTypeConverterBase
+	public class GoboFunctionNameConverter : EffectListTypeConverterBase<GoboModule>
 	{
-		#region Public Methods
+		#region Protected Methods
 
 		/// <summary>
 		/// Retrieves the collection of available gobo function names.
 		/// </summary>
 		/// <param name="context">Gobo effect instance</param>
 		/// <returns>Collection of available gobo function names</returns>		
-		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		protected override List<string> GetStandardValuesInternal(GoboModule effect)
 		{
-			// Convert the context to a Gobo effect module
-			GoboModule theGoboEffect = (GoboModule)context.Instance;
-
 			// Return the gobo function names associated with effect 
-			return new TypeConverter.StandardValuesCollection(theGoboEffect.GetMatchingFunctionNames(FunctionIdentity.Gobo).ToArray());
+			return effect.GetMatchingFunctionNames(FunctionIdentity.Gobo);
 		}
 
 		#endregion
