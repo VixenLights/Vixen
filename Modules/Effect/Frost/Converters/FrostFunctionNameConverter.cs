612b95a31ced72a3983124657693fb795525591e
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-28 21:31:27 -0500

    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected
    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected

--- Modules/Effect/Frost/Converters/FrostFunctionNameConverter.cs
+++ Modules/Effect/Frost/Converters/FrostFunctionNameConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using Vixen.Data.Value;
 using VixenModules.Effect.Effect;
 using VixenModules.Effect.Frost;
@@ -6,24 +7,21 @@ using VixenModules.Effect.Frost;
 namespace VixenModules.Effect.Frost
 {
 	/// <summary>
-	/// Provides the frost function names associated with a target node.
+	/// Provides the frost function names associated with target node(s).
 	/// </summary>
-	public class FrostFunctionNameConverter : EffectListTypeConverterBase
+	public class FrostFunctionNameConverter : EffectListTypeConverterBase<FrostModule>
 	{
-		#region Public Methods
+		#region Protected Methods
 
 		/// <summary>
 		/// Retrieves the collection of available frost function names.
 		/// </summary>
 		/// <param name="context">Frost effect instance</param>
-		/// <returns>Collection of available frsot function names</returns>		
-		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		/// <returns>Collection of available frost function names</returns>		
+		protected override List<string> GetStandardValuesInternal(FrostModule effect)
 		{
-			// Convert the context to a Frost effect module
-			FrostModule theFrostEffect = (FrostModule)context.Instance;
-
 			// Return the frost function names associated with effect 
-			return new TypeConverter.StandardValuesCollection(theFrostEffect.GetMatchingFunctionNames(FunctionIdentity.Frost).ToArray());
+			return effect.GetMatchingFunctionNames(FunctionIdentity.Frost);
 		}
 
 		#endregion
