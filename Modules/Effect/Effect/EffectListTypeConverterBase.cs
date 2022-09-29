16c3b2d4b942ce7ce360f7d21b820573d6204c0d
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-28 21:31:27 -0500

    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected
    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected

--- Modules/Effect/Effect/EffectListTypeConverterBase.cs
+++ Modules/Effect/Effect/EffectListTypeConverterBase.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using Vixen.Module.Effect;
 
 namespace VixenModules.Effect.Effect
 {
 	/// <summary>
 	/// Base class for an effect converter that provides a list of available items.
 	/// </summary>
-	public abstract class EffectListTypeConverterBase : TypeConverter
+	/// <typeparam name="TEffectModule">Type of effect module</typeparam>
+	public abstract class EffectListTypeConverterBase<TEffectModule> : TypeConverter
+		where TEffectModule : class
 	{
 		#region Public Methods
 
@@ -40,6 +44,58 @@ namespace VixenModules.Effect.Effect
 		public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
 		{
 			return true;
+		}
+
+		protected abstract List<string> GetStandardValuesInternal(TEffectModule effectModule);
+
+
+		/// <summary>
+		/// Gets the standard values associated with the specified effect context.
+		/// </summary>
+		/// <param name="context">Single effect or a collection of effects</param>
+		/// <returns>Collection of string names</returns>
+		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			// Create the return collection
+			List<string> values = new List<string>();
+
+			// If the context is a single effect module then...
+			if (context.Instance is TEffectModule)
+			{
+				// Cast the context to the effect
+				TEffectModule effectModule = (TEffectModule)context.Instance;
+
+				// Retrieve the standard values from the effect
+				values = GetStandardValuesInternal(effectModule);
+			}
+			// Otherwise the context is a collection of effects
+			else if (context.Instance is IEffectModuleInstance[])
+			{
+				// Loop over the effects
+				foreach (IEffectModuleInstance effect in ((IEffectModuleInstance[])context.Instance))
+				{
+					// If the effect is the effect of interest then...
+					if (effect is TEffectModule)
+					{
+						// Cast the effect to the fixture effect
+						TEffectModule effectModule = (TEffectModule)effect;
+
+						// Loop over the fixture functions supported by the effect
+						foreach (string value in GetStandardValuesInternal(effectModule))
+						{
+							// If the string value is NOT already in the collection then...
+							if (!values.Contains(value))
+							{
+								// Add the string value to the collection
+								values.Add(value);
+							}
+						}
+					}
+				}
+			}
+
+			return new TypeConverter.StandardValuesCollection(values.ToArray());
+
 		}
 
 		#endregion
