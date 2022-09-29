4ad559e5cf56874b235e508fcf7479faa80e7a9d
johncbaur <46469358+johncbaur@users.noreply.github.com>
2022-09-28 21:31:27 -0500

    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected
    VIX-3280 Update Intelligent Fixture Effects converters to handle when more than one effect instance is selected

--- Modules/Effect/Gobo/Converters/GoboFixtureIndexConverter.cs
+++ Modules/Effect/Gobo/Converters/GoboFixtureIndexConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using VixenModules.Effect.Effect;
 
 namespace VixenModules.Effect.Gobo
@@ -6,22 +7,19 @@ namespace VixenModules.Effect.Gobo
 	/// <summary>
 	/// Provides the gobo value names associated with a target node.
 	/// </summary>
-	public class GoboFixtureIndexCollectionNameConverter : EffectListTypeConverterBase
+	public class GoboFixtureIndexCollectionNameConverter : EffectListTypeConverterBase<GoboModule>
 	{
-		#region Public Methods
+		#region protected Methods
 
 		/// <summary>
 		/// Retrieves the collection of available gobo index value names.
 		/// </summary>
 		/// <param name="context">Gobo effect instance</param>
-		/// <returns>Collection of available gobo index values</returns>
-		public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		/// <returns>Collection of available gobo index names</returns>
+		protected override List<string> GetStandardValuesInternal(GoboModule theGoboEffect)
 		{
-			// Convert the context to a Gobo effect module
-			GoboModule theGoboEffect = (GoboModule)context.Instance;
-			
 			// Return the collection of gobo index names
-			return new TypeConverter.StandardValuesCollection(theGoboEffect.IndexValues.ToArray());
+			return theGoboEffect.IndexValues;
 		}
 
 		#endregion
