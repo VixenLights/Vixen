3d1e06ceb586920fe7963850b60923fad0452e40
Jeff Uchitjil <jeffu@kb9kld.org>
2022-12-26 11:32:49 -0600

    VIX-3345 Add Root name space to the preview, property, and timing modules.

--- src/Vixen.Modules/Preview/VixenPreview/Properties/Resources.Designer.cs
+++ src/Vixen.Modules/Preview/VixenPreview/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VixenPreview.Properties {
+namespace VixenModules.Preview.VixenPreview.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace VixenPreview.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VixenPreview.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VixenModules.Preview.VixenPreview.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
