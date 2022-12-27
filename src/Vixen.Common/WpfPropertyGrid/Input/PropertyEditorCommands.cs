d54f9e9cc278d7293e515ff5dd2d1eba089c568a
Jeff Uchitjil <jeffu@kb9kld.org>
2022-12-27 08:56:11 -0600

    VIX-3345 Custom Prop Editor fixes.
    * Add WpfPropertyGrid as a project under common to get it included for visibility vs an included dll. Can we wean ourselves off of this library?
    * Adjust includes to be project includes vs assembly includes in the Timing track browser and the custom prop editor to use the above library.
    * Add the WPF Property grid as a dependency to the Vixen.Application project to solve an assembly loader problem. Since the CPE is loaded from the the main form via some dynamic menu includes, the WPG needs to be in the dependency tree so it will resolve correctly. This feels like a 100% hack, but currently I am under how to get around it. This gets things working to move forward.

---
+++ src/Vixen.Common/WpfPropertyGrid/Input/PropertyEditorCommands.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Copyright © 2010, Denys Vuika
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Windows.Input;
+
+namespace System.Windows.Controls.WpfPropertyGrid
+{
+  /// <summary>
+  ///Provides a standard set of property editor related commands.
+  /// </summary>
+  public static class PropertyEditorCommands
+  {
+    private static readonly Type ThisType = typeof(PropertyEditorCommands);
+        
+    private static readonly RoutedUICommand _ShowDialogEditor = new RoutedUICommand("Show Dialog Editor", "ShowDialogEditorCommand", ThisType);
+    private static readonly RoutedUICommand _ShowExtendedEditor = new RoutedUICommand("Show Extended Editor", "ShowExtendedEditorCommand", ThisType);
+
+    /// <summary>
+    /// Defines a command to show dialog editor for a property.
+    /// </summary>    
+    public static RoutedUICommand ShowDialogEditor
+    {
+      get { return _ShowDialogEditor; }
+    }
+
+    /// <summary>
+    /// Defines a command to show extended editor for a property.
+    /// </summary>
+    public static RoutedUICommand ShowExtendedEditor
+    {
+      get { return _ShowExtendedEditor; }
+    }
+  }
+}
