535e0eb83a7984d15b4675c560dd697b8c479ffd
johncbaur <46469358+johncbaur@users.noreply.github.com>
2023-01-22 17:28:33 -0600

    VIX-3380 Create Basket Weave Pattern Effect
    VIX-3380 Create Basket Weave Pattern Effect

---
+++ src/Vixen.Modules/Effect/Weave/WeaveDescriptor.cs
@@ -0,0 +1,60 @@
+﻿using Vixen.Module.Effect;
+using Vixen.Sys;
+
+namespace VixenModules.Effect.Weave
+{
+	/// <summary>
+	/// Descriptor for the Weave effect.
+	/// </summary>
+	public class WeaveDescriptor : EffectModuleDescriptorBase
+	{
+		#region Fields
+
+		/// <summary>
+		/// Type ID for the effect.
+		/// </summary>
+		private static readonly Guid _typeId = new Guid("4633DE41-BB29-443F-A390-23EA0C464AB2");
+
+		#endregion
+
+		#region IModuleDescriptor
+
+		/// <inheritdoc />
+		public override string TypeName => EffectName;
+
+		/// <inheritdoc />
+		public override Guid TypeId => _typeId;
+
+		/// <inheritdoc />
+		public override Type ModuleClass => typeof(Weave);
+
+		/// <inheritdoc />
+		public override Type ModuleDataClass => typeof(WeaveData);
+
+		/// <inheritdoc />
+		public override string Author => "Vixen Team";
+
+		/// <inheritdoc />
+		public override string Description => "Applies a Weave effect to pixel elements";
+
+		/// <inheritdoc />
+		public override string Version => "1.0";
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets the effect name.
+		/// </summary>
+		public override string EffectName => "Weave";
+
+		/// <inheritdoc />
+		public override ParameterSignature Parameters { get; }
+
+		/// <inheritdoc />
+		public override EffectGroups EffectGroup => EffectGroups.Pixel;
+
+		#endregion
+	}
+}
