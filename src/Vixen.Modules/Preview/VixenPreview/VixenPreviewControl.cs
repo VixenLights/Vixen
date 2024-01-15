1d7896cec0afdd16b6b61f507f3176ecc71a7643
johncbaur <46469358+johncbaur@users.noreply.github.com>
2024-01-15 05:25:03 -0600

    VIX-3511 Restore rubber band behavior
    VIX-3511 Restore rubber band behavior

--- src/Vixen.Modules/Preview/VixenPreview/VixenPreviewControl.cs
+++ src/Vixen.Modules/Preview/VixenPreview/VixenPreviewControl.cs
@@ -1118,7 +1118,7 @@ namespace VixenModules.Preview.VixenPreview
 						_selectedDisplayItem.Shape.MouseMove(dragCurrent.X, dragCurrent.Y, changeX, changeY);
 						EndUpdate();
 					}
-						// If we get here, we're drawing a rubber band
+					// If we get here, we're drwing a rubber band
 					else if (_banding)
 					{
 						int X1 = Math.Min(dragStart.X, dragStart.X + changeX);
@@ -1130,8 +1130,10 @@ namespace VixenModules.Preview.VixenPreview
 
 						foreach (DisplayItem item in DisplayItems)
 						{
-							if (item.Shape.ShapeInRect(_bandRect) ||
-								item.Shape.ShapeAllInRect(_bandRect))
+							if (
+								(changeX < 0 && item.Shape.ShapeInRect(_bandRect)) ||
+								(changeX > 0 && item.Shape.ShapeAllInRect(_bandRect))
+								)
 							{
 								if (!SelectedDisplayItems.Contains(item))
 								{
