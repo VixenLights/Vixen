0e6446af7444b113ab42265a451cdbb78aabe629
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-03-12 05:01:38 -0500

    VIX-3663 Adding WPF Preview
    More core classes to support the WPF preview.

---
+++ src/Vixen.Core/Module/Preview/WindowPreviewModuleInstanceBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Vixen.Sys;
+
+namespace Vixen.Module.Preview
+{
+	public abstract class WindowPreviewModuleInstanceBase : PreviewModuleInstanceBase
+	{
+		private IThreadBehavior _threadBehavior;
+
+		protected override IThreadBehavior ThreadBehavior
+		{
+			get
+			{
+				if (_threadBehavior == null)
+				{
+					// Needs to be cast to avoid ambiguity since there are no parameters
+					// to differentiate the signature.
+					Func<Window> initMethod = Initialize;
+					_threadBehavior = //VixenSystem.SystemConfig.IsPreviewThreaded
+										  new WPFMultiThreadBehavior(initMethod);
+										  //: (IThreadBehavior)new SingleThreadBehavior(initMethod);
+				}
+				return _threadBehavior;
+			}
+		}
+
+		protected abstract Window Initialize();
+	}
+}
