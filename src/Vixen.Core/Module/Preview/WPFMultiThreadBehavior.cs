b4cff63c7f25857ab3e60490b4fc44638f6261e8
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-03-12 05:01:38 -0500

    VIX-3663 Adding WPF Preview
    More core classes to support the WPF preview.

---
+++ src/Vixen.Core/Module/Preview/WPFMultiThreadBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vixen.Sys;
+using System.Windows;
+using System.IO;
+
+namespace Vixen.Module.Preview
+{
+	
+	public class WPFMultiThreadBehavior : IThreadBehavior
+	{
+		private Func<WPFUIThread> _threadInit;
+		private WPFUIThread _thread;
+		private Func<Window> _windowInit;
+		private Func<ApplicationContext> _appContextInit;
+
+		public WPFMultiThreadBehavior(Func<Window> windowInit)
+		{
+			if (windowInit == null) throw new InvalidOperationException();
+			_windowInit = windowInit;
+			_threadInit = _GetFormThread;
+		}
+
+		public WPFMultiThreadBehavior(Func<ApplicationContext> appContextInit)
+		{
+			if (appContextInit == null) throw new InvalidOperationException();
+			_appContextInit = appContextInit;
+			_threadInit = _GetApplicationContextThread;
+		}
+
+		public void Start()
+		{
+			_thread = _threadInit();
+			if (_thread != null)
+			{
+				_thread.Start();
+			}
+		}
+
+		public void Stop()
+		{
+			if (_thread != null)
+			{
+				_thread.Stop();
+			}
+		}
+
+		public bool IsRunning
+		{
+			get { return _thread != null && _thread.ThreadState == ThreadState.Running; }
+		}
+
+		public void BeginInvoke(Action methodToInvoke)
+		{
+			_thread.BeginInvoke(methodToInvoke);
+		}
+
+		private WPFUIThread _GetFormThread()
+		{
+			return new WPFUIThread(_windowInit);
+		}
+
+		private WPFUIThread _GetApplicationContextThread()
+		{
+			return new WPFUIThread(_appContextInit);
+		}		
+	}	
+}
