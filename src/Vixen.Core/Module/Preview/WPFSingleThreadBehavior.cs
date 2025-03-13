2144fa8448fd17245dbeeb1c5e446a8162dff631
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-03-12 20:32:02 -0500

    VIX-3663 Adding WPF Preview
    Adding single thread support for completeness.

---
+++ src/Vixen.Core/Module/Preview/WPFSingleThreadBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+
+namespace Vixen.Module.Preview
+{
+	public class WPFSingleThreadBehavior : IThreadBehavior
+	{
+		private Func<Window> _windowInit;
+		private Window _window;
+
+		public WPFSingleThreadBehavior(Func<Window> formInit)
+		{
+			if (formInit == null) throw new InvalidOperationException();
+			_windowInit = formInit;
+		}
+
+		public void Start()
+		{
+			_window = _windowInit();
+			_window.Show();
+		}
+
+		public void Stop()
+		{
+			_window.Close();
+			((IDisposable)_window).Dispose();
+			_window = null;
+		}
+
+		public bool IsRunning
+		{
+			get { return _window != null && _window.IsVisible; }
+		}
+
+		public void BeginInvoke(Action methodToInvoke)
+		{
+			_window.Dispatcher.BeginInvoke(methodToInvoke);
+		}
+	}	
+}
