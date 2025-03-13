fcf8aecefc96b45859dfdaf580da25f8be38c531
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-03-12 20:32:02 -0500

    VIX-3663 Adding WPF Preview
    Adding single thread support for completeness.

--- src/Vixen.Core/Module/Preview/WPFMultiThreadBehavior.cs
+++ src/Vixen.Core/Module/Preview/WPFMultiThreadBehavior.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+
 using Vixen.Sys;
-using System.Windows;
-using System.IO;
 
 namespace Vixen.Module.Preview
 {
-	
+
 	public class WPFMultiThreadBehavior : IThreadBehavior
 	{
 		private Func<WPFUIThread> _threadInit;
