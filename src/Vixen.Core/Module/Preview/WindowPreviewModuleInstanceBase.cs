b331316182723b9174999b73fcb3adf7f8729d01
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-03-29 16:01:00 -0500

    VIX-3663 See previous commit.
    VIX-3663 See previous commit.

--- src/Vixen.Core/Module/Preview/WindowPreviewModuleInstanceBase.cs
+++ src/Vixen.Core/Module/Preview/WindowPreviewModuleInstanceBase.cs
@@ -11,25 +11,55 @@ namespace Vixen.Module.Preview
 {
 	public abstract class WindowPreviewModuleInstanceBase : PreviewModuleInstanceBase
 	{
+		protected bool UseWPF { get; set; }
+
 		private IThreadBehavior _threadBehavior;
 
 		protected override IThreadBehavior ThreadBehavior
 		{
 			get
 			{
-				if (_threadBehavior == null)
+				if (UseWPF)
 				{
-					// Needs to be cast to avoid ambiguity since there are no parameters
-					// to differentiate the signature.
-					Func<Window> initMethod = Initialize;
-					_threadBehavior = VixenSystem.SystemConfig.IsPreviewThreaded
-										? new WPFMultiThreadBehavior(initMethod)
-										: (IThreadBehavior)new WPFSingleThreadBehavior(initMethod);
+					if (_threadBehavior == null)
+					{
+#if OPENGL_PREVIEW_WIN_FORMS
+						// Needs to be cast to avoid ambiguity since there are no parameters
+						// to differentiate the signature.
+						Func<Window> initMethod = InitializeWindow;
+						_threadBehavior = VixenSystem.SystemConfig.IsPreviewThreaded
+											? new WPFMultiThreadBehavior(initMethod)
+											: (IThreadBehavior)new WPFSingleThreadBehavior(initMethod);
+#else
+						// Needs to be cast to avoid ambiguity since there are no parameters
+						// to differentiate the signature.
+						Func<Form> initMethod = InitializeForm;
+						_threadBehavior = VixenSystem.SystemConfig.IsPreviewThreaded
+											  ? new MultiThreadBehavior(initMethod)
+											  : (IThreadBehavior)new SingleThreadBehavior(initMethod);
+#endif
+					}
+					return _threadBehavior;
 				}
+				else
+				{
+					if (_threadBehavior == null)
+					{
+						// Needs to be cast to avoid ambiguity since there are no parameters
+						// to differentiate the signature.
+						Func<Form> initMethod = InitializeForm;
+						_threadBehavior = VixenSystem.SystemConfig.IsPreviewThreaded
+											  ? new MultiThreadBehavior(initMethod)
+											  : (IThreadBehavior)new SingleThreadBehavior(initMethod);
+					}
+				}
+
 				return _threadBehavior;
 			}
 		}
 
-		protected abstract Window Initialize();
+		protected abstract Window InitializeWindow();
+
+		protected abstract Form InitializeForm();				
 	}
 }
