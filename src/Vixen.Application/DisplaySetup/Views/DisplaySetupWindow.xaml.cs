c09ea9f8cfadfe0ce8679a88e8f5baf7018cdb3b
Jeff Uchitjil <git@k9kld.org>
2025-05-26 16:18:44 -0500

    Enable WPF in Vixen Application for new Display Setup

---
+++ src/Vixen.Application/DisplaySetup/Views/DisplaySetupWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WPFCommon.Extensions;
+
+namespace VixenApplication.DisplaySetup.Views
+{
+	public partial class DisplaySetupWindow: Window
+	{
+		public DisplaySetupWindow()
+		{
+			InitializeComponent();
+            Icon = Common.Resources.Properties.Resources.Icon_Vixen3.ToImageSource();
+            //_viewModel = new PropEditorViewModel();
+           // DataContext = _viewModel;
+		}
+	}
+}
