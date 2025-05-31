fc589b8bd15ba4f31b38e0bbf4a148215e8e4fe7
Jeff Uchitjil <git@k9kld.org>
2025-05-31 12:34:27 -0500

    VIX-3694 Add logic to display Prop Preview
    * Basic POC logic to display the simple rendering of a Prop in a window on the setup screen.
    * Added logic to render the Arch model.
    * This is preliminary POC and could be the basis for all light type props. Need to look into renderign for things like Intelligent Fixtures.

---
+++ src/Vixen.Core/Sys/Props/IPropModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Vixen.Sys.Props
+{
+    /// <summary>
+    /// Base interface for Prop model implementations
+    /// </summary>
+    public interface IPropModel
+    {
+        Guid Id { get; init; }
+    }
+}
