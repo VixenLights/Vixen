44ffd888f0169839ed7fef58a113ebea12e56770
Jeff Uchitjil <git@k9kld.org>
2025-05-31 12:34:27 -0500

    VIX-3694 Add logic to display Prop Preview
    * Basic POC logic to display the simple rendering of a Prop in a window on the setup screen.
    * Added logic to render the Arch model.
    * This is preliminary POC and could be the basis for all light type props. Need to look into renderign for things like Intelligent Fixtures.

---
+++ src/Vixen.Application/SetupDisplay/ViewModels/PropPreviewViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using Catel.Data;
+using Catel.MVVM;
+using NLog;
+using Vixen.Sys.Props;
+using Vixen.Sys.Props.Model;
+
+namespace VixenApplication.SetupDisplay.ViewModels
+{
+	public class PropPreviewViewModel : ViewModelBase
+	{
+        private static readonly Logger Logging = LogManager.GetCurrentClassLogger();
+
+		public PropPreviewViewModel(/* dependency injection here */)
+		{
+		}
+
+		public override string Title { get { return "Prop Preview"; } }
+
+		// TODO: Register models with the vmpropmodel codesnippet
+		// TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
+		// TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+		protected override async Task InitializeAsync()
+		{
+			await base.InitializeAsync();
+
+			// TODO: subscribe to events here
+		}
+
+		protected override async Task CloseAsync()
+		{
+			// TODO: unsubscribe from events here
+
+			await base.CloseAsync();
+		}
+
+        
+
+        /// <summary>
+        /// Gets or sets the PropModel value.
+        /// </summary>;
+        [Model]
+        public ILightPropModel PropModel
+        {
+            get { return GetValue<ILightPropModel>(PropModelProperty); }
+            private set { SetValue(PropModelProperty, value); }
+        }
+
+        /// <summary>;
+        /// PropModel property data.
+        /// </summary>;
+        public static readonly IPropertyData PropModelProperty = RegisterProperty<ILightPropModel>(nameof(PropModel));
+
+        #region NodePoints property
+
+        /// <summary>
+        /// Gets or sets the NodePoints value.
+        /// </summary>
+        [ViewModelToModel("PropModel")]
+        public ObservableCollection<NodePoint> NodePoints
+        {
+            get { return GetValue<ObservableCollection<NodePoint>>(NodePointsProperty); }
+            set { SetValue(NodePointsProperty, value); }
+        }
+
+        /// <summary>
+        /// NodePoints property data.
+        /// </summary>
+        public static readonly IPropertyData NodePointsProperty = RegisterProperty<ObservableCollection<NodePoint>>(nameof(NodePoints));
+
+        #endregion
+	}
+}
