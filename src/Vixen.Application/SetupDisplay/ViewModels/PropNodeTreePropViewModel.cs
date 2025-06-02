e78f43ea81caa34b0a2db5ca42e42527d5f5d526
Jeff Uchitjil <git@k9kld.org>
2025-06-02 15:12:46 -0500

    VIX-3694 Refactored Prop model preview logic
    * Created a seperate view model for the Prop tab to handle the selections without cross contamination.
    * Moved the model preview binding to the main setup model.
    * Added logic to listen to the selections in the two tabs and bind the correct model data. Need to experiment with doing this with messages instead.
    * Created a Tree model for displaying the Tree Prop.

---
+++ src/Vixen.Application/SetupDisplay/ViewModels/PropNodeTreePropViewModel.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.ObjectModel;
+using Catel.Data;
+using Catel.MVVM;
+using Vixen.Sys;
+using Vixen.Sys.Managers;
+
+namespace VixenApplication.SetupDisplay.ViewModels
+{
+    public class PropNodeTreePropViewModel : ViewModelBase
+    {
+        public PropNodeTreePropViewModel()
+        {
+            PropManager = VixenSystem.Props;
+			PropNodeViewModel vm = new(PropManager.RootNode, null);
+            RootNodeViewModel = [vm];
+			SelectedItems = new();
+            SelectedItems.CollectionChanged += SelectedItemsCollectionChanged;
+		}
+
+        #region PropManager model property
+
+        /// <summary>
+        /// Gets or sets the PropManager value.
+        /// </summary>
+        public PropManager PropManager
+        {
+            get { return GetValue<PropManager>(PropManagerProperty); }
+            private set { SetValue(PropManagerProperty, value); }
+        }
+
+        /// <summary>
+        /// Prop property data.
+        /// </summary>
+        public static readonly IPropertyData PropManagerProperty = RegisterProperty<PropManager>(nameof(PropManager));
+
+        #endregion
+
+        #region RootNodeViewModels property
+
+        /// <summary>
+        /// Gets or sets the RootNodesViewModels value.
+        /// </summary>
+        public ObservableCollection<PropNodeViewModel> RootNodeViewModel
+        {
+            get { return GetValue<ObservableCollection<PropNodeViewModel>>(RootNodeViewModelProperty); }
+            set { SetValue(RootNodeViewModelProperty, value); }
+        }
+
+        /// <summary>
+        /// RootNodesViewModels property data.
+        /// </summary>
+        public static readonly IPropertyData RootNodeViewModelProperty = RegisterProperty<ObservableCollection<PropNodeViewModel>>(nameof(RootNodeViewModel));
+
+        #endregion
+
+        #region LeafNodes property
+
+        /// <summary>
+        /// Gets or sets the LeafNodes value.
+        /// </summary>
+        public ObservableCollection<PropNodeViewModel> LeafNodes
+        {
+            get
+            {
+                return new(RootNodeViewModel.SelectMany(x => x.GetLeafEnumerator()));
+            }
+        }
+
+        #endregion
+
+		#region SelectedItem property
+
+		/// <summary>
+		/// Gets or sets the SelectedItem value.
+		/// </summary>
+		public PropNodeViewModel? SelectedItem
+        {
+            get { return GetValue<PropNodeViewModel>(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        /// <summary>
+        /// SelectedItem property data.
+        /// </summary>
+        public static readonly IPropertyData SelectedItemProperty = RegisterProperty<PropNodeViewModel>(nameof(SelectedItem));
+
+		#endregion
+
+        #region SelectedItems property
+
+        /// <summary>
+        /// Gets or sets the SelectedItems value.
+        /// </summary>
+        public ObservableCollection<PropNodeViewModel> SelectedItems
+        {
+            get { return GetValue<ObservableCollection<PropNodeViewModel>>(SelectedItemsProperty); }
+            set { SetValue(SelectedItemsProperty, value); }
+        }
+
+        /// <summary>
+        /// SelectedItems property data.
+        /// </summary>
+        public static readonly IPropertyData SelectedItemsProperty =
+            RegisterProperty<ObservableCollection<PropNodeViewModel>>(nameof(SelectedItems));
+
+
+        private void SelectedItemsCollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+			if (SelectedItems.Count == 1)
+            {
+                SelectedItem = SelectedItems.First();
+            }
+            else
+            {
+                SelectedItem = null;
+            }
+		}
+
+        #endregion
+
+		#region Selection
+
+		public void DeselectAll()
+        {
+            SelectedItems.ToList().ForEach(x => x.IsSelected = false);
+            SelectedItems.Clear();
+        }
+
+        public void SelectModels(IEnumerable<PropNodeViewModel> propNodeModels)
+        {
+            foreach (var elementModelViewModel in propNodeModels)
+            {
+                elementModelViewModel.IsSelected = true;
+                if (elementModelViewModel.ParentViewModel is PropNodeViewModel parent)
+                {
+                    parent.IsExpanded = true;
+                }
+            }
+        }
+
+        public void DeselectModels(IEnumerable<PropNodeViewModel> propNodeModels)
+        {
+            foreach (var elementModelViewModel in propNodeModels)
+            {
+                elementModelViewModel.IsSelected = false;
+            }
+        }
+
+        #endregion
+	}
+}
---
+++ src/Vixen.Core/Sys/Props/Model/Tree/Tree.cs
@@ -0,0 +1,60 @@
+﻿#nullable enable
+
+namespace Vixen.Sys.Props.Model.Tree
+{
+    public class Tree: Prop
+	{
+        private int _strings;
+        private int _nodesPerString;
+
+        public Tree() : this("Tree 1", 16, 50)
+        {
+
+        }
+
+        public Tree(string name, int strings, int nodesPerString) : this(name, strings, nodesPerString, StringTypes.Pixel)
+        {
+
+        }
+
+        public Tree(string name, int strings = 16, int nodesPerString = 50, StringTypes stringType = StringTypes.Pixel) : base(name, PropType.Arch)
+        {
+            Name = name;
+            _strings = strings;
+            _nodesPerString = nodesPerString;
+            StringType = stringType;
+            //TODO create default element structure
+            //TODO create Preview model
+            TreeModel model = new TreeModel(Strings, nodesPerString);
+            PropModel = model;
+        }
+
+        /// <summary>
+        /// The number of light strings
+        /// </summary>
+        public int Strings
+        {
+            get => _strings;
+            set => SetProperty(ref _strings, value);
+        }
+
+        /// <summary>
+        /// The number of light nodes per string
+        /// </summary>
+        public int NodesPerString
+        {
+            get => _nodesPerString;
+            set => SetProperty(ref _nodesPerString, value);
+        }
+
+        public int CoverageDegrees { get; set; }
+
+        public int DegreeOffset { get; set; }
+
+        public int BaseHeight { get; set; }
+
+        public int TopHeight { get; set; }
+
+        public int TopWidth { get; set; }
+    }
+}
