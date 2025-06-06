69b6c09956d2be0dbd45e017fe63441520bbf807
Jeff Uchitjil <git@k9kld.org>
2025-06-06 14:40:54 -0500

    VIX-3694 Add model field editing
    * Add the property grid control to allow for editing of fields on the props.
    * TODO determine why the property order does not seem to work.
    * Change the order of the panels to put the preview window to the side.

--- src/Vixen.Core/Sys/Props/Model/Arch/Arch.cs
+++ src/Vixen.Core/Sys/Props/Model/Arch/Arch.cs
@@ -1,42 +1,77 @@
 ﻿
 #nullable enable
+using System.ComponentModel;
+using Vixen.Attributes;
+
 namespace Vixen.Sys.Props.Model.Arch
 {
     /// <summary>
     /// A class that defines an Arch Prop
     /// </summary>
-    public class Arch: Prop
+    public class Arch: BaseProp, IProp
     {
-        private int _lightCount;
-
+        private ArchModel _propModel;
+        
         public Arch():this("Arch 1", 25)
         {
             
         }
 
-        public Arch(string name, int lightCount): this(name, lightCount, StringTypes.Pixel)
+        public Arch(string name, int nodeCount): this(name, nodeCount, StringTypes.Pixel)
         {
             
         }
 
-        public Arch(string name, int lightCount = 25, StringTypes stringType = StringTypes.Pixel) : base(name, PropType.Arch)
+        public Arch(string name, int nodeCount = 25, StringTypes stringType = StringTypes.Pixel) : base(name, PropType.Arch)
         {
-            LightCount = lightCount;
-            
             //TODO create default element structure
-
-            ArchModel model = new ArchModel(lightCount);
-            PropModel = model;
+            StringType = stringType;
+            ArchModel model = new ArchModel(nodeCount);
+            _propModel = model;
         }
 
-		public int LightCount
+        [Browsable(false)]
+		IPropModel IProp.PropModel => PropModel;
+
+		[Browsable(false)]
+        public new ArchModel PropModel
         {
-            get => _lightCount;
+            get => _propModel;
+            protected set => SetProperty(ref _propModel, value);
+        }
+
+        [DisplayName("Nodes Count")]
+        [PropertyOrder(0)]
+		public int NodeCount
+        {
+            get => _propModel.NodeCount;
             set
             {
-                SetProperty(ref _lightCount, value);
+                if (value == _propModel.NodeCount)
+                {
+                    return;
+                }
 
-			}
+                _propModel.NodeCount = value;
+                OnPropertyChanged(nameof(NodeCount));
+            }
         }
-    }
+
+        [DisplayName("Nodes Size")]
+        [PropertyOrder(1)]
+        public int NodeSize
+        {
+            get => _propModel.NodeSize;
+            set
+            {
+                if (value == _propModel.NodeSize)
+                {
+                    return;
+                }
+
+                _propModel.NodeSize = value;
+                OnPropertyChanged(nameof(NodeSize));
+            }
+        }
+	}
 }
