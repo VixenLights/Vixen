4be53ecec4ad8a35912501138b4c670b22eb67fb
Jeff Uchitjil <git@k9kld.org>
2025-06-09 17:06:36 -0500

    VIX-3694 Fix field population in the Arch wizard

--- src/Vixen.Application/SetupDisplay/Wizards/Wizard/ArchPropWizard.cs
+++ src/Vixen.Application/SetupDisplay/Wizards/Wizard/ArchPropWizard.cs
@@ -1,15 +1,51 @@
-﻿using Catel.IoC;
+﻿using Catel.Data;
+using Catel.IoC;
+using Catel.Services;
 using Orc.Wizard;
 using VixenApplication.SetupDisplay.Wizards.Pages;
 
 namespace VixenApplication.SetupDisplay.Wizards.Wizard
 {
-	public class ArchPropWizard: SideNavigationWizardBase
+	public class ArchPropWizard : SideNavigationWizardBase
 	{
-        public ArchPropWizard(ITypeFactory typeFactory) : base(typeFactory)
-        {
-            Title = "Arch Prop";
-            this.AddPage<ArchPropWizardPage>();
-        }
-    }
+		private readonly IMessageService _messageService;
+
+		public ArchPropWizard(ITypeFactory typeFactory, IMessageService messageService) : base(typeFactory)
+		{
+			ArgumentNullException.ThrowIfNull(messageService);
+
+			_messageService = messageService;
+
+			Title = "Arch Prop";
+			ShowInTaskbar = true;
+			this.AddPage<ArchPropWizardPage>();
+
+			ResizeMode = System.Windows.ResizeMode.CanResize;
+
+
+		}
+
+		public override Task ShowHelpAsync()
+		{
+			return _messageService.ShowAsync("HELP HANDLER");
+		}
+
+		#region HideNavigationSystem property
+
+		/// <summary>
+		/// Gets or sets the HideNavigationSystem value.
+		/// </summary>
+		public bool HideNavigationSystem
+		{
+			get { return GetValue<bool>(HideNavigationSystemProperty); }
+			set { SetValue(HideNavigationSystemProperty, value); }
+		}
+
+		/// <summary>
+		/// HideNavigationSystem property data.
+		/// </summary>
+		public static readonly IPropertyData HideNavigationSystemProperty = RegisterProperty<bool>(nameof(HideNavigationSystem));
+
+		#endregion
+	}
 }
