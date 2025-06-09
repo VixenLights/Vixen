7383aba8cc9e142f68501913b1dab924e70e18db
Jeff Uchitjil <git@k9kld.org>
2025-06-09 17:06:36 -0500

    VIX-3694 Fix field population in the Arch wizard

--- src/Vixen.Application/SetupDisplay/Wizards/Pages/ArchPropWizardPage.cs
+++ src/Vixen.Application/SetupDisplay/Wizards/Pages/ArchPropWizardPage.cs
@@ -1,41 +1,92 @@
-﻿using Orc.Wizard;
+﻿using Catel.Data;
+using Orc.Wizard;
 using Vixen.Sys;
 using Vixen.Sys.Props;
 using VixenModules.App.Props.Models.Arch;
 
 namespace VixenApplication.SetupDisplay.Wizards.Pages
 {
-	public class ArchPropWizardPage: WizardPageBase, IPropWizardFinalPage
+	public class ArchPropWizardPage : WizardPageBase, IPropWizardFinalPage
 	{
-        public ArchPropWizardPage()
-        {
-            Title = "Arch";
-            Description = $"Enter the details for your {Title}";
-            Name = "Arch 1";
-        }
+		public ArchPropWizardPage()
+		{
+			Title = "Arch";
+			Description = $"Enter the details for your {Title}";
+			Name = "Arch 1";
+			NodeCount = 25;
+			StringType = StringTypes.Pixel;
+		}
 
-        public string Name { get; set; }
+		#region Name property
 
-        public int LightCount { get; set; } = 25;
+		/// <summary>
+		/// Gets or sets the Name value.
+		/// </summary>
+		public string Name
+		{
+			get { return GetValue<string>(NameProperty); }
+			set { SetValue(NameProperty, value); }
+		}
 
-        public StringTypes StringType { get; set; } = StringTypes.Pixel;
+		/// <summary>
+		/// Name property data.
+		/// </summary>
+		public static readonly IPropertyData NameProperty = RegisterProperty<string>(nameof(Name));
 
-        public override ISummaryItem GetSummary()
-        {
-            return new SummaryItem
-            {
-                Title = "Arch",
-                Summary = string.Format("A new {0} with name {1} and {2} {3} lights", Title, Name, LightCount,
-                    StringType.ToString())
-            };
-        }
+		#endregion
 
-        public IProp GetProp()
-        {
-            var arch = VixenSystem.Props.CreateProp<Arch>(Name);
-            arch.NodeCount = LightCount;
-            arch.StringType = StringType;
-            return arch;
-        }
-    }
+		#region NodeCount property
+
+		/// <summary>
+		/// Gets or sets the NodeCount value.
+		/// </summary>
+		public int NodeCount
+		{
+			get { return GetValue<int>(NodeCountProperty); }
+			set { SetValue(NodeCountProperty, value); }
+		}
+
+		/// <summary>
+		/// NodeCount property data.
+		/// </summary>
+		public static readonly IPropertyData NodeCountProperty = RegisterProperty<int>(nameof(NodeCount));
+
+		#endregion
+
+		#region StringType property
+
+		/// <summary>
+		/// Gets or sets the StringType value.
+		/// </summary>
+		public StringTypes StringType
+		{
+			get { return GetValue<StringTypes>(StringTypeProperty); }
+			set { SetValue(StringTypeProperty, value); }
+		}
+
+		/// <summary>
+		/// StringType property data.
+		/// </summary>
+		public static readonly IPropertyData StringTypeProperty = RegisterProperty<StringTypes>(nameof(StringType));
+
+		#endregion
+
+		public override ISummaryItem GetSummary()
+		{
+			return new SummaryItem
+			{
+				Title = "Arch",
+				Summary = string.Format("A new {0} with name {1} and {2} {3} nodes", Title, Name, NodeCount,
+					StringType.ToString())
+			};
+		}
+
+		public IProp GetProp()
+		{
+			var arch = VixenSystem.Props.CreateProp<Arch>(Name);
+			arch.NodeCount = NodeCount;
+			arch.StringType = StringType;
+			return arch;
+		}
+	}
 }
