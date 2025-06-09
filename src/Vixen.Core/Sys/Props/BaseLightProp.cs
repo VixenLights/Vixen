dd5bd545ad2d0897ee212dffe52cb8ae545ea39a
Jeff Uchitjil <git@k9kld.org>
2025-06-09 09:54:06 -0500

    VIX-3694 Refactor to provide BaseLightProp
    * Create BaseLightProp to refine some functions that will be specific to a Lighting style prop. (Pixel / Standard)
    * Refactor the AddStrings method and StringType Property into the new BaseLightPRop class.
    * Can refince this further as we work on things like Fixtures and other non lighting type Props.

---
+++ src/Vixen.Core/Sys/Props/BaseLightProp.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using System.ComponentModel;
+using Vixen.Attributes;
+using Vixen.Services;
+
+namespace Vixen.Sys.Props
+{
+    public abstract class BaseLightProp : BaseProp
+    {
+        private StringTypes _stringType;
+
+        protected BaseLightProp(string name, PropType propType): base(name, propType)
+        {
+			StringType = StringTypes.Pixel;
+		}
+
+		[PropertyOrder(1)]
+        [DisplayName("String Type")]
+        public StringTypes StringType
+        {
+            get => _stringType;
+            set => SetProperty(ref _stringType, value);
+        }
+
+        protected void AddStringElements(ElementNode node, int count, int nodesPerString, int namingIndex = 0)
+        {
+            for (int i = namingIndex; i < count + namingIndex; i++)
+            {
+                string stringName = $"{AutoPropStringName} {i + 1}";
+                ElementNode stringNode = ElementNodeService.Instance.CreateSingle(node, stringName, true, false);
+
+                if (StringType == StringTypes.Pixel)
+                {
+                    AddNodeElements(stringNode, nodesPerString);
+                }
+            }
+        }
+	}
+
+}
