4113a04f6adcf79f50b657471fd6fc4b4c5afe27
Jeff Uchitjil <git@k9kld.org>
2025-06-10 10:51:50 -0500

    VIX-3694  Make the element tree refresh automatically

--- src/Vixen.Application/SetupDisplay/Views/SetupDisplayWindow.xaml.cs
+++ src/Vixen.Application/SetupDisplay/Views/SetupDisplayWindow.xaml.cs
@@ -1,18 +1,39 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using Orc.Theming;
+using Vixen.Sys.Managers;
 using VixenApplication.SetupDisplay.ViewModels;
 using WPFCommon.Extensions;
+using Timer = System.Threading.Timer;
 
 namespace VixenApplication.SetupDisplay.Views
 {
 	public partial class SetupDisplayWindow: Window
 	{
+		private readonly Timer _nodeRefreshTimer;
         public SetupDisplayWindow()
 		{
             ThemeManager.Current.SynchronizeTheme();
 			InitializeComponent();
             Icon = Common.Resources.Properties.Resources.Icon_Vixen3.ToImageSource();
             DataContext = new SetupDisplayViewModel();
+            _nodeRefreshTimer = new Timer(Timer_Elapsed, null, 2000, Timeout.Infinite);
+            NodeManager.NodesChanged += NodeManager_NodesChanged;
+
         }
+
+        private void Timer_Elapsed(object? state)
+        {
+	        if (ElementTree.InvokeRequired)
+	        {
+		        ElementTree.Invoke(ElementTree.PopulateNodeTree);
+	        }
+			Debug.WriteLine("Refreshed");
+		}
+
+		private void NodeManager_NodesChanged(object? sender, EventArgs e)
+		{
+			_nodeRefreshTimer.Change(2000, Timeout.Infinite);
+		}
 	}
 }
