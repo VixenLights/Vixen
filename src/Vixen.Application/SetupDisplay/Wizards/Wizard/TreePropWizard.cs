140a056d2ac1865e46358434ae2c5a5dc70c7dd1
Jeff Uchitjil <git@k9kld.org>
2025-06-10 10:15:39 -0500

    VIX-3694 Add logic to add a Tree
    * Create a basic Tree Wizard. Needs many more options, but enough to make it work.
    * Add context menu to add either a Arch or a Tree. This should be dynamically generated at some point.

---
+++ src/Vixen.Application/SetupDisplay/Wizards/Wizard/TreePropWizard.cs
@@ -0,0 +1,66 @@
+﻿using Catel.Data;
+using Catel.IoC;
+using Catel.Services;
+using Orc.Wizard;
+using System.Diagnostics;
+using VixenApplication.SetupDisplay.Wizards.Pages;
+
+namespace VixenApplication.SetupDisplay.Wizards.Wizard
+{
+	public class TreePropWizard : SideNavigationWizardBase
+	{
+		private readonly IMessageService _messageService;
+
+		private const string HelpUrl =
+			"https://www.vixenlights.com/docs/usage/preview/smart-objects/preview-mega-tree/";
+
+		public TreePropWizard(ITypeFactory typeFactory, IMessageService messageService) : base(typeFactory)
+		{
+			ArgumentNullException.ThrowIfNull(messageService);
+
+			_messageService = messageService;
+
+			Title = "Tree Prop";
+			ShowInTaskbar = true;
+			this.AddPage<TreePropWizardPage>();
+
+			ResizeMode = System.Windows.ResizeMode.CanResize;
+
+
+		}
+
+		#region IWizard
+
+		/// <summary>
+		/// Catel Wizard override for showing Tree specific help.
+		/// </summary>
+		/// <returns>Task for showing the online help</returns>
+		public override Task ShowHelpAsync()
+		{
+			// Launch a browser with the help URL for the current page
+			return Task.FromResult(Process.Start(new ProcessStartInfo(HelpUrl) { UseShellExecute = true }));
+		}
+
+		#region HideNavigationSystem property
+
+		/// <summary>
+		/// Gets or sets the HideNavigationSystem value.
+		/// </summary>
+		public bool HideNavigationSystem
+		{
+			get { return GetValue<bool>(HideNavigationSystemProperty); }
+			set { SetValue(HideNavigationSystemProperty, value); }
+		}
+
+		/// <summary>
+		/// HideNavigationSystem property data.
+		/// </summary>
+		public static readonly IPropertyData HideNavigationSystemProperty = RegisterProperty<bool>(nameof(HideNavigationSystem));
+
+		#endregion
+
+		#endregion
+
+
+	}
+}
