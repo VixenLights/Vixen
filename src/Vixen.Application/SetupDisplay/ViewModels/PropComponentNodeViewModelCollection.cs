7a061113658418163487ad0a9f497f4dda6c48e0
Jeff Uchitjil <git@k9kld.org>
2025-07-02 14:36:49 -0500

    VIX-3694 Add view to visualize the Prop Component Tree
    * Add logic in the Tree to generate a simple Prop Component stucture for demonstration.

---
+++ src/Vixen.Application/SetupDisplay/ViewModels/PropComponentNodeViewModelCollection.cs
@@ -0,0 +1,16 @@
+﻿using Common.WPFCommon.Utils;
+using Vixen.Sys.Props.Components;
+
+namespace VixenApplication.SetupDisplay.ViewModels
+{
+	public class PropComponentNodeViewModelCollection : TransformedCollection<PropComponentNode, PropComponentNodeViewModel>
+	{
+		public PropComponentNodeViewModelCollection(IEnumerable<PropComponentNode> propComponentNodeModels, PropComponentNodeViewModel parent) : base(
+			propComponentNodeModels,
+			elementModel => new PropComponentNodeViewModel(elementModel, parent),
+			propNodeViewModel => propNodeViewModel.Dispose())
+		{
+		}
+
+	}
+}
