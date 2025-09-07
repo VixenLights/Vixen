6eaccd979b6cdefb11cb78f15478dfad5cecc8fd
johncbaur <46469358+johncbaur@users.noreply.github.com>
2025-09-07 18:11:51 -0500

    Adding Intelligent Fixture support.

---
+++ src/Vixen.Modules/App/Props/Models/IntelligentFixture/YesNo.cs
@@ -0,0 +1,11 @@
+﻿namespace VixenModules.App.Props.Models.IntelligentFixture
+{
+	/// <summary>
+	/// Enumeration to use in the property grid.
+	/// </summary>
+	public enum YesNoType
+	{
+		No,
+		Yes,
+	}
+}
