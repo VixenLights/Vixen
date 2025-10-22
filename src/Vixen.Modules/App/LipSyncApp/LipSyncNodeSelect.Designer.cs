1ef1f3af0307b659c34ba1d29ef7ab523f91d334
Jeff Uchitjil <git@k9kld.org>
2025-10-22 14:52:33 -0500

    VIX-3743 Fix usability issues in the LipSync Node selection dialog
    * Make the dialog resizable so that the user can make it bigger if needed.
    * Fix the layout so that it fills the space as it is resized so the boxes are usable.
    * Fix the dialog title to have a real user friendly title.

--- src/Vixen.Modules/App/LipSyncApp/LipSyncNodeSelect.Designer.cs
+++ src/Vixen.Modules/App/LipSyncApp/LipSyncNodeSelect.Designer.cs
@@ -20,250 +20,253 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LipSyncNodeSelect));
-			this.buttonOk = new System.Windows.Forms.Button();
-			this.buttonCancel = new System.Windows.Forms.Button();
-			this.chosenTargets = new System.Windows.Forms.ListBox();
-			this.buttonAdd = new System.Windows.Forms.Button();
-			this.buttonRemove = new System.Windows.Forms.Button();
-			this.buttonReset = new System.Windows.Forms.Button();
-			this.recurseCB = new System.Windows.Forms.CheckBox();
-			this.allowGroupsCheckbox = new System.Windows.Forms.CheckBox();
-			this.stringsGroupBox = new System.Windows.Forms.GroupBox();
-			this.colsRadioButton = new System.Windows.Forms.RadioButton();
-			this.rowsRadioButton = new System.Windows.Forms.RadioButton();
-			this.nodeTreeView = new Common.Controls.MultiSelectTreeview();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.stringsGroupBox.SuspendLayout();
-			this.panel1.SuspendLayout();
-			this.flowLayoutPanel1.SuspendLayout();
-			this.tableLayoutPanel1.SuspendLayout();
-			this.SuspendLayout();
+			buttonOk = new Button();
+			buttonCancel = new Button();
+			chosenTargets = new ListBox();
+			buttonAdd = new Button();
+			buttonRemove = new Button();
+			buttonReset = new Button();
+			recurseCB = new CheckBox();
+			allowGroupsCheckbox = new CheckBox();
+			stringsGroupBox = new GroupBox();
+			colsRadioButton = new RadioButton();
+			rowsRadioButton = new RadioButton();
+			nodeTreeView = new Common.Controls.MultiSelectTreeview();
+			panel1 = new Panel();
+			flowLayoutPanel1 = new FlowLayoutPanel();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			stringsGroupBox.SuspendLayout();
+			panel1.SuspendLayout();
+			flowLayoutPanel1.SuspendLayout();
+			tableLayoutPanel1.SuspendLayout();
+			SuspendLayout();
 			// 
 			// buttonOk
 			// 
-			this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonOk.Location = new System.Drawing.Point(419, 3);
-			this.buttonOk.Name = "buttonOk";
-			this.buttonOk.Size = new System.Drawing.Size(87, 27);
-			this.buttonOk.TabIndex = 0;
-			this.buttonOk.Text = "OK";
-			this.buttonOk.UseVisualStyleBackColor = true;
+			buttonOk.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			buttonOk.DialogResult = DialogResult.OK;
+			buttonOk.Location = new Point(483, 6);
+			buttonOk.Name = "buttonOk";
+			buttonOk.Size = new Size(87, 27);
+			buttonOk.TabIndex = 0;
+			buttonOk.Text = "OK";
+			buttonOk.UseVisualStyleBackColor = true;
 			// 
 			// buttonCancel
 			// 
-			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(512, 3);
-			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(87, 27);
-			this.buttonCancel.TabIndex = 1;
-			this.buttonCancel.Text = "Cancel";
-			this.buttonCancel.UseVisualStyleBackColor = true;
+			buttonCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+			buttonCancel.DialogResult = DialogResult.Cancel;
+			buttonCancel.Location = new Point(576, 6);
+			buttonCancel.Name = "buttonCancel";
+			buttonCancel.Size = new Size(87, 27);
+			buttonCancel.TabIndex = 1;
+			buttonCancel.Text = "Cancel";
+			buttonCancel.UseVisualStyleBackColor = true;
 			// 
 			// chosenTargets
 			// 
-			this.chosenTargets.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.chosenTargets.FormattingEnabled = true;
-			this.chosenTargets.ItemHeight = 15;
-			this.chosenTargets.Location = new System.Drawing.Point(369, 3);
-			this.chosenTargets.Name = "chosenTargets";
-			this.chosenTargets.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-			this.chosenTargets.Size = new System.Drawing.Size(220, 316);
-			this.chosenTargets.TabIndex = 3;
+			chosenTargets.Dock = DockStyle.Fill;
+			chosenTargets.FormattingEnabled = true;
+			chosenTargets.ItemHeight = 15;
+			chosenTargets.Location = new Point(411, 3);
+			chosenTargets.Name = "chosenTargets";
+			chosenTargets.SelectionMode = SelectionMode.MultiExtended;
+			chosenTargets.Size = new Size(261, 389);
+			chosenTargets.TabIndex = 3;
 			// 
 			// buttonAdd
 			// 
-			this.buttonAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.buttonAdd.Location = new System.Drawing.Point(30, 3);
-			this.buttonAdd.Name = "buttonAdd";
-			this.buttonAdd.Size = new System.Drawing.Size(78, 27);
-			this.buttonAdd.TabIndex = 4;
-			this.buttonAdd.Text = "->";
-			this.buttonAdd.UseVisualStyleBackColor = true;
-			this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+			buttonAdd.Anchor = AnchorStyles.None;
+			buttonAdd.Location = new Point(30, 3);
+			buttonAdd.Name = "buttonAdd";
+			buttonAdd.Size = new Size(78, 27);
+			buttonAdd.TabIndex = 4;
+			buttonAdd.Text = "->";
+			buttonAdd.UseVisualStyleBackColor = true;
+			buttonAdd.Click += buttonAdd_Click;
 			// 
 			// buttonRemove
 			// 
-			this.buttonRemove.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.buttonRemove.Location = new System.Drawing.Point(30, 36);
-			this.buttonRemove.Name = "buttonRemove";
-			this.buttonRemove.Size = new System.Drawing.Size(78, 27);
-			this.buttonRemove.TabIndex = 5;
-			this.buttonRemove.Text = "<-";
-			this.buttonRemove.UseVisualStyleBackColor = true;
-			this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+			buttonRemove.Anchor = AnchorStyles.None;
+			buttonRemove.Location = new Point(30, 36);
+			buttonRemove.Name = "buttonRemove";
+			buttonRemove.Size = new Size(78, 27);
+			buttonRemove.TabIndex = 5;
+			buttonRemove.Text = "<-";
+			buttonRemove.UseVisualStyleBackColor = true;
+			buttonRemove.Click += buttonRemove_Click;
 			// 
 			// buttonReset
 			// 
-			this.buttonReset.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.buttonReset.Location = new System.Drawing.Point(30, 69);
-			this.buttonReset.Name = "buttonReset";
-			this.buttonReset.Size = new System.Drawing.Size(78, 27);
-			this.buttonReset.TabIndex = 6;
-			this.buttonReset.Text = "Reset";
-			this.buttonReset.UseVisualStyleBackColor = true;
-			this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+			buttonReset.Anchor = AnchorStyles.None;
+			buttonReset.Location = new Point(30, 69);
+			buttonReset.Name = "buttonReset";
+			buttonReset.Size = new Size(78, 27);
+			buttonReset.TabIndex = 6;
+			buttonReset.Text = "Reset";
+			buttonReset.UseVisualStyleBackColor = true;
+			buttonReset.Click += buttonReset_Click;
 			// 
 			// recurseCB
 			// 
-			this.recurseCB.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.recurseCB.AutoSize = true;
-			this.recurseCB.Checked = true;
-			this.recurseCB.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.recurseCB.Location = new System.Drawing.Point(14, 220);
-			this.recurseCB.Name = "recurseCB";
-			this.recurseCB.Size = new System.Drawing.Size(110, 19);
-			this.recurseCB.TabIndex = 7;
-			this.recurseCB.Text = "Add Recursively";
-			this.recurseCB.UseVisualStyleBackColor = true;
+			recurseCB.Anchor = AnchorStyles.Top;
+			recurseCB.AutoSize = true;
+			recurseCB.Checked = true;
+			recurseCB.CheckState = CheckState.Checked;
+			recurseCB.Location = new Point(14, 220);
+			recurseCB.Name = "recurseCB";
+			recurseCB.Size = new Size(110, 19);
+			recurseCB.TabIndex = 7;
+			recurseCB.Text = "Add Recursively";
+			recurseCB.UseVisualStyleBackColor = true;
 			// 
 			// allowGroupsCheckbox
 			// 
-			this.allowGroupsCheckbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.allowGroupsCheckbox.AutoSize = true;
-			this.allowGroupsCheckbox.Location = new System.Drawing.Point(21, 195);
-			this.allowGroupsCheckbox.Name = "allowGroupsCheckbox";
-			this.allowGroupsCheckbox.Size = new System.Drawing.Size(97, 19);
-			this.allowGroupsCheckbox.TabIndex = 8;
-			this.allowGroupsCheckbox.Text = "Allow Groups";
-			this.allowGroupsCheckbox.UseVisualStyleBackColor = true;
-			this.allowGroupsCheckbox.CheckedChanged += new System.EventHandler(this.allowGroupsCheckbox_CheckedChanged);
+			allowGroupsCheckbox.Anchor = AnchorStyles.Top;
+			allowGroupsCheckbox.AutoSize = true;
+			allowGroupsCheckbox.Location = new Point(21, 195);
+			allowGroupsCheckbox.Name = "allowGroupsCheckbox";
+			allowGroupsCheckbox.Size = new Size(97, 19);
+			allowGroupsCheckbox.TabIndex = 8;
+			allowGroupsCheckbox.Text = "Allow Groups";
+			allowGroupsCheckbox.UseVisualStyleBackColor = true;
+			allowGroupsCheckbox.CheckedChanged += allowGroupsCheckbox_CheckedChanged;
 			// 
 			// stringsGroupBox
 			// 
-			this.stringsGroupBox.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.stringsGroupBox.Controls.Add(this.colsRadioButton);
-			this.stringsGroupBox.Controls.Add(this.rowsRadioButton);
-			this.stringsGroupBox.Location = new System.Drawing.Point(3, 102);
-			this.stringsGroupBox.Name = "stringsGroupBox";
-			this.stringsGroupBox.Size = new System.Drawing.Size(133, 87);
-			this.stringsGroupBox.TabIndex = 10;
-			this.stringsGroupBox.TabStop = false;
-			this.stringsGroupBox.Text = "Strings are:";
-			this.stringsGroupBox.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBoxes_Paint);
+			stringsGroupBox.Anchor = AnchorStyles.None;
+			stringsGroupBox.Controls.Add(colsRadioButton);
+			stringsGroupBox.Controls.Add(rowsRadioButton);
+			stringsGroupBox.Location = new Point(3, 102);
+			stringsGroupBox.Name = "stringsGroupBox";
+			stringsGroupBox.Size = new Size(133, 87);
+			stringsGroupBox.TabIndex = 10;
+			stringsGroupBox.TabStop = false;
+			stringsGroupBox.Text = "Strings are:";
+			stringsGroupBox.Paint += groupBoxes_Paint;
 			// 
 			// colsRadioButton
 			// 
-			this.colsRadioButton.AutoSize = true;
-			this.colsRadioButton.Location = new System.Drawing.Point(15, 48);
-			this.colsRadioButton.Name = "colsRadioButton";
-			this.colsRadioButton.Size = new System.Drawing.Size(64, 19);
-			this.colsRadioButton.TabIndex = 11;
-			this.colsRadioButton.Text = "Vertical";
-			this.colsRadioButton.UseVisualStyleBackColor = true;
+			colsRadioButton.AutoSize = true;
+			colsRadioButton.Location = new Point(15, 48);
+			colsRadioButton.Name = "colsRadioButton";
+			colsRadioButton.Size = new Size(63, 19);
+			colsRadioButton.TabIndex = 11;
+			colsRadioButton.Text = "Vertical";
+			colsRadioButton.UseVisualStyleBackColor = true;
 			// 
 			// rowsRadioButton
 			// 
-			this.rowsRadioButton.AutoSize = true;
-			this.rowsRadioButton.Location = new System.Drawing.Point(15, 22);
-			this.rowsRadioButton.Name = "rowsRadioButton";
-			this.rowsRadioButton.Size = new System.Drawing.Size(80, 19);
-			this.rowsRadioButton.TabIndex = 9;
-			this.rowsRadioButton.Text = "Horizontal";
-			this.rowsRadioButton.UseVisualStyleBackColor = true;
+			rowsRadioButton.AutoSize = true;
+			rowsRadioButton.Location = new Point(15, 22);
+			rowsRadioButton.Name = "rowsRadioButton";
+			rowsRadioButton.Size = new Size(80, 19);
+			rowsRadioButton.TabIndex = 9;
+			rowsRadioButton.Text = "Horizontal";
+			rowsRadioButton.UseVisualStyleBackColor = true;
 			// 
 			// nodeTreeView
 			// 
-			this.nodeTreeView.AllowDrop = true;
-			this.nodeTreeView.Cursor = System.Windows.Forms.Cursors.Default;
-			this.nodeTreeView.CustomDragCursor = null;
-			this.nodeTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.nodeTreeView.DragDefaultMode = System.Windows.Forms.DragDropEffects.Move;
-			this.nodeTreeView.DragDestinationNodeBackColor = System.Drawing.SystemColors.Highlight;
-			this.nodeTreeView.DragDestinationNodeForeColor = System.Drawing.SystemColors.HighlightText;
-			this.nodeTreeView.DragSourceNodeBackColor = System.Drawing.SystemColors.ControlLight;
-			this.nodeTreeView.DragSourceNodeForeColor = System.Drawing.SystemColors.ControlText;
-			this.nodeTreeView.Location = new System.Drawing.Point(3, 3);
-			this.nodeTreeView.Name = "nodeTreeView";
-			this.nodeTreeView.SelectedNodes = ((System.Collections.Generic.List<System.Windows.Forms.TreeNode>)(resources.GetObject("nodeTreeView.SelectedNodes")));
-			this.nodeTreeView.Size = new System.Drawing.Size(218, 316);
-			this.nodeTreeView.TabIndex = 2;
-			this.nodeTreeView.UsingCustomDragCursor = false;
+			nodeTreeView.AllowDrop = true;
+			nodeTreeView.CustomDragCursor = null;
+			nodeTreeView.Dock = DockStyle.Fill;
+			nodeTreeView.DragDefaultMode = DragDropEffects.Move;
+			nodeTreeView.DragDestinationNodeBackColor = SystemColors.Highlight;
+			nodeTreeView.DragDestinationNodeForeColor = SystemColors.HighlightText;
+			nodeTreeView.DragSourceNodeBackColor = SystemColors.ControlLight;
+			nodeTreeView.DragSourceNodeForeColor = SystemColors.ControlText;
+			nodeTreeView.Location = new Point(3, 3);
+			nodeTreeView.Name = "nodeTreeView";
+			nodeTreeView.SelectedNodes = (List<TreeNode>)resources.GetObject("nodeTreeView.SelectedNodes");
+			nodeTreeView.Size = new Size(260, 389);
+			nodeTreeView.TabIndex = 2;
+			nodeTreeView.UsingCustomDragCursor = false;
 			// 
 			// panel1
 			// 
-			this.panel1.Controls.Add(this.buttonOk);
-			this.panel1.Controls.Add(this.buttonCancel);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(0, 351);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(618, 38);
-			this.panel1.TabIndex = 11;
+			panel1.Controls.Add(buttonOk);
+			panel1.Controls.Add(buttonCancel);
+			panel1.Dock = DockStyle.Bottom;
+			panel1.Location = new Point(0, 395);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(675, 38);
+			panel1.TabIndex = 11;
 			// 
 			// flowLayoutPanel1
 			// 
-			this.flowLayoutPanel1.Controls.Add(this.buttonAdd);
-			this.flowLayoutPanel1.Controls.Add(this.buttonRemove);
-			this.flowLayoutPanel1.Controls.Add(this.buttonReset);
-			this.flowLayoutPanel1.Controls.Add(this.stringsGroupBox);
-			this.flowLayoutPanel1.Controls.Add(this.allowGroupsCheckbox);
-			this.flowLayoutPanel1.Controls.Add(this.recurseCB);
-			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(227, 3);
-			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(136, 316);
-			this.flowLayoutPanel1.TabIndex = 12;
+			flowLayoutPanel1.Controls.Add(buttonAdd);
+			flowLayoutPanel1.Controls.Add(buttonRemove);
+			flowLayoutPanel1.Controls.Add(buttonReset);
+			flowLayoutPanel1.Controls.Add(stringsGroupBox);
+			flowLayoutPanel1.Controls.Add(allowGroupsCheckbox);
+			flowLayoutPanel1.Controls.Add(recurseCB);
+			flowLayoutPanel1.Dock = DockStyle.Fill;
+			flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+			flowLayoutPanel1.Location = new Point(269, 3);
+			flowLayoutPanel1.Name = "flowLayoutPanel1";
+			flowLayoutPanel1.Size = new Size(136, 389);
+			flowLayoutPanel1.TabIndex = 12;
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.ColumnCount = 3;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.Controls.Add(this.nodeTreeView, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
-			this.tableLayoutPanel1.Controls.Add(this.chosenTargets, 2, 0);
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 14);
-			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 1;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(592, 322);
-			this.tableLayoutPanel1.TabIndex = 13;
+			tableLayoutPanel1.AutoSize = true;
+			tableLayoutPanel1.ColumnCount = 3;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+			tableLayoutPanel1.Controls.Add(nodeTreeView, 0, 0);
+			tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 0);
+			tableLayoutPanel1.Controls.Add(chosenTargets, 2, 0);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Location = new Point(0, 0);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 1;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Size = new Size(675, 395);
+			tableLayoutPanel1.TabIndex = 13;
 			// 
 			// LipSyncNodeSelect
 			// 
-			this.AcceptButton = this.buttonOk;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSize = true;
-			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(618, 389);
-			this.Controls.Add(this.tableLayoutPanel1);
-			this.Controls.Add(this.panel1);
-			this.Name = "LipSyncNodeSelect";
-			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.FormBorderStyle = FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Text = "LipSyncNodeSelect";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LipSyncNodeSelect_FormClosing);
-			this.Load += new System.EventHandler(this.LipSyncNodeSelect_Load);
-			this.stringsGroupBox.ResumeLayout(false);
-			this.stringsGroupBox.PerformLayout();
-			this.panel1.ResumeLayout(false);
-			this.flowLayoutPanel1.ResumeLayout(false);
-			this.flowLayoutPanel1.PerformLayout();
-			this.tableLayoutPanel1.ResumeLayout(false);
-			this.tableLayoutPanel1.PerformLayout();
-			this.ResumeLayout(false);
+			AcceptButton = buttonOk;
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			AutoSize = true;
+			CancelButton = buttonCancel;
+			ClientSize = new Size(675, 433);
+			Controls.Add(tableLayoutPanel1);
+			Controls.Add(panel1);
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "LipSyncNodeSelect";
+			SizeGripStyle = SizeGripStyle.Hide;
+			StartPosition = FormStartPosition.CenterParent;
+			Text = "LipSync Mapping Node Select";
+			FormClosing += LipSyncNodeSelect_FormClosing;
+			Load += LipSyncNodeSelect_Load;
+			stringsGroupBox.ResumeLayout(false);
+			stringsGroupBox.PerformLayout();
+			panel1.ResumeLayout(false);
+			flowLayoutPanel1.ResumeLayout(false);
+			flowLayoutPanel1.PerformLayout();
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel1.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.Button buttonOk;
+		private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonCancel;
         private Common.Controls.MultiSelectTreeview nodeTreeView;
         private System.Windows.Forms.ListBox chosenTargets;
