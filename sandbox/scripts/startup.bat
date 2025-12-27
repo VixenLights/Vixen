6cff10ae884c9d33260d6a8f312b667b25b4d58c
Jeff Uchitjil <git@k9kld.org>
2025-12-27 15:12:19 -0600

    VIX-3781 Update installer for NET 10
    * Update the versions of asp core net and desktop net to 10.0.1.
    * Update the sandbox configurations to use the same NET 10 dependencies as the installer.
    * Include the newest versions of the C++ redist.

--- sandbox/scripts/startup.bat
+++ sandbox/scripts/startup.bat
@@ -1,11 +1,11 @@
 REM Download windowsdesktop-runtime
-curl -L "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/8.0.21/windowsdesktop-runtime-8.0.21-win-x64.exe" --output C:\users\WDAGUtilityAccount\Downloads\windowsdesktop-runtime.exe
+curl -L "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.1/windowsdesktop-runtime-10.0.1-win-x64.exe" --output C:\users\WDAGUtilityAccount\Downloads\windowsdesktop-runtime.exe
 
 REM Install windowsdesktop-runtime
 C:\users\WDAGUtilityAccount\Downloads\windowsdesktop-runtime.exe /norestart /quiet
 
 REM Download aspcore-runtime
-curl -L "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.21/dotnet-hosting-8.0.21-win.exe" --output C:\users\WDAGUtilityAccount\Downloads\aspcore-runtime.exe
+curl -L "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.1/aspnetcore-runtime-10.0.1-win-x64.exe" --output C:\users\WDAGUtilityAccount\Downloads\aspcore-runtime.exe
 
 REM Install aspcore-runtime
 C:\users\WDAGUtilityAccount\Downloads\aspcore-runtime.exe /norestart /quiet
