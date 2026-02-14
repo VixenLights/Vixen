23d84f63f01b0ba1679d2b17c9da51f2cd3d5654
da-markson <davida.markson@gmail.com>
2026-02-13 23:22:43 -0600

    VIX-3697 Changes now report back to Setup Display dialog

---
+++ src/Vixen.Core/Sys/Props/HelperTools/PropParameters.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VixenApplication.SetupDisplay.Wizards.HelperTools
+{
+	public class PropParameters : Dictionary<string, object>
+	{
+		public PropParameters()
+		{
+		}
+
+		public PropParameters(string key, object value)
+		{
+			Update(key, value);
+		}
+
+		public PropParameters Update(string key, object value)
+		{
+			if (ContainsKey(key))
+			{
+				this[key] = value;
+			}
+			else
+			{
+				Add(key, value);
+			}
+			return this;
+		}
+
+		public object Get(string key)
+		{
+			if (ContainsKey(key))
+			{
+				return this[key];
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
