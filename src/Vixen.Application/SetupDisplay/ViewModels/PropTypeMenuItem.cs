0efb853381a808fe54a222a53e43b505b9ee53cf
da-markson <davida.markson@gmail.com>
2026-02-16 21:37:25 -0600

    VIX-3697 Incorporated the final list of changes from the previous PR

---
+++ src/Vixen.Application/SetupDisplay/ViewModels/PropTypeMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using Vixen.Sys.Props;
+
+namespace VixenApplication.SetupDisplay.ViewModels
+{
+	public class PropTypeMenuItem
+	{
+		public PropType Id { get; set; }
+		public required string DisplayName { get; set; }
+	}
+}
