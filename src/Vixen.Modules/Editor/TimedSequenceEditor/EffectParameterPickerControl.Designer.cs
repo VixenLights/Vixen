09dd06dd947d7854abacda274b8c31e9681ffb02
Jeff Uchitjil <git@k9kld.org>
2026-02-19 09:10:58 -0600

    VIX-3626 Switch to Autoscale Dpi mode for better scaling on large monitors

--- src/Vixen.Modules/Editor/TimedSequenceEditor/EffectParameterPickerControl.Designer.cs
+++ src/Vixen.Modules/Editor/TimedSequenceEditor/EffectParameterPickerControl.Designer.cs
@@ -77,8 +77,8 @@
 			// 
 			// EffectParameterPickerControl
 			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
+			AutoScaleDimensions = new SizeF(96F, 96F);
+			AutoScaleMode = AutoScaleMode.Dpi;
 			AutoSize = true;
 			AutoSizeMode = AutoSizeMode.GrowAndShrink;
 			Controls.Add(flowLayoutPanel1);
