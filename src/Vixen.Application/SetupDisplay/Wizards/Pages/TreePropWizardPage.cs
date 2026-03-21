f4f38b9091cd5dde46b18b506da0db62d8ce46ac
da-markson <davida.markson@gmail.com>
2026-03-21 18:08:32 -0500

    VIX-3697 1) Restoring/updating IPropFactory 2) Includes changes to keep the tree and intelligent fixture props working

--- src/Vixen.Application/SetupDisplay/Wizards/Pages/TreePropWizardPage.cs
+++ src/Vixen.Application/SetupDisplay/Wizards/Pages/TreePropWizardPage.cs
@@ -1,232 +1,242 @@
-﻿using Catel.Data;
-using Orc.Wizard;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+
+using Catel.Data;
+
 using Common.WPFCommon.Converters;
+
+using Orc.Wizard;
+
 using Vixen.Extensions;
 using Vixen.Sys;
 using Vixen.Sys.Props;
+
 using VixenApplication.SetupDisplay.ViewModels;
+
 using VixenModules.App.Props;
 using VixenModules.App.Props.Models.Tree;
 
 namespace VixenApplication.SetupDisplay.Wizards.Pages
 {
-	public class TreePropWizardPage : WizardPageBase, IPropWizardFinalPage
-	{
-		public TreePropWizardPage()
-		{
-			Title = "Basic Attributes";
-			Description = $"Enter attributes for {PropType.Tree.GetEnumDescription()}";
+    public class TreePropWizardPage : WizardPageBase, IPropWizardFinalPage
+    {
+        public TreePropWizardPage()
+        {
+            Title = "Basic Attributes";
+            Description = $"Enter attributes for {PropType.Tree.GetEnumDescription()}";
 
-			// Generic parameters
-			Name = "Tree 1";
-			Strings = 16;
-			NodesPerString = 50;
-		}
-		#region Name property
+            // Generic parameters
+            Name = "Tree 1";
+            Strings = 16;
+            NodesPerString = 50;
+            DegreesCoverage = 360;
+            LightSize = 2;
+            TopRadius = 10;
+            BottomRadius = 100;
+        }
+        #region Name property
 
-		/// <summary>
-		/// Gets or sets the Name value.
-		/// </summary>
-		public string Name
-		{
-			get { return GetValue<string>(NameProperty); }
-			set { SetValue(NameProperty, value); }
-		}
+        /// <summary>
+        /// Gets or sets the Name value.
+        /// </summary>
+        public string Name
+        {
+            get { return GetValue<string>(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
 
-		/// <summary>
-		/// Name property data.
-		/// </summary>
-		public static readonly IPropertyData NameProperty = RegisterProperty<string>(nameof(Name));
+        /// <summary>
+        /// Name property data.
+        /// </summary>
+        public static readonly IPropertyData NameProperty = RegisterProperty<string>(nameof(Name));
 
-		#endregion
+        #endregion
 
-		#region Strings property
+        #region Strings property
 
-		/// <summary>
-		/// Gets or sets the Strings value.
-		/// </summary>
-		public int Strings
-		{
-			get { return GetValue<int>(NodeCountProperty); }
-			set { SetValue(NodeCountProperty, value); }
-		}
-		public static readonly IPropertyData NodeCountProperty = RegisterProperty<int>(nameof(Strings));
+        /// <summary>
+        /// Gets or sets the Strings value.
+        /// </summary>
+        public int Strings
+        {
+            get { return GetValue<int>(NodeCountProperty); }
+            set { SetValue(NodeCountProperty, value); }
+        }
+        public static readonly IPropertyData NodeCountProperty = RegisterProperty<int>(nameof(Strings));
 
-		#endregion
+        #endregion
 
-		#region NodesPerString property
+        #region NodesPerString property
 
-		/// <summary>
-		/// Gets or sets the NodesPerString value.
-		/// </summary>
-		public int NodesPerString
-		{
-			get { return GetValue<int>(NodesPerStringProperty); }
-			set { SetValue(NodesPerStringProperty, value); }
-		}
+        /// <summary>
+        /// Gets or sets the NodesPerString value.
+        /// </summary>
+        public int NodesPerString
+        {
+            get { return GetValue<int>(NodesPerStringProperty); }
+            set { SetValue(NodesPerStringProperty, value); }
+        }
 
-		/// <summary>
-		/// NodesPerString property data.
-		/// </summary>
-		public static readonly IPropertyData NodesPerStringProperty = RegisterProperty<int>(nameof(NodesPerString));
+        /// <summary>
+        /// NodesPerString property data.
+        /// </summary>
+        public static readonly IPropertyData NodesPerStringProperty = RegisterProperty<int>(nameof(NodesPerString));
 
-		#endregion
+        #endregion
 
-		public int LightSize
-		{
-			get { return GetValue<int>(LightSizeProperty); }
-			set { SetValue(LightSizeProperty, Math.Clamp(value, LightSizeMinimum, LightSizeMaximum)); }
-		}
-		private static readonly IPropertyData LightSizeProperty = RegisterProperty<int>(nameof(LightSize));
+        public int LightSize
+        {
+            get { return GetValue<int>(LightSizeProperty); }
+            set { SetValue(LightSizeProperty, Math.Clamp(value, LightSizeMinimum, LightSizeMaximum)); }
+        }
+        private static readonly IPropertyData LightSizeProperty = RegisterProperty<int>(nameof(LightSize));
 
-		protected int LightSizeMinimum
-		{
-			get { return GetValue<int>(LightSizeMinimumProperty); }
-			set
-			{
-				if (LightSize < value)
-				{
-					LightSize = value;
-				}
-				SetValue(LightSizeMinimumProperty, value);
-			}
-		}
-		private static readonly IPropertyData LightSizeMinimumProperty = RegisterProperty<int>(nameof(LightSizeMinimum));
+        protected int LightSizeMinimum
+        {
+            get { return GetValue<int>(LightSizeMinimumProperty); }
+            set
+            {
+                if (LightSize < value)
+                {
+                    LightSize = value;
+                }
+                SetValue(LightSizeMinimumProperty, value);
+            }
+        }
+        private static readonly IPropertyData LightSizeMinimumProperty = RegisterProperty<int>(nameof(LightSizeMinimum));
 
-		protected int LightSizeMaximum
-		{
-			get { return GetValue<int>(LightSizeMaximumProperty); }
-			set
-			{
-				if (LightSize > value)
-				{
-					LightSize = value;
-				}
-				SetValue(LightSizeMaximumProperty, value);
-			}
-		}
-		private static readonly IPropertyData LightSizeMaximumProperty = RegisterProperty<int>(nameof(LightSizeMaximum));
+        protected int LightSizeMaximum
+        {
+            get { return GetValue<int>(LightSizeMaximumProperty); }
+            set
+            {
+                if (LightSize > value)
+                {
+                    LightSize = value;
+                }
+                SetValue(LightSizeMaximumProperty, value);
+            }
+        }
+        private static readonly IPropertyData LightSizeMaximumProperty = RegisterProperty<int>(nameof(LightSizeMaximum));
 
-		public int DegreesCoverage
-		{
-			get { return GetValue<int>(DegreesCoverageProperty); }
-			set { SetValue(DegreesCoverageProperty, value); }
-		}
-		public static readonly IPropertyData DegreesCoverageProperty = RegisterProperty<int>(nameof(DegreesCoverage));
+        public int DegreesCoverage
+        {
+            get { return GetValue<int>(DegreesCoverageProperty); }
+            set { SetValue(DegreesCoverageProperty, value); }
+        }
+        public static readonly IPropertyData DegreesCoverageProperty = RegisterProperty<int>(nameof(DegreesCoverage));
 
-		public int DegreeOffset
-		{
-			get { return GetValue<int>(DegreeOffsetProperty); }
-			set { SetValue(DegreeOffsetProperty, value); }
-		}
-		public static readonly IPropertyData DegreeOffsetProperty = RegisterProperty<int>(nameof(DegreeOffset));
+        public int DegreeOffset
+        {
+            get { return GetValue<int>(DegreeOffsetProperty); }
+            set { SetValue(DegreeOffsetProperty, value); }
+        }
+        public static readonly IPropertyData DegreeOffsetProperty = RegisterProperty<int>(nameof(DegreeOffset));
 
-		public int BaseHeight
-		{
-			get { return GetValue<int>(BaseHeightProperty); }
-			set { SetValue(BaseHeightProperty, value); }
-		}
-		public static readonly IPropertyData BaseHeightProperty = RegisterProperty<int>(nameof(BaseHeight));
+        public int BaseHeight
+        {
+            get { return GetValue<int>(BaseHeightProperty); }
+            set { SetValue(BaseHeightProperty, value); }
+        }
+        public static readonly IPropertyData BaseHeightProperty = RegisterProperty<int>(nameof(BaseHeight));
 
-		public int TopHeight
-		{
-			get { return GetValue<int>(TopHeightProperty); }
-			set { SetValue(TopHeightProperty, value); }
-		}
-		public static readonly IPropertyData TopHeightProperty = RegisterProperty<int>(nameof(TopHeight));
+        public int TopHeight
+        {
+            get { return GetValue<int>(TopHeightProperty); }
+            set { SetValue(TopHeightProperty, value); }
+        }
+        public static readonly IPropertyData TopHeightProperty = RegisterProperty<int>(nameof(TopHeight));
 
-		public int TopWidth
-		{
-			get { return GetValue<int>(TopWidthProperty); }
-			set { SetValue(TopWidthProperty, value); }
-		}
-		public static readonly IPropertyData TopWidthProperty = RegisterProperty<int>(nameof(TopWidth));
+        public int TopWidth
+        {
+            get { return GetValue<int>(TopWidthProperty); }
+            set { SetValue(TopWidthProperty, value); }
+        }
+        public static readonly IPropertyData TopWidthProperty = RegisterProperty<int>(nameof(TopWidth));
 
-		public StartLocation StartLocation
-		{
-			get { return GetValue<StartLocation>(StartLocationProperty); }
-			set { SetValue(StartLocationProperty, value); }
-		}
-		public static readonly IPropertyData StartLocationProperty = RegisterProperty<StartLocation>(nameof(StartLocation));
+        public StartLocation StartLocation
+        {
+            get { return GetValue<StartLocation>(StartLocationProperty); }
+            set { SetValue(StartLocationProperty, value); }
+        }
+        public static readonly IPropertyData StartLocationProperty = RegisterProperty<StartLocation>(nameof(StartLocation));
 
-		public bool ZigZag
-		{
-			get { return GetValue<bool>(ZigZagProperty); }
-			set { SetValue(ZigZagProperty, value); }
-		}
-		public static readonly IPropertyData ZigZagProperty = RegisterProperty<bool>(nameof(ZigZag));
+        public bool ZigZag
+        {
+            get { return GetValue<bool>(ZigZagProperty); }
+            set { SetValue(ZigZagProperty, value); }
+        }
+        public static readonly IPropertyData ZigZagProperty = RegisterProperty<bool>(nameof(ZigZag));
 
-		public int ZigZagOffset
-		{
-			get { return GetValue<int>(ZigZagOffsetProperty); }
-			set { SetValue(ZigZagOffsetProperty, value); }
-		}
-		public static readonly IPropertyData ZigZagOffsetProperty = RegisterProperty<int>(nameof(ZigZagOffset));
+        public int ZigZagOffset
+        {
+            get { return GetValue<int>(ZigZagOffsetProperty); }
+            set { SetValue(ZigZagOffsetProperty, value); }
+        }
+        public static readonly IPropertyData ZigZagOffsetProperty = RegisterProperty<int>(nameof(ZigZagOffset));
 
-		public float TopRadius
-		{
-			get { return GetValue<float>(TopRadiusProperty); }
-			set { SetValue(TopRadiusProperty, value); }
-		}
-		public static readonly IPropertyData TopRadiusProperty = RegisterProperty<float>(nameof(TopRadius));
+        public float TopRadius
+        {
+            get { return GetValue<float>(TopRadiusProperty); }
+            set { SetValue(TopRadiusProperty, value); }
+        }
+        public static readonly IPropertyData TopRadiusProperty = RegisterProperty<float>(nameof(TopRadius));
 
-		public float BottomRadius
-		{
-			get { return GetValue<float>(BottomRadiusProperty); }
-			set { SetValue(BottomRadiusProperty, value); }
-		}
-		public static readonly IPropertyData BottomRadiusProperty = RegisterProperty<float>(nameof(BottomRadius));
+        public float BottomRadius
+        {
+            get { return GetValue<float>(BottomRadiusProperty); }
+            set { SetValue(BottomRadiusProperty, value); }
+        }
+        public static readonly IPropertyData BottomRadiusProperty = RegisterProperty<float>(nameof(BottomRadius));
 
-		#region Rotation property
-		public ObservableCollection<AxisRotationViewModel> Rotations
-		{
-			get { return GetValue<ObservableCollection<AxisRotationViewModel>>(RotationsProperty); }
-			set { SetValue(RotationsProperty, value); }
-		}
-		private static readonly IPropertyData RotationsProperty = RegisterProperty<ObservableCollection<AxisRotationViewModel>>(nameof(Rotations));
-		#endregion
+        #region Rotation property
+        public ObservableCollection<AxisRotationViewModel> Rotations
+        {
+            get { return GetValue<ObservableCollection<AxisRotationViewModel>>(RotationsProperty); }
+            set { SetValue(RotationsProperty, value); }
+        }
+        private static readonly IPropertyData RotationsProperty = RegisterProperty<ObservableCollection<AxisRotationViewModel>>(nameof(Rotations));
+        #endregion
 
-		public TreeModel LightPropModel
-		{
-			get { return GetValue<TreeModel>(LightPropModelProperty); }
-			set { SetValue(LightPropModelProperty, value); }
-		}
-		private static readonly IPropertyData LightPropModelProperty = RegisterProperty<TreeModel>(nameof(LightPropModel));
+        public TreeModel LightPropModel
+        {
+            get { return GetValue<TreeModel>(LightPropModelProperty); }
+            set { SetValue(LightPropModelProperty, value); }
+        }
+        private static readonly IPropertyData LightPropModelProperty = RegisterProperty<TreeModel>(nameof(LightPropModel));
 
-		public override ISummaryItem GetSummary()
-		{
-			return new SummaryItem
-			{
-				Title = this.Title,
-				Summary = $"Prop Type: {PropType.Tree.GetEnumDescription()}\n" +
-				          $"Name: {Name}\n" +
-				          $"Strings: {Strings}\n" +
-				          $"Light Size: {LightSize}\n" +
-				          $"Start Location: {EnumValueTypeConverter.GetDescription(StartLocation)}\n" +
-				          $"Nodes Per String: {NodesPerString}\n" +
-				          $"Degree Offset: {DegreeOffset}\n" +
-				          $"Degrees Coverage: {DegreesCoverage}\n" +
-				          $"Base Height: {BaseHeight}\n" +
-				          $"Top Height: {TopHeight}\n" +
-				          $"Top Width: {TopWidth}\n" +
-				          $"ZigZag: {ZigZag}\n" +
-				          $"ZigZag Offset: {ZigZagOffset}\n" +
-				          $"Top Radius: {TopRadius}\n" +
-				          $"Bottom Radius: {BottomRadius}\n" +
-				          $"{Rotations[0].Axis} Rotation: {Rotations[0].RotationAngle}\u00B0\n" +
-				          $"{Rotations[1].Axis} Rotation: {Rotations[1].RotationAngle}\u00B0\n" +
-				          $"{Rotations[2].Axis} Rotation: {Rotations[2].RotationAngle}\u00B0"
+        public override ISummaryItem GetSummary()
+        {
+            return new SummaryItem
+            {
+                Title = this.Title,
+                Summary = $"Prop Type: {PropType.Tree.GetEnumDescription()}\n" +
+                          $"Name: {Name}\n" +
+                          $"Strings: {Strings}\n" +
+                          $"Light Size: {LightSize}\n" +
+                          $"Start Location: {EnumValueTypeConverter.GetDescription(StartLocation)}\n" +
+                          $"Nodes Per String: {NodesPerString}\n" +
+                          $"Degree Offset: {DegreeOffset}\n" +
+                          $"Degrees Coverage: {DegreesCoverage}\n" +
+                          $"Base Height: {BaseHeight}\n" +
+                          $"Top Height: {TopHeight}\n" +
+                          $"Top Width: {TopWidth}\n" +
+                          $"ZigZag: {ZigZag}\n" +
+                          $"ZigZag Offset: {ZigZagOffset}\n" +
+                          $"Top Radius: {TopRadius}\n" +
+                          $"Bottom Radius: {BottomRadius}\n" +
+                          $"{Rotations[0].Axis} Rotation: {Rotations[0].RotationAngle}\u00B0\n" +
+                          $"{Rotations[1].Axis} Rotation: {Rotations[1].RotationAngle}\u00B0\n" +
+                          $"{Rotations[2].Axis} Rotation: {Rotations[2].RotationAngle}\u00B0"
             };
-		}
+        }
 
         public IProp GetProp()
-		{
-			var tree = VixenSystem.Props.CreateProp<Tree>(Name);
-			tree.Strings = Strings;
-			tree.NodesPerString = NodesPerString;
-			//TODO add in other fields when wizard has full function
-			return tree;
-		}
-	}
+        {
+            var tree = VixenSystem.Props.CreateProp<Tree>(Name);
+            tree.Strings = Strings;
+            tree.NodesPerString = NodesPerString;
+            //TODO add in other fields when wizard has full function
+            return tree;
+        }
+    }
 }
--- src/Vixen.Application/SetupDisplay/Wizards/PropFactories/TreePropFactory.cs
+++ src/Vixen.Application/SetupDisplay/Wizards/PropFactories/TreePropFactory.cs
@@ -7,99 +7,101 @@ using VixenModules.Editor.PropWizard;
 
 namespace VixenApplication.SetupDisplay.Wizards.PropFactories
 {
-	/// <summary>
-	/// Creates Tree props from wizard data.
-	/// </summary>
-	internal class TreePropFactory : IPropFactory
-	{
-		/// <summary>
-		/// Create a default Tree prop
-		/// </summary>
-		/// <returns>Returns both <see cref="IProp"/> which specifies the new Prop and <see cref="IPropGroup"/> which specifies the group that contains the Prop</returns>
-		public (IProp, IPropGroup) CreateBaseProp()
-		{
-			// Create the Tree prop
-			Tree Tree = VixenSystem.Props.CreateProp<Tree>(VixenSystem.Props.GenerateUniquePropTitle(PropType.Tree));
+    /// <summary>
+    /// Creates Tree props from wizard data.
+    /// </summary>
+    internal class TreePropFactory : IPropFactory
+    {
+        #region IPropFactory
 
-			// Create the collection of props to return 
-			IPropGroup propGroup = new PropGroup();
+        /// <inheritdoc/>		
+        public IPropGroup GetProps(IPropWizard wizard)
+        {
+            // Create the Tree prop
+            Tree tree = VixenSystem.Props.CreateProp<Tree>(VixenSystem.Props.GenerateUniquePropTitle(PropType.Tree));
 
-			// Add the Tree to the prop collections 
-			propGroup.Props.Add(Tree);
+            // Transfer the data from the wizard into the tree prop
+            UpdateProp(tree, wizard);
 
-			// Return the collection of props
-			return (Tree, propGroup);
-		}
+            // Create the collection of props to return 
+            IPropGroup propGroup = new PropGroup();
 
-		/// <summary>
-		/// Transfers data from the Tree Prop into the Wizard
-		/// </summary>
-		/// <param name="prop">Specifies the Tree prop source</param>
-		/// <param name="wizard">Specifies the Tree prop wizard destination</param>
-		public void LoadWizard(IProp prop, IPropWizard wizard)
-		{
-			Tree tree = (Tree)prop;
+            // Add the Tree to the prop collections 
+            propGroup.Props.Add(tree);
 
-			// Configure the wizard with the base Tree properties
-			TreePropWizardPage treePropPage = (TreePropWizardPage)wizard.Pages.Single(page => page is TreePropWizardPage);
-			treePropPage.Name = tree.Name;
-			treePropPage.Strings = tree.Strings;
-			treePropPage.NodesPerString = tree.NodesPerString;
-			treePropPage.LightSize = tree.LightSize;
-			treePropPage.DegreeOffset = tree.DegreeOffset;
-			treePropPage.DegreesCoverage = tree.DegreesCoverage;
-			treePropPage.BaseHeight = tree.BaseHeight;
-			treePropPage.TopHeight = tree.TopHeight;
-			treePropPage.TopWidth = tree.TopWidth;
-			treePropPage.StartLocation = tree.StartLocation;
-			treePropPage.ZigZag = tree.ZigZag;
-			treePropPage.ZigZagOffset = tree.ZigZagOffset;
-			treePropPage.TopRadius = tree.TopRadius;
-			treePropPage.BottomRadius = tree.BottomRadius;
-			treePropPage.Rotations = AxisRotationViewModel.ConvertToViewModel(tree.Rotations);
+            // Return the collection of props
+            return propGroup;
+        }
 
-			// Configure the wizard with the Additional Tree properties
-			//TreePropAdditionalWizardPage additionalPage = (TreePropAdditionalWizardPage)wizard.Pages.Single(page => page is TreePropAdditionalWizardPage);
-			//additionalPage.LeftRight = tree.LeftRight;
+        /// <summary>
+        /// Transfers data from the Tree Prop into the Wizard
+        /// </summary>
+        /// <param name="prop">Specifies the Tree prop source</param>
+        /// <param name="wizard">Specifies the Tree prop wizard destination</param>
+        public void LoadWizard(IProp prop, IPropWizard wizard)
+        {
+            Tree tree = (Tree)prop;
 
-			// Configure the wizard with the Dimming Tree properties
-			DimmingWizardPage dimmingPage = (DimmingWizardPage)wizard.Pages.Single(page => page is DimmingWizardPage);
-			dimmingPage.Brightness = tree.Brightness;
-			dimmingPage.Gamma = tree.Gamma;
+            // Configure the wizard with the base Tree properties
+            TreePropWizardPage treePropPage = (TreePropWizardPage)wizard.Pages.Single(page => page is TreePropWizardPage);
+            treePropPage.Name = tree.Name;
+            treePropPage.Strings = tree.Strings;
+            treePropPage.NodesPerString = tree.NodesPerString;
+            treePropPage.LightSize = tree.LightSize;
+            treePropPage.DegreeOffset = tree.DegreeOffset;
+            treePropPage.DegreesCoverage = tree.DegreesCoverage;
+            treePropPage.BaseHeight = tree.BaseHeight;
+            treePropPage.TopHeight = tree.TopHeight;
+            treePropPage.TopWidth = tree.TopWidth;
+            treePropPage.StartLocation = tree.StartLocation;
+            treePropPage.ZigZag = tree.ZigZag;
+            treePropPage.ZigZagOffset = tree.ZigZagOffset;
+            treePropPage.TopRadius = tree.TopRadius;
+            treePropPage.BottomRadius = tree.BottomRadius;
+            treePropPage.Rotations = AxisRotationViewModel.ConvertToViewModel(tree.Rotations);
 
-			// Configure the Color Tree properties
-			ColorWizardPage colorPage = (ColorWizardPage)wizard.Pages.Single(page => page is ColorWizardPage);
-			colorPage.StringType = tree.StringType;
-			colorPage.SingleColorOption = tree.SingleColorOption;
-			colorPage.SelectedColorSet = tree.SelectedColorSet;
-		}
+            // Configure the wizard with the Additional Tree properties
+            //TreePropAdditionalWizardPage additionalPage = (TreePropAdditionalWizardPage)wizard.Pages.Single(page => page is TreePropAdditionalWizardPage);
+            //additionalPage.LeftRight = tree.LeftRight;
 
-		/// <summary>
-		/// Transfers data from the Wizard into the Tree Prop
-		/// </summary>
-		/// <param name="prop">Specifies the Tree prop destination</param>
-		/// <param name="wizard">Specifies the Tree prop wizard source</param>
-		public void UpdateProp(IProp prop, IPropWizard wizard)
-		{
-			Tree tree = (Tree)prop;
+            // Configure the wizard with the Dimming Tree properties
+            DimmingWizardPage dimmingPage = (DimmingWizardPage)wizard.Pages.Single(page => page is DimmingWizardPage);
+            dimmingPage.Brightness = tree.Brightness;
+            dimmingPage.Gamma = tree.Gamma;
 
-			// Configure the base Tree properties
-			TreePropWizardPage treePropPage = (TreePropWizardPage)wizard.Pages.Single(page => page is TreePropWizardPage);
-			tree.Name = treePropPage.Name;
-			tree.Strings = treePropPage.Strings;
-			tree.NodesPerString = treePropPage.NodesPerString;
-			tree.LightSize = treePropPage.LightSize;
-			tree.DegreeOffset = treePropPage.DegreeOffset;
-			tree.DegreesCoverage = treePropPage.DegreesCoverage;
-			tree.BaseHeight = treePropPage.BaseHeight;
-			tree.TopHeight = treePropPage.TopHeight;
-			tree.TopWidth = treePropPage.TopWidth;
-			tree.StartLocation = treePropPage.StartLocation;
-			tree.ZigZag = treePropPage.ZigZag;
-			tree.ZigZagOffset = treePropPage.ZigZagOffset;
-			tree.TopRadius = treePropPage.TopRadius;
-			tree.BottomRadius = treePropPage.BottomRadius;
-			tree.Rotations = AxisRotationViewModel.ConvertToModel(treePropPage.Rotations);
+            // Configure the Color Tree properties
+            ColorWizardPage colorPage = (ColorWizardPage)wizard.Pages.Single(page => page is ColorWizardPage);
+            colorPage.StringType = tree.StringType;
+            colorPage.SingleColorOption = tree.SingleColorOption;
+            colorPage.SelectedColorSet = tree.SelectedColorSet;
+        }
+
+        /// <summary>
+        /// Transfers data from the Wizard into the Tree Prop
+        /// </summary>
+        /// <param name="prop">Specifies the Tree prop destination</param>
+        /// <param name="wizard">Specifies the Tree prop wizard source</param>
+        public void UpdateProp(IProp prop, IPropWizard wizard)
+        {
+            Tree tree = (Tree)prop;
+
+            // Configure the base Tree properties
+            TreePropWizardPage treePropPage = (TreePropWizardPage)wizard.Pages.Single(page => page is TreePropWizardPage);
+            tree.Name = treePropPage.Name;
+            tree.Strings = treePropPage.Strings;
+            tree.NodesPerString = treePropPage.NodesPerString;
+            tree.LightSize = treePropPage.LightSize;
+            tree.DegreeOffset = treePropPage.DegreeOffset;
+            tree.DegreesCoverage = treePropPage.DegreesCoverage;
+            tree.BaseHeight = treePropPage.BaseHeight;
+            tree.TopHeight = treePropPage.TopHeight;
+            tree.TopWidth = treePropPage.TopWidth;
+            tree.StartLocation = treePropPage.StartLocation;
+            tree.ZigZag = treePropPage.ZigZag;
+            tree.ZigZagOffset = treePropPage.ZigZagOffset;
+            tree.TopRadius = treePropPage.TopRadius;
+            tree.BottomRadius = treePropPage.BottomRadius;
+            tree.Rotations = AxisRotationViewModel.ConvertToModel(treePropPage.Rotations);
 
             // Configure the Additional Tree properties
             //TreePropAdditionalWizardPage additionalPage = (TreePropAdditionalWizardPage)wizard.Pages.Single(page => page is TreePropAdditionalWizardPage);
@@ -107,14 +109,16 @@ namespace VixenApplication.SetupDisplay.Wizards.PropFactories
 
             // Configure the Dimming Tree properties
             DimmingWizardPage dimmingPage = (DimmingWizardPage)wizard.Pages.Single(page => page is DimmingWizardPage);
-			tree.Brightness = dimmingPage.Brightness;
-			tree.Gamma = dimmingPage.Gamma;
+            tree.Brightness = dimmingPage.Brightness;
+            tree.Gamma = dimmingPage.Gamma;
 
-			// Configure the Color Tree properties
-			ColorWizardPage colorPage = (ColorWizardPage)wizard.Pages.Single(page => page is ColorWizardPage);
-			tree.StringType = colorPage.StringType;
-			tree.SingleColorOption = colorPage.SingleColorOption;
-			tree.SelectedColorSet = colorPage.SelectedColorSet;
-		}
-	}
+            // Configure the Color Tree properties
+            ColorWizardPage colorPage = (ColorWizardPage)wizard.Pages.Single(page => page is ColorWizardPage);
+            tree.StringType = colorPage.StringType;
+            tree.SingleColorOption = colorPage.SingleColorOption;
+            tree.SelectedColorSet = colorPage.SelectedColorSet;
+        }
+
+        #endregion
+    }
 }
