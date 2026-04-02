f5c3e74fb6f608c0032c78fe5b6275e44e695b10
Jeff Uchitjil <git@k9kld.org>
2026-04-02 10:46:25 -0500

    VIX-3856 Fix the debug access log
    * Fix a spelling error in existing logging.
    * Fix the debug request logger that has not worked since the Owin was replaced.

---
+++ src/Vixen.Modules/App/WebServer/RequestLoggingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using NLog;
+
+namespace VixenModules.App.WebServer
+{
+	//Writes request info to the debug log for debugging purposes
+	internal class RequestLoggingMiddleware
+	{
+		private static readonly Logger Logging = LogManager.GetCurrentClassLogger();
+		private readonly RequestDelegate _next;
+
+		public RequestLoggingMiddleware(RequestDelegate next)
+		{
+			_next = next ?? throw new ArgumentNullException(nameof(next));
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			await _next(context); // Call the next middleware
+
+			var address = context.Connection.RemoteIpAddress != null
+				? context.Connection.RemoteIpAddress.ToString().Equals("::1")
+					? "127.0.0.1"
+					: context.Connection.RemoteIpAddress.ToString()
+				: string.Empty;
+
+			// Log Response Details
+			Logging.Debug("{Ip} - \"{Method} {Path} {Protocol}\" {StatusCode} {Size}", address, 
+				context.Request.Method, context.Request.Path, context.Request.Protocol, context.Response.StatusCode, context.Response.ContentLength);
+		}
+
+	}
+}
