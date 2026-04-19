3171b7028ef78938403acfb29aeeb5afd9de1138
johncbaur <46469358+johncbaur@users.noreply.github.com>
2026-04-18 20:23:36 -0500

    VIX-3885 Create Base Prop Wizard Pages
    VIX-3885 Create Base Prop Wizard Pages

---
+++ src/Vixen.Application/SetupDisplay/Wizards/Pages/ILightPropWizardPage.cs
@@ -0,0 +1,13 @@
+﻿namespace VixenApplication.SetupDisplay.Wizards.Pages
+{
+	/// <summary>
+	/// Maintains light prop wizard page data.
+	/// </summary>
+	public interface ILightPropWizardPage : IBasePropWizardPage
+	{
+		/// <summary>
+		/// Size of the lights.
+		/// </summary>
+		int LightSize { get; set; }
+	}
+}
