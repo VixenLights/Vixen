1a175722333538d2d9ce6de04e35bfce6b8938d4
johncbaur <46469358+johncbaur@users.noreply.github.com>
2026-04-18 20:23:36 -0500

    VIX-3885 Create Base Prop Wizard Pages
    VIX-3885 Create Base Prop Wizard Pages

---
+++ src/Vixen.Application/SetupDisplay/Wizards/Pages/IBasePropWizardPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using VixenApplication.SetupDisplay.ViewModels;
+
+namespace VixenApplication.SetupDisplay.Wizards.Pages
+{
+	/// <summary>
+	/// Maintains base prop wizard data.
+	/// </summary>
+	public interface IBasePropWizardPage
+	{
+		/// <summary>
+		/// Gets or sets the Name of the prop.
+		/// </summary>
+		string Name { get; set; }
+
+		/// <summary>
+		/// Collection of axis rotations.
+		/// </summary>
+		ObservableCollection<AxisRotationViewModel> Rotations { get; set; }
+	}
+}
